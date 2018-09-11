7b731714f01ab2dc7e0c36fd1064c9c35f7b16d8
Danny Vail <danny_vail@outlook.com>
2018-09-11 16:02:50 -0400

    MODEL PROJECTS/EXPERIENCES/EDUCATION WITH EMBEDDED DOCUMENTS In Seeker.model.js - Add project, experience and education schema to seeker model file and add to seeker schema as subdocument - Remove id field; use _id - Edit validation messages for consistency

--- backEnd/api/models/Seeker.model.js
+++ backEnd/api/models/Seeker.model.js
@@ -2,91 +2,172 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const Schema = mongoose.Schema;
 
-const Seeker = new Schema({
-  id: { type: ObjectId },
-  profilePicture: {
+const projectSchema = new Schema({
+  title: {
     type: String,
+    required: [true, 'Project title is required']
+  },
+  description: {
+    type: String
+  },
+  img: {
+    type: String
+    // PENDING Validate URL
+  },
+  link: {
+    type: String
+    // PENDING Validate URL
+  },
+  repo: {
+    type: String
+    // PENDING Validate URL
+  },
+  tech: {
+    type: Array
+  },
+});
+
+const experienceSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Job title is required'],
+  },
+  company: {
+    type: String,
+    required: [true, 'Company is required'],
+  },
+  location: {
+    type: String,
+    required: [true, 'Job location is required'],
+    // PENDING Validate
+  },
+  headline: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  startYear: {
+    type: Date,
+    required: [true, 'Start year is required'],
+  },
+  endYear: {
+    type: Date,
+  },
+});
+
+const educationSchema = new Schema({
+  school: {
+    type: String,
+    required: [true, 'School is required']
+  },
+  degree: {
+    type: String
+  },
+  fieldOfStudy: {
+    type: String,
+    required: [true, 'Field of study is required']
+  },
+  startYear: {
+    type: Date,
+    required: [true, 'Start year is required']
+  },
+  endYear: {
+    type: Date
+  },
+  description: {
+    type: String
+  },
+});
+
+const seekerSchema = new Schema({
+  img: {
+    type: String
     // TODO: Validate the URL
   },
   firstName: {
     type: String,
-    required: [true, 'First name is required'],
+    required: [true, 'First name is required']
   },
   lastName: {
     type: String,
-    required: [true, 'Last name is required'],
+    required: [true, 'Last name is required']
   },
   password: {
     type: String,
-    required: [true, 'Password required'],
+    required: [true, 'Password is required'],
     minlength: [8, 'Password must be at least 8 characters'],
     validate: {
       validator: val => {
-        return /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[!@#$%&]).{8,})\S$/.test(val);
+        return /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9])(?=\S*?[!@#$%&]).{8,})\S$/.test(
+          val
+        );
       },
       message:
-        'Password must contain 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character: !, @, #, $, %, &',
-    },
+        'Password must contain 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character: !, @, #, $, %, &'
+    }
   },
   email: {
     type: String,
     unique: true,
-    required: [true, 'Email address required'],
+    required: [true, 'Email address is required'],
     validate: {
       validator: val => {
-        return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(val);
+        return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(
+          val
+        );
       },
-      message: '{VALUE} is not a valid email address',
-    },
+      message: '{VALUE} is not a valid email address'
+    }
   },
   desiredTitle: {
     type: String,
-    required: [true, 'Define your prefered title'],
+    required: [true, 'Desired title is required']
   },
   currentLocation: {
-    type: String,
+    type: String
     // PENDING: Implement validation.
   },
+  summary: {
+    type: String
+  },
   github: {
-    type: String,
+    type: String
     // PENDING: Validate with github API
   },
   linkedin: {
-    type: String,
+    type: String
     // PENDING: Validate with linkedin API
   },
   portfolio: {
-    type: String,
+    type: String
+    // TODO: Validate the URL
+  },
+  resume: {
+    type: String
     // TODO: Validate the URL
   },
   acclaimBadge: {
-    type: String,
+    type: String
     // PENDING: Validate via API
     // TODO: Validate the URL
   },
   placesInterested: {
-    type: Array,
+    type: Array
     // TODO: Validate city's names
   },
-  summary: {
-    type: String,
-  },
   topSkills: {
-    type: Array,
+    type: Array
   },
   additionalSkills: {
-    type: Array,
+    type: Array
   },
   familiarWith: {
-    type: Array,
+    type: Array
   },
-  projects: [{ type: ObjectId, ref: 'Projects' }],
-  experience: [{ type: ObjectId, ref: 'Experience' }],
-  education: [{ type: ObjectId, ref: 'Educations' }],
-  linkToResume: {
-    type: String,
-    // TODO: Validate the URL
-  },
+  projects: [projectSchema],
+  experience: [experienceSchema],
+  education: [educationSchema],
 });
 
-module.exports = mongoose.model('Seekers', Seeker);
+module.exports = mongoose.model('Seeker', seekerSchema);
