9a4b4e3d7ee69bdf118982bafc4f23481465cdf9
jesuarva <jesuarva.developer@gmail.com>
2018-09-12 00:56:04 +0200

    DEFINE the  endpoint path which will serve the Employer model

--- backEnd/server/routes.js
+++ backEnd/server/routes.js
@@ -5,6 +5,7 @@
   const { usersRouter } = require('../api/routes/users');
 */
 const seekersRouter = require('../api/routes/Seeker.router');
+const employerRouter = require('../api/routes/Employer.router');
 
 module.exports = function(server) {
   /*
@@ -14,9 +15,14 @@ module.exports = function(server) {
     server.use('/api/tasks', tasksRouter);
   */
   server.use('/api/seekers', seekersRouter);
+  server.use('/api/employers', employerRouter);
 
   server.get('/api', (req, res) => {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Developer Profiles API"}');
   });
 };
+
+{
+  'hiringManagerFirstName', 'hiringManagerLastName', 'hiringManagerEmail';
+}
