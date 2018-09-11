56e6943bcdcb391e11c328c8de5b27d9ad0707dc
samarv <samarvir1996@gmail.com>
2018-09-12 04:51:35 +0530

    added login page

--- front-end/lambda-in/src/App.js
+++ front-end/lambda-in/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import DevSignUp from './Components/DevSignUp/DevSignUp'
+import DevSignUp from './Components/DevSignUp/DevSignUp';
+import DevLogin from './Components/DevLogIn/DevLogIn';
 
 
 class App extends Component {
+  state = {
+    isSignedIn: false
+  }
   render() {
-    return (
-      <div>
-        <DevSignUp />
-      </div>
-    )
+    if(this.state.isSignedIn){
+      return (
+        <div>
+          <DevLogin />
+        </div>
+      );
+    }
+    else{
+      return (
+        <div>
+          <DevSignUp />
+        </div>
+      );
+    }
+  
   }
 }
 
 
 export default App;
-
 
 
