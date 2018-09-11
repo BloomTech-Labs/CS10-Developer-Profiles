8d99438532b64da1f233d23e0209e3cbffdefa31
NathanielFlory <nathanielflory@gmail.com>
2018-09-11 19:21:23 -0400

    Added background image for landing page, need alt prop for image -Added Image Container folder -Added image-container CSS and JS files

--- front-end/lambda-in/src/Components/Landing Page/landing-page.js
+++ front-end/lambda-in/src/Components/Landing Page/landing-page.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavBar from './Navbar/navbar.js';
+import ImageContainer from './Image Container/image-container.js'
 
 class LandingPage extends React.Component {
     render() {
         return (
+            <div>
             <NavBar />
+            <ImageContainer />
+            </div>
         )
     }
-}
+};
 
 
 export default LandingPage;
