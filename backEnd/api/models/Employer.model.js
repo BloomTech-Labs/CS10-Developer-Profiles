b9593caefd920267bb6039f493621687610101d0
jesuarva <jesuarva.developer@gmail.com>
2018-09-11 23:51:52 +0200

    DELETE no neccesary old files

--- backEnd/api/models/Employer.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
-const Schema = mongoose.Schema;
-
-const Employer = new Schema({
-  id: { type: ObjectId },
-});
-
-module.exports = mongoose.model('Employers', Employer);
