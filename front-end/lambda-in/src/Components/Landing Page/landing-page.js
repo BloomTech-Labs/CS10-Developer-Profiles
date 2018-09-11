384a94cff78081abcbfa4da32d84b3d6b820492d
NathanielFlory <nathanielflory@gmail.com>
2018-09-11 17:56:04 -0400

    Navbar Component Complete and rendering, Implemented Material UI for styling -Added material dependencies -Added Navbar Folder to landing page -Added Navbar CSS and JS files -Broke out landing page component and navbar component within landing page

---
+++ front-end/lambda-in/src/Components/Landing Page/landing-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './Navbar/navbar.js';
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <NavBar />
+        )
+    }
+}
+
+
+export default LandingPage;
