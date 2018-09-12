df83be6d763a98039939444ea82d8ae2db43f626
Danny Vail <danny_vail@outlook.com>
2018-09-12 17:59:59 -0400

    REMOVE SEEKER OBJECT CREATION FROM SETUP FUNCTION AND USE MOCK DATA OBJECT In Seeker.model.test.js - Remove testSeeker object from setup function - Import testSeeker object from mock data in utils

--- backEnd/api/models/Seeker/Seeker.model.test.js
+++ backEnd/api/models/Seeker/Seeker.model.test.js
@@ -2,21 +2,14 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const Seeker = require('./Seeker.model');
+const { testSeeker } = require('../../utils/seeker.testData');
 
 const { MONGODB_URI_TEST } = process.env;
 
 describe('seeker model', () => {
-  const testSeeker = {};
+  // const testSeeker = {};
 
-  beforeAll(() => {
-    testSeeker.firstName = 'John';
-    testSeeker.lastName = 'Smith';
-    testSeeker.password = 'Password123&';
-    testSeeker.email = 'jsmith@example.com';
-    testSeeker.desiredTitle = 'Software Engineer';
-
-    return mongoose.connect(MONGODB_URI_TEST);
-  });
+  beforeAll(() => mongoose.connect(MONGODB_URI_TEST));
 
   afterEach(() => Seeker.remove());
 
