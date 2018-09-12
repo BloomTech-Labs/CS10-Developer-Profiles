34fe0cea2638ba6d5ee77e22002ae62e1a9b2b1f
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-12 13:16:02 -0700

    feat: Building DevProfile Page
    - page tested with dummy state
    - page tested with dummy App.js

--- front-end/lambda-in/src/Components/DevProfile/DevProfile.js
+++ front-end/lambda-in/src/Components/DevProfile/DevProfile.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 // temporary css
-import './tempDevProp.css';
+import './DevProfile.css';
 
-class individualDevProfile extends React.Component {
-    constructor(props) {
-        super(props);
+class DevProfile extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    state = {
+        firstName: 'Jean Luc',
+        lastName: 'Picard',
+        currentLocation: 'USS Stargazer',
+        summary:'Space, the final frontier. These are the voyages of starship Stargazer.',
+        desiredTitle: 'Captain',
+        github: 'github.com/JeanLucPicard',
+        linkedin: 'linkedin.com/JeanLucPicard',
+        portfolio: 'JeanLucPicard.com',
+        acclaimBadge: 'Lambda Badge',
+        projects: 'Battle of Maxia',
+        experiences: 'Captain of USS Stargazer',
+        education: 'Star Fleet Academy',
+        placesInterested: 'USS Enterprise, Qo\'nos',
     }
 
     render() {
@@ -15,7 +31,7 @@ class individualDevProfile extends React.Component {
                         <div className="top-card-left">
                             <div className="photo">
                                 <img
-                                    src={this.props.seeker.profilePicture}
+                                    src=""
                                     width="150"
                                     height="150"
                                     title="Profile Photo"
@@ -23,28 +39,28 @@ class individualDevProfile extends React.Component {
                             </div>
                             <div classname="nameCard">
                                 <h1 className="name">
-                                    {this.props.seeker.firstName} {this.props.seeker.lastName}
+                                    {this.state.firstName} {this.state.lastName}
                                 </h1>
-                                <h2 className="location">{this.props.seeker.location}</h2>
-                                <p className="description">{this.props.seeker.summary}</p>
+                                <h2 className="location">{this.state.currentLocation}</h2>
+                                <p className="description">{this.state.summary}</p>
                             </div>
                             <hr/>
                             <h1>Desired Title</h1>
                             <hr/>
-                            <h2>{this.props.desiredTitle}</h2>
+                            <h2>{this.state.desiredTitle}</h2>
                             {/* insert tag cloud here */}
                             <div className="tag-cloud"/>
                         </div>
                         <div className="top-card-right"> 
-                            <a href={this.props.seeker.acclaimBadge}>
+                            <a href={this.state.acclaimBadge}>
                                 <img
                                     src=""
                                     width="50"
                                     height="50"
                                     title="Acclaimed Badge"
-                                    alt="Badge"/
-                            ></a>
-                            <a href={this.props.seeker.github}>
+                                    alt="Badge"/>
+                                </a>
+                            <a href={this.state.github}>
                                 <img
                                     src=""
                                     width="50"
@@ -52,7 +68,7 @@ class individualDevProfile extends React.Component {
                                     title="Github"
                                     alt="Github Repo"/>
                             </a>
-                            <a href={this.props.seeker.linkedin}>
+                            <a href={this.state.linkedin}>
                                 <img
                                     src=""
                                     width="50"
@@ -60,7 +76,7 @@ class individualDevProfile extends React.Component {
                                     title="Linkedin"
                                     alt="Linkedin"/>
                             </a>
-                            <a href={this.props.seeker.portfolio}>
+                            <a href={this.state.portfolio}>
                                 <img
                                     src=""
                                     width="50"
@@ -72,16 +88,16 @@ class individualDevProfile extends React.Component {
                     </div>
                     <div classname="profile-body">
                         <h1>Projects</h1>
-                        <p>{this.props.seeker.projects}</p>
+                        <p>{this.state.projects}</p>
                         <hr/>
                         <h1>Experience</h1>
-                        <p>{this.props.seeker.experiences}</p>
+                        <p>{this.state.experiences}</p>
                         <hr/>
                         <h1>Education</h1>
-                        <p>{this.props.seeker.Education}</p>
+                        <p>{this.state.education}</p>
                         <hr/>
                         <h1>Locations Interested</h1>
-                        <p>{this.props.seeker.placesInterested}</p>
+                        <p>{this.state.placesInterested}</p>
                     </div>
                 </div>
             </div>
@@ -89,4 +105,4 @@ class individualDevProfile extends React.Component {
     }
 }
 
-export default individualDevProfile;
+export default DevProfile;
