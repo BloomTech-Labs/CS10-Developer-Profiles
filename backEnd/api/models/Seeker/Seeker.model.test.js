e6c93e9df4d48fbebb5570c93df06c24dea783c0
Danny Vail <danny_vail@outlook.com>
2018-09-12 19:00:45 -0400

    TEST SEEKER MODEL REQUIRED FIELDS In Seeker.model.test.js - Test that firstName, lastName, password, email and desiredTitle are required fields

--- backEnd/api/models/Seeker/Seeker.model.test.js
+++ backEnd/api/models/Seeker/Seeker.model.test.js
@@ -7,13 +7,23 @@ const { testSeeker } = require('../../utils/seeker.testData');
 const { MONGODB_URI_TEST } = process.env;
 
 describe('seeker model', () => {
-  // const testSeeker = {};
-
   beforeAll(() => mongoose.connect(MONGODB_URI_TEST));
 
   afterEach(() => Seeker.remove());
 
   afterAll(() => mongoose.disconnect());
+
+  it('should require firstName, lastName, password, email, desiredTitle', () => {
+    const seeker = new Seeker();
+
+    seeker.validate((err) => {
+      expect(err.errors).toHaveProperty('firstName');
+      expect(err.errors).toHaveProperty('lastName');
+      expect(err.errors).toHaveProperty('password');
+      expect(err.errors).toHaveProperty('email');
+      expect(err.errors).toHaveProperty('desiredTitle');
+    });
+  });
 
   it('should hash password before saving', async () => {
     const newSeeker = await Seeker.create(testSeeker);
