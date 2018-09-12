1b28fe5d6928195f9b82ce3e0068d01ffa6d673e
jesuarva <jesuarva.developer@gmail.com>
2018-09-13 00:47:21 +0200

    REFACTOR Validate required fields

--- backEnd/api/models/Employer/Employer.model.test.js
+++ backEnd/api/models/Employer/Employer.model.test.js
@@ -1,14 +1,20 @@
 // Set envirement variable NODE_ENV to 'test'
 // process.env.NODE_ENV = 'test';
 
+require('dotenv').config();
 const mongoose = require('mongoose');
 const EmployerModel = require('./Employer.model');
+
+const { MONGODB_URI_TEST } = process.env;
 
 describe('Employer Model', () => {
   // Connect to MongoDB
   beforeAll(() => {
-    const connect = mongoose.connect(process.env.MONGODB_URI_TEST);
-    return connect.then(() => console.log('\n=== connected to TEST DB ===')).catch(e => console.log('error', e));
+    const connect = mongoose.connect(MONGODB_URI_TEST);
+    // const connect = mongoose.connect('mongodb://localhost/testdb');
+    return connect
+      .then(() => console.log('\n=== connected to TEST DB ==='))
+      .catch(e => console.log('Error Connecting to DB', e));
   });
 
   // Disconnect DB
@@ -19,28 +25,40 @@ describe('Employer Model', () => {
       .catch(e => console.log('error', e));
   });
 
-  // Test Employer.model.js (Without the Embedded Docs)
-  describe('Employer.model.js', () => {
-    // Create mockup data
-    const requiredFields = {
-      companyName: 'The company inc',
-      companyEmail: 'company@email.com',
-      password: 'super4duper-sercret',
-    };
+  // delete form the DB the data created by each test
+  afterAll(() => {
+    const deletingEmployer = EmployerModel.deleteMany({});
+    return deletingEmployer
+      .then(response => console.log('Deleting document', response))
+      .catch(e => console.log('error', e));
+  });
 
-    // delte form DB the data created by each test
-    afterEach(() => {
-      const deletingEmployer = EmployerModel.remove({});
-      return deletingEmployer
-        .then(response => console.lo('Deleting document', response))
-        .catch(e => console.log('error', e));
-    });
-
+  describe('*** Validate required fields ***', () => {
     test('Validate companyName', () => {
       const newEmployer = EmployerModel.create({
-        // companyName is missing here
+        // companyName: 'The company inc',
         companyEmail: 'company@email.com',
         password: 'super4duper-sercret',
+      });
+
+      return newEmployer.catch(e => expect(e.name).toEqual('ValidationError'));
+    });
+
+    test('Validate companyEmail', () => {
+      const newEmployer = EmployerModel.create({
+        companyName: 'The company inc',
+        // companyEmail: 'company@email.com',
+        password: 'super4duper-sercret',
+      });
+
+      return newEmployer.catch(e => expect(e.name).toEqual('ValidationError'));
+    });
+
+    test('Validate password', () => {
+      const newEmployer = EmployerModel.create({
+        companyName: 'The company inc',
+        companyEmail: 'company@email.com',
+        // password: 'super4duper-sercret',
       });
 
       return newEmployer.catch(e => expect(e.name).toEqual('ValidationError'));
