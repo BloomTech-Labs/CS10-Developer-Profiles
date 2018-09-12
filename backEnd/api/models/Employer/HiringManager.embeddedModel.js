28d8ad878b029851bacf7e73ecef8b257631d45d
Danny Vail <danny_vail@outlook.com>
2018-09-12 14:27:01 -0400

    FIX LINTER ERRORS AND REMOVE UNNECESSARY VARIRABLE In HiringManager.embeddedModel.js - Fix eslint errors   * Add newline after require statements   * Use object destructuring   * Remove return from single statement arrow function - Deleted id from schema -- use _id - Deleted objectID import

--- backEnd/api/models/Employer/HiringManager.embeddedModel.js
+++ backEnd/api/models/Employer/HiringManager.embeddedModel.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const hiringManagerSchema = new Schema({
-  id: { type: ObjectId },
   hiringManagerFirstName: {
     type: String,
     required: [true, 'First name is required'],
@@ -16,9 +15,9 @@ const hiringManagerSchema = new Schema({
     type: String,
     required: [true, "Contact person's email is required"],
     validate: {
-      validator: val => {
-        return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(val);
-      },
+      validator: val => /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(
+        val,
+      ),
       message: '{VALUE} is not a valid email address',
     },
   },
