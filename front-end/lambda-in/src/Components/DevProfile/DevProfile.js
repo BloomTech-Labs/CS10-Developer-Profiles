5fca19b773ecbdd7668d386735499998fde71a40
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-12 16:07:28 -0700

    feat: preparing tagCloud
    - fetch topSkills and addirionalSkills array from seeker.model
    - compare with skills from employer model
    - change the font big and bold if found

--- front-end/lambda-in/src/Components/DevProfile/DevProfile.js
+++ front-end/lambda-in/src/Components/DevProfile/DevProfile.js
@@ -3,31 +3,42 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-// temporary css
 import './DevProfile.css';
 
-
-
-class DevProfile extends Component {
+class DevProfile extends React.Component {
     // constructor(props) {
     //     super(props);
     // }
+    static defaultProps = {
+            firstName: 'Jean Luc',
+            lastName: 'Picard',
+            currentLocation: 'USS Stargazer',
+            summary:'Space, the final frontier. These are the voyages of starship Stargazer.',
+            desiredTitle: 'Captain',
+            github: 'github.com/JeanLucPicard',
+            linkedin: 'linkedin.com/JeanLucPicard',
+            portfolio: 'JeanLucPicard.com',
+            acclaimBadge: 'Lambda Badge',
+            projects: 'Battle of Maxia',
+            experience: 'Captain of USS Stargazer',
+            education: 'Star Fleet Academy',
+            placesInterested: 'USS Enterprise',
 
-    state = {
-        firstName: 'Jean Luc',
-        lastName: 'Picard',
-        currentLocation: 'USS Stargazer',
-        summary:'Space, the final frontier. These are the voyages of starship Stargazer.',
-        desiredTitle: 'Captain',
-        github: 'github.com/JeanLucPicard',
-        linkedin: 'linkedin.com/JeanLucPicard',
-        portfolio: 'JeanLucPicard.com',
-        acclaimBadge: 'Lambda Badge',
-        projects: 'Battle of Maxia',
-        experience: 'Captain of USS Stargazer',
-        education: 'Star Fleet Academy',
-        placesInterested: 'USS Enterprise',
-    }
+            topSkills: ['Javascript', 'C', 'Node JS', 'Python', 'React', 'Redux', 'MongoDB', 'Bootstrap', 'Django', 'Heroku'],
+            additionalSkills: ['MS Office', 'Photoshop', 'VS Code', 'Sublime Text', 'Sketch', 'SketchUp', 'AutoCAD', 'Desalination'],
+
+            skills: ['React', 'MongoDB', 'Heroku']
+        }
+
+    // componentDidMount() {
+    //     fetch(this.props)
+    //     skillCloudHighlighter(state) {
+        
+
+    //     }
+    // }
+
+
 
     render() {
         return (
@@ -60,10 +71,10 @@ class DevProfile extends Component {
                                 </div>
                                 <div className="basicInfo">
                                     <div className="name">
-                                        <h1>{this.state.firstName} {this.state.lastName}</h1>
+                                        <h1>{this.props.firstName} {this.props.lastName}</h1>
                                     </div>
-                                    <h3 className="currentLocation">{this.state.currentLocation}</h3>
-                                    <p className="summary">{this.state.summary}</p>
+                                    <h3 className="currentLocation">{this.props.currentLocation}</h3>
+                                    <p className="summary">{this.props.summary}</p>
                                 </div>
                             </div>
                             <div className="desiredTitle">
@@ -71,14 +82,14 @@ class DevProfile extends Component {
                                     <h2>Desired Title:</h2>
                                 </div>
                                 <div className="title2">
-                                    <h3>{this.state.desiredTitle}</h3>
+                                    <h3>{this.props.desiredTitle}</h3>
                                 </div>
                             </div>
                             {/* insert tag cloud here */}
                             <div className="tagCloud"/>
                         </div>
                         <div className="topCardRight"> 
-                            <a href={this.state.acclaimBadge}>
+                            <a href={this.props.acclaimBadge}>
                                 <img
                                     src=""
                                     width="50"
@@ -86,7 +97,7 @@ class DevProfile extends Component {
                                     title="Acclaimed Badge"
                                     alt="Badge"/>
                                 </a>
-                            <a href={this.state.github}>
+                            <a href={this.props.github}>
                                 <img
                                     src=""
                                     width="50"
@@ -94,7 +105,7 @@ class DevProfile extends Component {
                                     title="Github"
                                     alt="Github Repo"/>
                             </a>
-                            <a href={this.state.linkedin}>
+                            <a href={this.props.linkedin}>
                                 <img
                                     src=""
                                     width="50"
@@ -102,7 +113,7 @@ class DevProfile extends Component {
                                     title="Linkedin"
                                     alt="Linkedin"/>
                             </a>
-                            <a href={this.state.portfolio}>
+                            <a href={this.props.portfolio}>
                                 <img
                                     src=""
                                     width="50"
@@ -115,19 +126,19 @@ class DevProfile extends Component {
                     <div className="profileBody">
                         <div className="projects">
                             <h2>Projects</h2>
-                            <p>{this.state.projects}</p>
+                            <p>{this.props.projects}</p>
                         </div>
                         <div className="experience">
                             <h2>Experience</h2>
-                            <p>{this.state.experience}</p>
+                            <p>{this.props.experience}</p>
                         </div>
                         <div className="education">
                             <h2>Education</h2>
-                            <p>{this.state.education}</p>
+                            <p>{this.props.education}</p>
                         </div>
                         <div className="placesInterested">
                             <h2>Locations Interested</h2>
-                            <p>{this.state.placesInterested}</p>
+                            <p>{this.props.placesInterested}</p>
                         </div>
                     </div>
                 </div>
