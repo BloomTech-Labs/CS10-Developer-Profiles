4dc2b0a8780775e1784b59ed4b2295f72c9afda7
Danny Vail <danny_vail@outlook.com>
2018-09-12 20:17:40 -0400

    RENAME FILE

--- backEnd/api/utils/seeker.testData.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testSeeker: {
-    firstName: 'John',
-    lastName: 'Smith',
-    password: 'Password123&',
-    email: 'jsmith@example.com',
-    desiredTitle: 'Software Engineer',
-  },
-};
