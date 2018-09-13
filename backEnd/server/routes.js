2aba68f0a1c78ababeba7fec27850b6a95297e46
jesuarva <jesuarva.developer@gmail.com>
2018-09-13 19:16:38 +0200

    ADD comments explaining the purpose of each route-endpoint

--- backEnd/server/routes.js
+++ backEnd/server/routes.js
@@ -1,8 +1,11 @@
 const seekersRouter = require('../api/routes/Seeker.router');
 const employerRouter = require('../api/routes/Employer.router');
 
-module.exports = (server) => {
+module.exports = function defineRoutes(server) {
+  // This serve the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
   server.use('/api/seekers', seekersRouter);
+
+  // This serve the Employers DB. It allows GET, POST, PUT and DELETE
   server.use('/api/employers', employerRouter);
 
   server.get('/api', (req, res) => {
