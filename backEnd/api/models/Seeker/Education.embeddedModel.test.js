a678404fa4effa6b020a447955c7ff56f7d654f2
Danny Vail <danny_vail@outlook.com>
2018-09-13 17:47:32 -0400

    ADD THEN/CATCH TO ASYNC FUNCTIONS RETURNED IN SETUP/TEARDOWN In Education.embeddedModel.test.js - Add then/catch statements with console logs to make setup and teardown consistent with Seeker test

--- backEnd/api/models/Seeker/Education.embeddedModel.test.js
+++ backEnd/api/models/Seeker/Education.embeddedModel.test.js
@@ -7,9 +7,21 @@ const { MONGODB_URI_TEST } = process.env;
 const Education = mongoose.model('Education', educationSchema);
 
 describe('education model', () => {
-  beforeAll(() => mongoose.connect(MONGODB_URI_TEST));
+  beforeAll(() => mongoose
+    .connect(
+      MONGODB_URI_TEST,
+      { useNewUrlParser: true },
+    )
+    // eslint-disable-next-line no-console
+    .then(() => console.log('\n*** CONNECTED to database ***\n'))
+    // eslint-disable-next-line no-console
+    .catch(err => console.log('\n*** ERROR connecting to database ***\n', err)));
 
-  afterAll(() => mongoose.disconnect());
+  afterAll(() => mongoose.disconnect()
+    // eslint-disable-next-line no-console
+    .then(() => console.log('\n*** DISCONNECTED from database ***\n'))
+    // eslint-disable-next-line no-console
+    .catch(err => console.log('\n*** ERROR disconnecting from database ***\n', err)));
 
   it('should require school, fieldOfStudy, startYear', () => {
 
