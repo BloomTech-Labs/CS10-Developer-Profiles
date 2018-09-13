374535f907c29042998bacd723c86d3f5caad0e5
Danny Vail <danny_vail@outlook.com>
2018-09-13 18:22:47 -0400

    TEST VALIDATION FOR REQUIRED EXPERIENCE FIELDS In Experience.embeddedModel.test.js - Test that title, company, location and startYear are required fields for Experience

---
+++ backEnd/api/models/Seeker/Experience.embeddedModel.test.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const experienceSchema = require('./Experience.embeddedModel');
+
+const Experience = mongoose.model('Experience', experienceSchema);
+
+describe('Experience Model', () => {
+  it('should require title, company, location, startYear', (done) => {
+    const experience = new Experience();
+
+    experience.validate((err) => {
+      expect(err.errors).toHaveProperty('title');
+      expect(err.errors).toHaveProperty('company');
+      expect(err.errors).toHaveProperty('location');
+      expect(err.errors).toHaveProperty('startYear');
+      done();
+    });
+  });
+});
