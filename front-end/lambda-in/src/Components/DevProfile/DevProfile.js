3ba0a011d6dd93e235f5fb5a30d98969729cea02
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-13 00:53:07 -0700

    feat: images added to determine positions

--- front-end/lambda-in/src/Components/DevProfile/DevProfile.js
+++ front-end/lambda-in/src/Components/DevProfile/DevProfile.js
@@ -6,32 +6,34 @@ import Button from '@material-ui/core/Button';
 import './DevProfile.css';
 
 class DevProfile extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-    static defaultProps = {
+    constructor(props) {
+        super(props);
+    
+    this.state = {
             firstName: 'Jean Luc',
             lastName: 'Picard',
             currentLocation: 'USS Stargazer',
             summary:'Space, the final frontier. These are the voyages of starship Stargazer.',
             desiredTitle: 'Captain',
+            acclaimBadge: 'Lambda Badge',
+            socialNetwork: 'facebook.com',
             github: 'github.com/JeanLucPicard',
             linkedin: 'linkedin.com/JeanLucPicard',
             portfolio: 'JeanLucPicard.com',
-            acclaimBadge: 'Lambda Badge',
             projects: 'Battle of Maxia',
             experience: 'Captain of USS Stargazer',
             education: 'Star Fleet Academy',
             placesInterested: 'USS Enterprise',
 
-            topSkills: ['Javascript', 'C', 'Node JS', 'Python', 'React', 'Redux', 'MongoDB', 'Bootstrap', 'Django', 'Heroku'],
-            additionalSkills: ['MS Office', 'Photoshop', 'VS Code', 'Sublime Text', 'Sketch', 'SketchUp', 'AutoCAD', 'Desalination'],
+            topSkills: ['Leardership', 'Archaeology', 'Literature', 'tlhIngan Hol', 'Mythology', 'Tactical Strategies', 'Navigation', 'Engineering', 'Diplomacy'],
+            additionalSkills: ['Close Combat', 'Knife Combat', 'Phaser', 'Shakespear'],
 
-            skills: ['React', 'MongoDB', 'Heroku']
+            skills: ['Leadership', 'Tactical Strategy', 'Diplomacy']
         }
+    }
 
     // componentDidMount() {
-    //     fetch(this.props)
+    //     fetch(this.state)
     //     skillCloudHighlighter(state) {
         
 
@@ -62,60 +64,66 @@ class DevProfile extends React.Component {
                         <div className="topCardLeft">
                             <div className="nameCard">
                                 <div className="photo">
-                                    <img
-                                        src=""
-                                        width="150"
-                                        height="150"
+                                    <img className="pic"
+                                        src={require('./img/picard.jpg')}
                                         title="Profile Photo"
                                         alt="Photo"/>
                                 </div>
                                 <div className="basicInfo">
                                     <div className="name">
-                                        <h1>{this.props.firstName} {this.props.lastName}</h1>
+                                        <h1>{this.state.firstName} {this.state.lastName}</h1>
                                     </div>
-                                    <h3 className="currentLocation">{this.props.currentLocation}</h3>
-                                    <p className="summary">{this.props.summary}</p>
+                                    <h3 className="currentLocation">{this.state.currentLocation}</h3>
+                                    <p className="summary">{this.state.summary}</p>
                                 </div>
                             </div>
                             <div className="desiredTitle">
-                                <div classname="title1">
+                                <div className="title1">
                                     <h2>Desired Title:</h2>
                                 </div>
                                 <div className="title2">
-                                    <h3>{this.props.desiredTitle}</h3>
+                                    <h3>{this.state.desiredTitle}</h3>
                                 </div>
                             </div>
                             {/* insert tag cloud here */}
                             <div className="tagCloud"/>
                         </div>
                         <div className="topCardRight"> 
-                            <a href={this.props.acclaimBadge}>
+                            <a href={this.state.acclaimBadge}>
                                 <img
-                                    src=""
+                                    src={require('./img/lambdaColor.png')}
                                     width="50"
                                     height="50"
                                     title="Acclaimed Badge"
                                     alt="Badge"/>
                                 </a>
-                            <a href={this.props.github}>
+                            <a href={this.state.socialNetwork}>
                                 <img
-                                    src=""
+                                    src={require('./img/sns.png')}
+                                    width="50"
+                                    height="50"
+                                    title="SNS"
+                                    alt="SNS"/>
+                            </a>
+                            <a href={this.state.github}>
+                                <img
+                                    src={require('./img/GitHub.png')}
                                     width="50"
                                     height="50"
                                     title="Github"
                                     alt="Github Repo"/>
                             </a>
-                            <a href={this.props.linkedin}>
+                            <a href={this.state.linkedin}>
                                 <img
-                                    src=""
+                                    src={require('./img/linkedin.jpg')}
                                     width="50"
                                     height="50"
                                     title="Linkedin"
                                     alt="Linkedin"/>
                             </a>
-                            <a href={this.props.portfolio}>
+                            <a href={this.state.portfolio}>
                                 <img
-                                    src=""
+                                    src={require('./img/portfolio.png')}
                                     width="50"
                                     height="50"
                                     title="Portfolio"
@@ -126,19 +134,19 @@ class DevProfile extends React.Component {
                     <div className="profileBody">
                         <div className="projects">
                             <h2>Projects</h2>
-                            <p>{this.props.projects}</p>
+                            <p>{this.state.projects}</p>
                         </div>
                         <div className="experience">
                             <h2>Experience</h2>
-                            <p>{this.props.experience}</p>
+                            <p>{this.state.experience}</p>
                         </div>
                         <div className="education">
                             <h2>Education</h2>
-                            <p>{this.props.education}</p>
+                            <p>{this.state.education}</p>
                         </div>
                         <div className="placesInterested">
                             <h2>Locations Interested</h2>
-                            <p>{this.props.placesInterested}</p>
+                            <p>{this.state.placesInterested}</p>
                         </div>
                     </div>
                 </div>
