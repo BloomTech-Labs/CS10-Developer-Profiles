6b9ed8ce866f5f2e4b3db75bbca99d6dbbe98f20
samarv <samarvir1996@gmail.com>
2018-09-14 04:57:34 +0530

    added routes

--- backEnd/server/routes.js
+++ backEnd/server/routes.js
@@ -1,9 +1,11 @@
 const seekersRouter = require('../api/routes/Seeker.router');
 const employerRouter = require('../api/routes/Employer.router');
+const seekerLoginRouter = require('../api/routes/login.router')
 
 module.exports = (server) => {
   server.use('/api/seekers', seekersRouter);
   server.use('/api/employers', employerRouter);
+  server.use('/api/login/seekers', seekerLoginRouter )
 
   server.get('/api', (req, res) => {
     res.set('Content-Type', 'application/json');
