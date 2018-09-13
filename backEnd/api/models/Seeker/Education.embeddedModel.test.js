3a1ec21dd49be7f86786f3ae1271257dffab1b0d
Danny Vail <danny_vail@outlook.com>
2018-09-13 17:55:06 -0400

    TEST VALIDATION FOR REQUIRED EDUCATION FIELDS In Education.embeddedModel.test.js - Test that school, fieldOfStudy, startYear are required fields for education

--- backEnd/api/models/Seeker/Education.embeddedModel.test.js
+++ backEnd/api/models/Seeker/Education.embeddedModel.test.js
@@ -1,29 +1,17 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 const educationSchema = require('./Education.embeddedModel');
 
-const { MONGODB_URI_TEST } = process.env;
 const Education = mongoose.model('Education', educationSchema);
 
 describe('education model', () => {
-  beforeAll(() => mongoose
-    .connect(
-      MONGODB_URI_TEST,
-      { useNewUrlParser: true },
-    )
-    // eslint-disable-next-line no-console
-    .then(() => console.log('\n*** CONNECTED to database ***\n'))
-    // eslint-disable-next-line no-console
-    .catch(err => console.log('\n*** ERROR connecting to database ***\n', err)));
+  it('should require school, fieldOfStudy, startYear', (done) => {
+    const education = new Education();
 
-  afterAll(() => mongoose.disconnect()
-    // eslint-disable-next-line no-console
-    .then(() => console.log('\n*** DISCONNECTED from database ***\n'))
-    // eslint-disable-next-line no-console
-    .catch(err => console.log('\n*** ERROR disconnecting from database ***\n', err)));
-
-  it('should require school, fieldOfStudy, startYear', () => {
-
+    education.validate((err) => {
+      expect(err.errors).toHaveProperty('school');
+      expect(err.errors).toHaveProperty('fieldOfStudy');
+      expect(err.errors).toHaveProperty('startYear');
+      done();
+    });
   });
 });
