ee3482389a8e2e045042a6c38fcc06ea62350085
Danny Vail <danny_vail@outlook.com>
2018-09-15 15:14:32 -0400

    UPDATE SERVER MIDDLEWARE TO SERVE STATIC FILES In middleware.js - On production build, serve frontend build directory as static files

--- backend/server/middleware.js
+++ backend/server/middleware.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 module.exports = {
   server(server) {
     server.use(express.json());
-    /*
-      ADD ADDITIONAL EXPRESS MIDDLEWARE
-    */
+
+    // In production build serve frontend build as static files
+    if (process.NODE_ENV === 'production') {
+      server.use(express.static(path.resolve(__dirname, '../../frontend/lambda-in/build')));
+    }
   },
 };
