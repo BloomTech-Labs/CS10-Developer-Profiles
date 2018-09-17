cf2bc099e2426522679f4d50b4397c5b042394bc
jesuarva <jesuarva.developer@gmail.com>
2018-09-18 00:17:02 +0200

    REFACTOR Remove a git commit -m REFACTOR

--- backEnd/api/models/Employer/HiringManager.embeddedModel.test.js
+++ backEnd/api/models/Employer/HiringManager.embeddedModel.test.js
@@ -1,57 +1,30 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const EmployerModel = require('./Employer.model');
+const { testEmployer, testHiringManager } = require('../../utils/testData');
 
 const { MONGODB_URI_TEST } = process.env;
 
 describe('Hiring Manager Model', () => {
-  // Connect to MongoDB
   // prettier-ignore
   beforeAll(() => mongoose
     .connect(
       MONGODB_URI_TEST,
-      // eslint-disable-next-line comma-dangle
-      { useNewUrlParser: true }
-      // A mongoose feature is deprecated, to fix that change it is neccesary to pass this options.
+      { useNewUrlParser: true },
       // eslint-disable-next-line no-console
     ).catch(e => console.log('Error Connecting to DB', e)));
 
-  // Disconnect DB
   // eslint-disable-next-line no-console
   afterAll(() => mongoose.disconnect().catch(e => console.log('Error disconecting from DB', e)));
 
   describe('*** Hiring Manager document is created ***', () => {
-    // Clean DB
-    // eslint-disable-next-line no-console
-    beforeAll(() => EmployerModel.deleteMany({}).catch(e => console.log("ERROR: Couldn't delete data form DB.", e)));
-
-    // Create a new Employer
-    // prettier-ignore
-    beforeEach(() => EmployerModel.create({
-      companyName: 'We hire talent inc',
-      email: 'company@email.com',
-      password: 'Super4duper$sercret',
-      hiringManagers: [
-        {
-          hiringManagerFirstName: 'Manager name',
-          hiringManagerEmail: 'hiring-manager@email.com',
-          hiringManagerLastName: 'Manager last name',
-        },
-      ],
-    }));
-
-    // Delete all entries in DB
-    // eslint-disable-next-line no-console
-    afterEach(() => EmployerModel.deleteMany({}).catch(e => console.log("ERROR: Couldn't delete data form DB.", e)));
-
-    test('should create the hiring manager document', async () => {
-      const doc = await EmployerModel.findOne({ email: 'company@email.com' });
-
+    test('should create the hiring manager document', () => {
+      const doc = new EmployerModel({ ...testEmployer, hiringManagers: testHiringManager });
       expect(doc).toHaveProperty('hiringManagers');
     });
 
     test('should have paths with key-values pairs', async () => {
-      const doc = await EmployerModel.findOne({ email: 'company@email.com' });
+      const doc = new EmployerModel({ ...testEmployer, hiringManagers: testHiringManager });
       const { hiringManagers } = doc;
 
       expect(hiringManagers[0].hiringManagerFirstName).toBe('Manager name');
--- backEnd/api/models/Employer/OpenPosition.embeddedModel.test.js
+++ backEnd/api/models/Employer/OpenPosition.embeddedModel.test.js
@@ -1,56 +1,31 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const EmployerModel = require('./Employer.model');
+const { testEmployer, testOpenPositions } = require('../../utils/testData');
 
 const { MONGODB_URI_TEST } = process.env;
 
 describe('Open Position Model', () => {
-  // Connect to MongoDB
   // prettier-ignore
   beforeAll(() => mongoose
     .connect(
       MONGODB_URI_TEST,
-      // eslint-disable-next-line comma-dangle
-      { useNewUrlParser: true }
-      // A mongoose feature is deprecated, to fix that change it is neccesary to pass this options.
+      { useNewUrlParser: true },
       // eslint-disable-next-line no-console
     ).catch(e => console.log('Error Connecting to DB', e)));
 
-  // Disconnect DB
   // eslint-disable-next-line no-console
   afterAll(() => mongoose.disconnect().catch(e => console.log('Error disconecting from DB', e)));
 
   describe('*** The document is created ***', () => {
-    // Create a new Employer
-    // eslint-disable-next-line no-console
-    beforeAll(() => EmployerModel.deleteMany({}).catch(e => console.log("ERROR: Couldn't delete data form DB.", e)));
-
-    // Create a new Employer
-    // prettier-ignore
-    beforeEach(() => EmployerModel.create({
-      companyName: 'We hire talent inc',
-      email: 'company@email.com',
-      password: 'Super4duper$sercret',
-      openPositions: [
-        {
-          jobTitle: 'Super Developer',
-          projectName: 'Super project',
-        },
-      ],
-    }));
-
-    // Delete all entries in DB
-    // eslint-disable-next-line no-console
-    afterEach(() => EmployerModel.deleteMany({}).catch(e => console.log("ERROR: Couldn't delete data form DB.", e)));
-
     test('should create the open position document', async () => {
-      const doc = await EmployerModel.findOne({ email: 'company@email.com' });
+      const doc = new EmployerModel({ ...testEmployer, openPositions: testOpenPositions });
 
       expect(doc).toHaveProperty('openPositions');
     });
 
     test('should have paths with key-values pairs', async () => {
-      const doc = await EmployerModel.findOne({ email: 'company@email.com' });
+      const doc = new EmployerModel({ ...testEmployer, openPositions: testOpenPositions });
       const { openPositions } = doc;
 
       expect(openPositions[0].jobTitle).toBe('Super Developer');
