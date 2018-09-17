efbf839bae0cc1d0450f27355112b2c18b617f03
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-17 15:34:16 -0700

    feat: added Employer Open Position page

--- front-end/lambda-in/src/Components/EmployerList/EmployerOpenPositionCard.js
+++ front-end/lambda-in/src/Components/EmployerList/EmployerOpenPositionCard.js
@@ -1,45 +1,52 @@
-import React, { Component } from 'react'
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-// import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
-import './EmployerProfileCard.css'
-
-
+import "./EmployerOpenPositionCard.css";
 
 class EmployerOpenPositionCard extends React.Component {
-    state = {
-        companyName: "Star Fleet",
-        companyEmail: "starfleetcommand@starfleet.ufp",
-    
-        hiringManagerFirstName: "Alynna",
-        hiringManagerLastName: "Nechayev",
-        hiringManagerEmail: "nechayev@starfleet.ufp",
-        password: "nechayev-7-alpha",
-        confirmPassword: "nechayev-7-alpha",
-    
-        projectName: "Enterprise-D",
-        description: "We are watching for the captain of the Federation Flagship.",
-        jobTitle: "Captain",
-        techStack: "full stack",
-        skills: ["Diplomacy", "Leadership", "Tactical Strategy"],
-        minSalrary: 1000000,
-        maxSalary: 2000000
-      };
+  state = {
+    projectName: "Enterprise-D",
+    description: "We are watching for the captain of the Federation Flagship.",
+    jobTitle: "Captain",
+    techStack: "full stack",
+    skills: ["Diplomacy", "Leadership", "Tactical Strategy"],
+    minSalary: 1000000,
+    maxSalary: 2000000
+  };
 
-
-    render() {
-        return (
-            <div>
-                <Paper class='container'></Paper>
-                
-            </div>
-       );
-    }
+  render() {
+    return (
+      <div class="container">
+        <Card class="cardContainer">
+          <CardContent>
+            <Typography variant="body1">
+              <br />
+              Project Name: {this.state.projectName}
+              <br />
+              Job Title: {this.state.jobTitle}
+              <br />
+              Tech Stack: {this.state.techStack}
+              <br />
+              Required Skills: {this.state.skills}
+              <br />
+              Salary range: {this.state.minSalary} - {this.state.maxSalary}
+              <br />
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button>Delete</Button>
+            <Button>Edit</Button>
+          </CardActions>
+          <Divider />
+        </Card>
+      </div>
+    );
+  }
 }
 export default EmployerOpenPositionCard;
