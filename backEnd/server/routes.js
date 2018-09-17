412928032c20e2e07f586b1830b94ad2bafeb383
jesuarva <jesuarva.developer@gmail.com>
2018-09-17 22:30:26 +0200

    RESOLVE merge conflict

--- backEnd/server/routes.js
+++ backEnd/server/routes.js
@@ -3,15 +3,9 @@ const employerRouter = require('../api/routes/Employer.router');
 const loginRouter = require('../api/routes/login.router');
 const registerRouter = require('../api/routes/register.router');
 
-<<<<<<< HEAD
-module.exports = function defineRoutes(server) {
-  // This serve the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
-||||||| merged common ancestors
-module.exports = (server) => {
-=======
 // prettier-ignore
 module.exports = (server) => {
->>>>>>> 81d1b4fd39d7d1d01d5240a26d8f68cbb5837f49
+  // This serve the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
   server.use('/api/seekers', seekersRouter);
 
   // This serve the Employers DB. It allows GET, POST, PUT and DELETE
