25cac898ac01c6c5e25cfdea0afdff27e67a7722
Danny Vail <danny_vail@outlook.com>
2018-09-18 12:59:38 -0400

    RESTRICT FRONTEND STATIC FILES TO PRODUCTION AND COMMENT In middleware.js - Only serve frontend build as statuc files in production - Add comments for clarity

--- backend/server/middleware.js
+++ backend/server/middleware.js
@@ -6,6 +6,10 @@ const helmet = require('helmet');
 module.exports = {
   server(server) {
     server.use(express.json());
-    server.use(express.static(path.resolve(__dirname, '../../frontend/lambda-in/build')));
+
+    // In production build, serve frontend build as static files
+    if (process.env.NODE_ENV === 'production') {
+      server.use(express.static(path.resolve(__dirname, '../../frontend/lambda-in/build')));
+    };
   },
 };
