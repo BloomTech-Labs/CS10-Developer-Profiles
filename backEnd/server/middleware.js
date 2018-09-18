866dd30df6fbbc3161bac8b5f89556f91fb5c654
jesuarva <jesuarva.developer@gmail.com>
2018-09-18 21:13:35 +0200

    REFACTOR middleware.js
    ADD a new functionality that allow to have a separate middleware-instance where to
    define middlewares required before restricted endpoints.

--- backEnd/server/middleware.js
+++ backEnd/server/middleware.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const { userHasToken } = require('../api/utils/authUtils');
 
 module.exports = {
-  server(server) {
+  globals(server) {
     server.use(express.json());
     server.use(helmet());
     server.use(cors({}));
+  },
+  auth(server) {
+    // Validate if client has a valid token
+    server.use(userHasToken);
   },
 };
