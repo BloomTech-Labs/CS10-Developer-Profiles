4531e5a03ee1afc8dfd5a50c0a1245cde69f96e8
NathanielFlory <nathanielflory@gmail.com>
2018-09-18 16:18:38 -0400

    Changes to the structure of the billing page based on how stripe works -Added billing options for 1, 3, and 6 months -Stripe receiving API requests and returning token -Removed Radio Buttons for time being

--- front-end/lambda-in/src/Components/Billing/billing.js
+++ front-end/lambda-in/src/Components/Billing/billing.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Checkout from './Stripe/stripe';
 import NavBar from '../../Components/Navbar/navbar';
-import RadioButtonsGroup from './Radio Buttons/radio-buttons'
+//import RadioButtonsGroup from './Radio Buttons/radio-buttons'
+import './billing.css';
 
 class Billing extends React.Component {
     render() {
         return (
             <div>
                 <NavBar />
-                <RadioButtonsGroup />
-                <Checkout />
+                <div className='checkout'>
+                    <div>
+                        <p>1 month - $9.99</p>
+                        <p>3 months - $19.99</p>
+                        <p>6 months - $29.99</p>
+                    </div>
+                    <div className='buttons'>
+                        <Checkout 
+                        name={'Subscription Fee'}
+                        description={'1 month'}
+                        amount={9.99}
+                        />
+                        <Checkout 
+                        name={'Subscription Fee'}
+                        description={'3 months'}
+                        amount={19.99}
+                        />
+                        <Checkout 
+                        name={'Subscription Fee'}
+                        description={'6 months'}
+                        amount={29.99}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
