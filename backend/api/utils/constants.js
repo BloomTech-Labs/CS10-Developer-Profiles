8dc35340fc8261fdad08080057c305390216444e
Danny Vail <danny_vail@outlook.com>
2018-09-20 12:36:06 -0400

    UPDATE CONSTANTS WITH FILTER FIELDS In constants.js - Add filter field maps for fields that will be filtered by value - Reorganize file

--- backend/api/utils/constants.js
+++ backend/api/utils/constants.js
@@ -1,18 +1,32 @@
+const SEEKERS_API_PATH = '/api/seekers';
 const PAGINATION_LIMIT = 10;
+
 const SORT_OPTIONS = {
   default: '_id',
   name: 'lastName',
   location: 'currentLocation',
 };
+
 const FILTER_VALUE = {
   desiredTitle: 'desiredTitle',
   location: 'currentLocation',
 };
-const SEEKERS_API_PATH = '/api/seekers';
+
+const FILTER_BOOLEAN = {
+  github: 'github',
+  linkedin: 'linkedin',
+  portfolio: 'portfolio',
+  resume: 'resume',
+  acclaim: 'acclaimBadge',
+  projects: 'projects',
+  experience: 'experience',
+  education: 'education',
+};
 
 module.exports = {
   PAGINATION_LIMIT,
   SEEKERS_API_PATH,
   SORT_OPTIONS,
   FILTER_VALUE,
+  FILTER_BOOLEAN,
 };
