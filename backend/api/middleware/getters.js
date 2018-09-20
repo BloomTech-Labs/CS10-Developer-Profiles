a6e7028ebac6c3bc6fa5d5dc7cba3441587dafda
Danny Vail <danny_vail@outlook.com>
2018-09-20 11:05:21 -0400

    ADD SORT FUNCTIONALITY TO SEEKERS GET In getters.js - Add sort to Seeker.find() options - getSortOptions() and getSortField() are helper functions to parse requested sort options from GET request

--- backend/api/middleware/getters.js
+++ backend/api/middleware/getters.js
@@ -3,7 +3,7 @@
  */
 const Seeker = require('../models/Seeker/Seeker.model');
 const { sendErr, sendRes } = require('../utils/apiResponses');
-const { PAGINATION_LIMIT, SEEKERS_API_PATH } = require('../utils/constants');
+const { PAGINATION_LIMIT, SORT_OPTIONS, SEEKERS_API_PATH } = require('../utils/constants');
 
 const getNextPage = (currentPage, totalPages) => {
   if (!currentPage || currentPage === 1) return 2;
@@ -21,8 +21,26 @@ const getSkipAmount = (currentPage) => {
   return (page - 1) * PAGINATION_LIMIT;
 };
 
+const getSortField = (sortOption) => {
+  const field = sortOption.startsWith('-') ? sortOption.substring(1) : sortOption;
+  return SORT_OPTIONS[field] ? SORT_OPTIONS[field] : SORT_OPTIONS.default;
+};
+
+const getSortOptions = (sortQuery) => {
+  const query = sortQuery.split(',');
+  const sortOptions = {};
+
+  query.forEach((option) => {
+    const sort = getSortField(option);
+    sortOptions[sort] = option.startsWith('-') ? -1 : 1;
+  });
+
+  return sortOptions;
+};
+
 const getSeekers = (model, req, res) => {
   const { page } = req.query;
+  const sort = req.query.sort || 'default';
 
   Seeker.estimatedDocumentCount()
     .then((count) => {
@@ -30,7 +48,11 @@ const getSeekers = (model, req, res) => {
 
       if (page > pages) return sendErr(res, '404', 'Page number is invalid.');
 
-      Seeker.find({}, null, { skip: getSkipAmount(+page), limit: PAGINATION_LIMIT })
+      Seeker.find({}, null, {
+        sort: getSortOptions(sort),
+        skip: getSkipAmount(+page),
+        limit: PAGINATION_LIMIT,
+      })
         .then((seekers) => {
           const nextPage = getNextPage(+page, pages);
           const prevPage = getPrevPage(+page);
