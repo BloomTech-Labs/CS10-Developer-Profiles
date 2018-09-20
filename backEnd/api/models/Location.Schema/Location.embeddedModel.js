e6db97a57a4b1b22e36f72f4a1a29fa5b559f56f
jesuarva <jesuarva.developer@gmail.com>
2018-09-20 15:30:08 +0200

    MAKE location.schema.place required

--- backEnd/api/models/Location.Schema/Location.embeddedModel.js
+++ backEnd/api/models/Location.Schema/Location.embeddedModel.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const locationSchema = new Schema({
   place: {
     type: String,
+    required: [true, 'A location name is required'],
   },
   loc: {
     type: { type: String },
