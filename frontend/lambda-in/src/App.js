33d9c6bcb1a50302b6a9f4057669a10f139fb1e0
jesuarva <jesuarva.developer@gmail.com>
2018-09-20 21:13:31 +0200

    Add test Router in App.js to test InputGeolocation Component

--- frontend/lambda-in/src/App.js
+++ frontend/lambda-in/src/App.js
@@ -8,32 +8,32 @@ import LandingPage from './Components/Landing Page/landing-page';
 import DevProfile from './Components/DevProfile/DevProfile';
 import Browse from './Components/Browse/browse(employer-view)';
 import Billing from './Components/Billing/billing';
+import InputGeolocation from './Components/InputGeolocation/InputGeolocation';
 
 import DevList from './Components/DevList/DevList';
-
 
 class App extends Component {
   state = {
     isSignedIn: false,
   };
   render() {
-      return (
-        <div>
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/dev-profiles" component={DevProfile} />
-            <Route path="/dev-sigup" component={DevSignUp} />
-            <Route path="/browse-developers" component={Browse} />
-            <Route path="/billing" component={Billing} />
-            <Route path="/dev-list" component={DevList} />
-            {/* For testing purpose */}
-            <Route path="/dev-signup" component={DevSignUp} />
-            <Route path="/dev-login" component={DevLogin} />
-          </Switch>
-        </div>
-      );
-    }
-  
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/dev-profiles" component={DevProfile} />
+          <Route path="/dev-sigup" component={DevSignUp} />
+          <Route path="/browse-developers" component={Browse} />
+          <Route path="/billing" component={Billing} />
+          <Route path="/dev-list" component={DevList} />
+          {/* For testing purpose */}
+          <Route path="/dev-signup" component={DevSignUp} />
+          <Route path="/dev-login" component={DevLogin} />
+          <Route path="/geolocation" component={InputGeolocation} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
