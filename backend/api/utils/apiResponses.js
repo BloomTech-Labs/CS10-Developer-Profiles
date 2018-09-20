eecd44fe0be0efcde417cae5b2fffccfcb1e1844
Danny Vail <danny_vail@outlook.com>
2018-09-19 20:36:23 -0400

    ADD SENDRESP AND SENDERR FUNCTIONS TO UTILITIES In apiResponses.js - Add functions to handle api responses

---
+++ backend/api/utils/apiResponses.js
@@ -0,0 +1,42 @@
+const VALIDATION_ERROR = 'ValidationError';
+const CAST_ERROR = 'CastError';
+const NOT_FOUND = '404';
+const UNAUTHORIZED = '401';
+const FORBIDDEN = '403';
+
+const sendErr = (res, err, message) => {
+  switch (err.name || err) {
+    case VALIDATION_ERROR:
+      res.status(400);
+      res.json({ error: err.message });
+      return;
+    case UNAUTHORIZED:
+      res.status(401);
+      res.json({ error: message });
+      return;
+    case FORBIDDEN:
+      res.status(403);
+      res.json({ error: message });
+      return;
+    case CAST_ERROR:
+      res.status(404);
+      res.json({ error: 'The document with the specified ID does not exist.' });
+      return;
+    case NOT_FOUND:
+      res.status(404);
+      res.json({ error: message });
+      return;
+    default:
+      res.status(500);
+      res.json({ error: message });
+  }
+};
+
+const sendRes = (res, status, resData) => (resData
+  ? res.status(status).json(resData)
+  : res.status(404).json({ error: 'The document with the specified ID does not exist.' }));
+
+module.exports = {
+  sendErr,
+  sendRes,
+};
