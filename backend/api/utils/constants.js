3e9e3426bb105fa5e6d82f69c94028ea5194ed48
Danny Vail <danny_vail@outlook.com>
2018-09-20 11:02:54 -0400

    STORE SEEKER SORT OPTIONS IN CONSTANTS In constants.js - Add an object to hold sort options and mapping for Seeker

--- backend/api/utils/constants.js
+++ backend/api/utils/constants.js
@@ -1,7 +1,13 @@
 const PAGINATION_LIMIT = 10;
+const SORT_OPTIONS = {
+  default: '_id',
+  name: 'lastName',
+  location: 'currentLocation',
+};
 const SEEKERS_API_PATH = '/api/seekers';
 
 module.exports = {
   PAGINATION_LIMIT,
   SEEKERS_API_PATH,
+  SORT_OPTIONS,
 };
