376287bd8ab2493aa2ad5a952f1e6f074f679ad4
jesuarva <jesuarva.developer@gmail.com>
2018-09-20 18:16:59 +0200

    DEFINE Starting InputGeolocation component structure

---
+++ frontend/lambda-in/src/Components/InputGeolocation/InputGeolocation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class InputGeolocation extends Component {
+  state = {
+    place: '',
+    lat: '',
+    log: ',',
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default InputGeolocation;
