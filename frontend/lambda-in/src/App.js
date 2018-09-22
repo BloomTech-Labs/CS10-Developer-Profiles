0081c56a40a1f2061b426d997cbd3338110d1b35
jesuarva <jesuarva.developer@gmail.com>
2018-09-22 18:41:45 +0200

    DEFINE a sample Route with path='/geolocation-test'

--- frontend/lambda-in/src/App.js
+++ frontend/lambda-in/src/App.js
@@ -6,82 +6,39 @@ import DevSignUp from './Components/DevSignUp/DevSignUp';
 import DevLogin from './Components/DevLogIn/DevLogIn';
 import LandingPage from './Components/Landing Page/landing-page';
 import DevProfile from './Components/DevProfile/DevProfile';
-import DevInfoEditz from './Components/DevInfoEditz/DevInfoEditz'
+import DevInfoEditz from './Components/DevInfoEditz/DevInfoEditz';
 import Billing from './Components/Billing/billing';
-<<<<<<< HEAD
-import SearchGeolocation from './Components/InputGeolocation/InputGeolocation';
-||||||| merged common ancestors
-=======
 import EmpSignUp from './Components/EmployerSignUp/EmployerSignUp';
 import EmpList from './Components/EmployerList/EmpOPList';
->>>>>>> ed5eb29e2aff4c5008dba750c8a1afc5bee00214
+import SearchGeolocation from './Components/InputGeolocation/SearchGeolocation';
+import InputGeolocation from './Components/InputGeolocation/InputGeolocation';
 
 import DevList from './Components/DevList/DevList';
 
 class App extends Component {
   state = {
     isSignedIn: false,
-    userInfo: {
-      
-    }
+    userInfo: {},
   };
   render() {
-<<<<<<< HEAD
     return (
-      <div>
+      <div className="TopContainer">
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/dev-profiles" component={DevProfile} />
-          <Route path="/dev-sigup" component={DevSignUp} />
-          <Route path="/browse-developers" component={Browse} />
           <Route path="/billing" component={Billing} />
           <Route path="/dev-list" component={DevList} />
+          <Route path="/emp-signup" component={EmpSignUp} />
+          <Route path="/emp-list" component={EmpList} />
+          <Route path="/dev-info-edit" component={DevInfoEditz} />
           {/* For testing purpose */}
           <Route path="/dev-signup" component={DevSignUp} />
           <Route path="/dev-login" component={DevLogin} />
-          <Route path="/geolocation" component={SearchGeolocation} />
+          <Route path="/geolocation-test" component={SearchGeolocation} />
         </Switch>
       </div>
     );
   }
-||||||| merged common ancestors
-      return (
-        <div>
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/dev-profiles" component={DevProfile} />
-            <Route path="/dev-sigup" component={DevSignUp} />
-            <Route path="/browse-developers" component={Browse} />
-            <Route path="/billing" component={Billing} />
-            <Route path="/dev-list" component={DevList} />
-            {/* For testing purpose */}
-            <Route path="/dev-signup" component={DevSignUp} />
-            <Route path="/dev-login" component={DevLogin} />
-          </Switch>
-        </div>
-      );
-    }
-  
-=======
-      return (
-        <div className="TopContainer">
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/dev-profiles" component={DevProfile} />
-            <Route path="/billing" component={Billing} />
-            <Route path="/dev-list" component={DevList} />
-            <Route path="/emp-signup" component={EmpSignUp} />
-            <Route path="/emp-list" component={EmpList} />
-            <Route path="/dev-info-edit" component={DevInfoEditz} />
-            {/* For testing purpose */}
-            <Route path="/dev-signup" component={DevSignUp} />
-            <Route path="/dev-login" component={DevLogin} />
-          </Switch>
-        </div>
-      );
-    }
-  
->>>>>>> ed5eb29e2aff4c5008dba750c8a1afc5bee00214
 }
 
 export default App;
