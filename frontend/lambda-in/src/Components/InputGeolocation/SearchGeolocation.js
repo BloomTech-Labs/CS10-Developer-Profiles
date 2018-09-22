651f5c859cfe71471cd79e22af950e01ce330059
jesuarva <jesuarva.developer@gmail.com>
2018-09-23 01:34:32 +0200

    IMPLMENT a component to test the InputGeolocation component usage

--- frontend/lambda-in/src/Components/InputGeolocation/SearchGeolocation.js
+++ frontend/lambda-in/src/Components/InputGeolocation/SearchGeolocation.js
@@ -2,9 +2,59 @@ import React from 'react';
 import InputGeolocation from './InputGeolocation';
 
 const SearchGeolocation = () => {
+  const textFieldProps = {
+    id: 'sampleTextField',
+    label: 'Desired Locations',
+    fullWidth: true,
+    margin: 'normal',
+    className: 'textField-class--name another-class--name',
+    //   variant: 'outlined',
+
+    /**
+     * The following are no required
+     *
+     * value: this.state.fieldValue
+     * onChange: {this.handleChange( ... )}
+     */
+  };
+  const listItemProps = {
+    className: 'list-suggestion--options',
+    style: {},
+  };
   return (
     <div>
+      <h1>
+        <strong>RAW COMPONENT</strong>{' '}
+      </h1>
       <InputGeolocation />
+      <hr />
+      <h1>
+        <strong>CUSTOMIZED COMPONENT</strong>{' '}
+      </h1>
+      <InputGeolocation textFieldProps={textFieldProps} listItemProps={listItemProps} />
+      <hr />
+      <h1>
+        <strong>SOME OTHER HTML ELEMENTS</strong>{' '}
+      </h1>
+      <p>
+        Spicy jalapeno bacon ipsum dolor amet in pancetta swine, culpa in veniam frankfurter. Pork chop tempor t-bone,
+        irure nulla filet mignon pork loin cupidatat ut jerky venison et aliquip. Officia veniam swine brisket est
+        leberkas shoulder sausage aliquip proident culpa aute reprehenderit doner consectetur. Capicola biltong doner
+        commodo t-bone, landjaeger lorem officia. Adipisicing chuck officia jowl exercitation tempor laboris et pork
+        picanha magna anim.
+      </p>
+      <p>
+        Short ribs tempor pancetta minim duis dolore consequat dolore jowl. Sed nulla boudin, elit in officia
+        adipisicing eiusmod pastrami. Shoulder salami est pork belly aliqua. Aliquip anim turkey meatball velit
+        tenderloin enim nulla bresaola aliqua rump eu.
+      </p>
+      <p>
+        Sed adipisicing id, eiusmod magna consectetur kevin brisket. Tri-tip anim elit eiusmod swine ut bresaola
+        excepteur chuck lorem salami ham hock. Laboris ea swine veniam beef ut turducken in shoulder anim tri-tip
+        kielbasa turkey eiusmod. Nulla minim shoulder, adipisicing pariatur nisi in. Voluptate sausage irure kevin
+        aliquip biltong. Anim aliquip chicken ipsum aute. Ipsum minim elit fugiat consequat jowl anim deserunt duis
+        meatloaf dolor pig filet mignon chicken cupidatat.
+      </p>
     </div>
   );
 };
