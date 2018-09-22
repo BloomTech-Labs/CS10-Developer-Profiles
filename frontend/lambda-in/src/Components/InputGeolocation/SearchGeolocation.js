369a963decdc4f1c96d4d72f24831ab9a825e03c
jesuarva <jesuarva.developer@gmail.com>
2018-09-22 18:39:49 +0200

    DEFINE a sample component which shows and explains the use of InputGeolocation component

---
+++ frontend/lambda-in/src/Components/InputGeolocation/SearchGeolocation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputGeolocation from './InputGeolocation';
+
+const SearchGeolocation = () => {
+  return (
+    <div>
+      <InputGeolocation />
+    </div>
+  );
+};
+
+export default SearchGeolocation;
