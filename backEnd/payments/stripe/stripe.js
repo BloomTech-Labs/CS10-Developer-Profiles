53558921bc2909b099537d6208ad39d6197c36d2
jesuarva <jesuarva.developer@gmail.com>
2018-09-25 01:37:57 +0200

    REFACTOR backend stripe.js

--- backEnd/payments/stripe/stripe.js
+++ backEnd/payments/stripe/stripe.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const configureStripe = require('stripe');
 
-const { STRIPE_TEST, STRIPE_PRODUCTION } = process.env;
+const { STRIPE_BACKEND_SECRET_KEY_TEST, STRIPE_BACKEND_SECRET_KEY_PRODUCTION } = process.env;
 
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production' ? STRIPE_PRODUCTION : STRIPE_TEST;
+const STRIPE_SECRET_KEY = {
+  production: STRIPE_BACKEND_SECRET_KEY_PRODUCTION,
+  development: STRIPE_BACKEND_SECRET_KEY_TEST,
+}[process.env.NODE_ENV];
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
