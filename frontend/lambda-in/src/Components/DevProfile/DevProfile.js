20e5126fadc79794a5582c2e23ac8fd91d0b4c89
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-24 15:45:45 -0700

    fix: fixing DevProfile layout
    - removed overlap
    - sizing based on viewport

--- frontend/lambda-in/src/Components/DevProfile/DevProfile.js
+++ frontend/lambda-in/src/Components/DevProfile/DevProfile.js
@@ -14,7 +14,7 @@ class DevProfile extends React.Component {
       lastName: "Picard",
       currentLocation: "USS Stargazer",
       summary:
-        "Space, the final frontier. These are the voyages of starship Stargazer.",
+        "Space, the final frontier. These are the voyages of starship Stargazer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ex bibendum, tincidunt tellus a, dignissim ligula. Nunc vitae sollicitudin orci. Duis consequat porttitor lacus. Aliquam erat volutpat. Donec egestas urna in lacinia venenatis. Nullam euismod est imperdiet viverra molestie.",
       desiredTitle: "Captain",
       acclaimBadge: "Lambda Badge",
       socialNetwork: "facebook.com",
@@ -61,7 +61,10 @@ class DevProfile extends React.Component {
     const SS = this.state.skills;
     const allSkills = TS.concat(AS, SS);
     const skillList = allSkills.map((skill, index) => (
-      <div key={index}>{skill}, </div>
+      <div key={index}>
+        {skill}
+        {"  "}
+      </div>
     ));
 
     return (
@@ -69,111 +72,111 @@ class DevProfile extends React.Component {
         <NavBar />
 
         <div className="container">
-          <Paper>
-            <div className="topCard">
-              <div className="topCardLeft">
-                <div className="nameCard">
-                  <div className="photo">
-                    <img
-                      className="pic"
-                      src={require("./img/picard.jpg")}
-                      title="Profile Photo"
-                      alt="Photo"
-                    />
-                  </div>
-                  <div className="basicInfo">
-                    <div className="name">
-                      <Typography variant="headline" component="h3">
-                        {this.state.firstName} {this.state.lastName}
-                      </Typography>
-                    </div>
-                    <div className="currentLocation">
-                      <Typography variant="headline" component="h3">
-                        {this.state.currentLocation}
-                      </Typography>
-                    </div>
-                    <Typography>
-                      <div className="summary">{this.state.summary}</div>
+        <Paper>
+          <div className="topCard">
+            <div className="topCardLeft">
+              <div className="nameCard">
+                <div className="photo">
+                  <img
+                    className="pic"
+                    src={require("./img/picard.jpg")}
+                    title="Profile Photo"
+                    alt="Photo"
+                  />
+                </div>
+                <div className="basicInfo">
+                  <div className="name">
+                    <Typography variant="headline" component="h3">
+                      {this.state.firstName} {this.state.lastName}
                     </Typography>
                   </div>
+                  <div className="currentLocation">
+                    <Typography variant="headline" component="h3">
+                      {this.state.currentLocation}
+                    </Typography>
+                  </div>
+                  <div className="summary">
+                    <Typography>{this.state.summary}</Typography>
+                  </div>
+                  <div className="desiredTitle">
+                  Desired Title: {this.state.desiredTitle}
                 </div>
-                <div className="desiredTitle">
-                  <div className="title1">Desired Title:</div>
-                  <div className="title2">{this.state.desiredTitle}</div>
-                </div>
-                <div className="tagCloud">
-                  {skillList}
+                <div className="tagCloud">{skillList}</div>
                 </div>
               </div>
+              <div className="nameCardDown">
 
-              <div className="topCardRight">
-                <a href={this.state.acclaimBadge}>
-                  <img
-                    src={require("./img/lambdaColor.png")}
-                    width="50"
-                    height="50"
-                    title="Acclaimed Badge"
-                    alt="Badge"
-                  />
-                </a>
-                <a href={this.state.socialNetwork}>
-                  <img
-                    src={require("./img/sns.png")}
-                    width="50"
-                    height="50"
-                    title="SNS"
-                    alt="SNS"
-                  />
-                </a>
-                <a href={this.state.github}>
-                  <img
-                    src={require("./img/GitHub.png")}
-                    width="50"
-                    height="50"
-                    title="Github"
-                    alt="Github Repo"
-                  />
-                </a>
-                <a href={this.state.linkedin}>
-                  <img
-                    src={require("./img/linkedin.jpg")}
-                    width="50"
-                    height="50"
-                    title="Linkedin"
-                    alt="Linkedin"
-                  />
-                </a>
-                <a href={this.state.portfolio}>
-                  <img
-                    src={require("./img/portfolio.png")}
-                    width="50"
-                    height="50"
-                    title="Portfolio"
-                    alt="Portfolio"
-                  />
-                </a>
               </div>
             </div>
-            <br />
-            <div className="profileBody">
-              <div className="projects">
-                <h2>Projects</h2>
-                <p>{this.state.projects}</p>
-              </div>
-              <div className="experience">
-                <h2>Experience</h2>
-                <p>{this.state.experience}</p>
-              </div>
-              <div className="education">
-                <h2>Education</h2>
-                <p>{this.state.education}</p>
-              </div>
-              <div className="placesInterested">
-                <h2>Locations Interested</h2>
-                <p>{this.state.placesInterested}</p>
-              </div>
+
+            <div className="topCardRight">
+              <a href={this.state.acclaimBadge}>
+                <img
+                  src={require("./img/lambdaColor.png")}
+                  width="50"
+                  height="50"
+                  title="Acclaimed Badge"
+                  alt="Badge"
+                />
+              </a>
+              <a href={this.state.socialNetwork}>
+                <img
+                  src={require("./img/sns.png")}
+                  width="50"
+                  height="50"
+                  title="SNS"
+                  alt="SNS"
+                />
+              </a>
+              <a href={this.state.github}>
+                <img
+                  src={require("./img/GitHub.png")}
+                  width="50"
+                  height="50"
+                  title="Github"
+                  alt="Github Repo"
+                />
+              </a>
+              <a href={this.state.linkedin}>
+                <img
+                  src={require("./img/linkedin.jpg")}
+                  width="50"
+                  height="50"
+                  title="Linkedin"
+                  alt="Linkedin"
+                />
+              </a>
+              <a href={this.state.portfolio}>
+                <img
+                  src={require("./img/portfolio.png")}
+                  width="50"
+                  height="50"
+                  title="Portfolio"
+                  alt="Portfolio"
+                />
+              </a>
             </div>
-          </Paper>
+          </div>
+
+          <div className="downCard">
+            <div className="projects">
+              <div>Projects: </div>
+              {this.state.projects}
+            </div>
+            <div className="experience">
+              <div>Experience: </div>
+              {this.state.experience}
+            </div>
+            <div className="education">
+              <div>Education: </div>
+              {this.state.education}
+            </div>
+            <div className="placesInterested">
+              <div>Locations Interested: </div>
+              {this.state.placesInterested}
+            </div>
+          </div>
+        </Paper>
         </div>
       </div>
     );
