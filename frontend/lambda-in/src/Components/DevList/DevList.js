dbd8f22892598eba9b42c9bd7e16fb62952c86f5
Danny Vail <danny_vail@outlook.com>
2018-09-24 16:31:42 -0400

    AUTO FORMAT DEV LIST COMPONENT In DevList.js - Auto formatter ran again, splitting lines that were too long into multi line statements

--- frontend/lambda-in/src/Components/DevList/DevList.js
+++ frontend/lambda-in/src/Components/DevList/DevList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from '@material-ui/core/FormControl';
@@ -6,51 +6,76 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Divider from '@material-ui/core/Divider';
-import DevProfileCard from './DevProfileCard'
+import DevProfileCard from './DevProfileCard';
 
-import '../DevList/DevList.css'
+import '../DevList/DevList.css';
 
 export default class DevList extends Component {
   state = {
     gilad: true,
     jason: false,
-    antoine: false,
+    antoine: false
   };
 
   handleChange = name => event => {
     this.setState({ [name]: event.target.checked });
   };
+
   render() {
     return (
       <div className="App">
-        <div className="mainBar" >
+        <div className="mainBar">
           <div className="sideBar">
             <Typography variant="headline" component="h3">
               Filters
-                        </Typography>
+            </Typography>
             <FormControl>
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="Full Stack Web"
               />
 
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="iOS"
               />
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="Android"
               />
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="UI/UX"
               />
@@ -59,7 +84,13 @@ export default class DevList extends Component {
             <FormControl>
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="Lambda Badge"
               />
@@ -67,37 +98,41 @@ export default class DevList extends Component {
             <Divider />
             <br />
             <FormControl>
-              <Typography variant="headline" component="h3" >
+              <Typography variant="headline" component="h3">
                 Located
-                        </Typography>
+              </Typography>
               <FormControlLabel
                 control={
-                  <Checkbox checked={this.state.gilad} defaultChecked color="default" onChange={this.handleChange('gilad')} value="gilad" />
+                  <Checkbox
+                    checked={this.state.gilad}
+                    defaultChecked
+                    color="default"
+                    onChange={this.handleChange('gilad')}
+                    value="gilad"
+                  />
                 }
                 label="with in 100 mile"
               />
             </FormControl>
 
-            <FormControl >
-              <InputLabel htmlFor="input-with-icon-adornment">search cities</InputLabel>
+            <FormControl>
+              <InputLabel htmlFor="input-with-icon-adornment">
+                search cities
+              </InputLabel>
 
-              <Input
-                id="input-with-icon-adornment"
-                type="search"
-              />
+              <Input id="input-with-icon-adornment" type="search" />
             </FormControl>
             <br />
-            <Typography variant="headline" component="h3" >
+            <Typography variant="headline" component="h3">
               Will Relocate
-                        </Typography>
+            </Typography>
 
-            <FormControl >
-              <InputLabel htmlFor="input-with-icon-adornment">search cities</InputLabel>
+            <FormControl>
+              <InputLabel htmlFor="input-with-icon-adornment">
+                search cities
+              </InputLabel>
 
-              <Input
-                id="input-with-icon-adornment"
-                type="search"
-              />
+              <Input id="input-with-icon-adornment" type="search" />
             </FormControl>
           </div>
           <div className="cardBar">
@@ -107,8 +142,7 @@ export default class DevList extends Component {
             <DevProfileCard />
           </div>
         </div>
-
       </div>
-    )
+    );
   }
 }
