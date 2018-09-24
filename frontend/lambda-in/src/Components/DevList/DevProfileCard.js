17e2e6583d08eb760b0092de92b24cb44e8d9d9e
Danny Vail <danny_vail@outlook.com>
2018-09-24 16:20:04 -0400

    CLEANUP DEV PROFILE CARD COMPONENT In DevProfileCard.js - Remove unused components - Auto formatter cleaned up tabs and new lines

--- frontend/lambda-in/src/Components/DevList/DevProfileCard.js
+++ frontend/lambda-in/src/Components/DevList/DevProfileCard.js
@@ -1,60 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
-import testimage from '../../static/images/testimage.png'
+import testimage from '../../static/images/testimage.png';
 
-import './DevProfileCard.css'
+import './DevProfileCard.css';
 
 export default class DevProfileCard extends Component {
   render() {
     return (
       <div>
-        <Card className = "cardContainer">
-            <div className = "personalInfo">
-                <div className="topCard">
-                    <div className="imageInfo">
-                        <Avatar alt="Remy Sharp" src={testimage} />
-                    </div>
-                    <div className="nameInfo">
-                        <Typography variant="title" >
-                            Samar vir
-                        </Typography>
-                        <Typography variant="subheading">
-                            Delhi, India
-                        </Typography>
-                        <Divider />
-                        <Typography variant="paragraph">
-                            cool boi 101 cool boi 1cool boi 101
-                        </Typography>
-                    </div>
-                </div>
+        <Card className="cardContainer">
+          <div className="personalInfo">
+            <div className="topCard">
+              <div className="imageInfo">
+                <Avatar alt="Remy Sharp" src={testimage} />
+              </div>
+              <div className="nameInfo">
+                <Typography variant="title">Samar vir</Typography>
+                <Typography variant="subheading">Delhi, India</Typography>
                 <Divider />
-                <div className="midCard">
-                    <Typography variant="Headline" component="h4">
-                        Full stack engineer
-                    </Typography>
-                </div>
-                <Divider />
-                <div className="bottomCard">
-                    <Typography variant="paragraph">
-                        python js python js python js python js python js python js python js python js
-                    </Typography>
-                </div>
-
-                
+                <Typography variant="paragraph">
+                  cool boi 101 cool boi 1cool boi 101
+                </Typography>
+              </div>
             </div>
-            <div className= "socialInfo">
-                
+            <Divider />
+            <div className="midCard">
+              <Typography variant="Headline" component="h4">
+                Full stack engineer
+              </Typography>
             </div>
+            <Divider />
+            <div className="bottomCard">
+              <Typography variant="paragraph">
+                python js python js python js python js python js python js
+                python js python js
+              </Typography>
+            </div>
+          </div>
+          <div className="socialInfo" />
         </Card>
       </div>
-    )
+    );
   }
 }
