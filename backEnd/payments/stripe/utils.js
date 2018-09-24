a6743bf7cae291e6169f02bf1de9d1c0e46d6166
jesuarva <jesuarva.developer@gmail.com>
2018-09-25 00:44:31 +0200

    ADD log to track Stripe payment's process state

--- backEnd/payments/stripe/utils.js
+++ backEnd/payments/stripe/utils.js
@@ -2,8 +2,10 @@ module.exports = {
   postStripeCharge(res) {
     return (stripeErr, stripeRes) => {
       if (stripeErr) {
+        console.log({ STRIPE: { Error: stripeErr } });
         res.status(500).send({ Error: stripeErr });
       } else {
+        console.log('STRIPE: Payment procesed');
         res.status(200).send({ Success: stripeRes });
       }
     };
