cf03f0d6df4e87e1a138801322f85d91d0840cd5
Danny Vail <danny_vail@outlook.com>
2018-09-25 10:34:17 -0400

    ADD ICONS TO DEV SOCIAL COMPONENT In DevSocial.js - Add font awesome icons to represent each external link/action

--- frontend/lambda-in/src/Components/DevSocial/DevSocial.js
+++ frontend/lambda-in/src/Components/DevSocial/DevSocial.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import {
+  faCertificate,
+  faFile,
+  faShareAlt
+} from '@fortawesome/free-solid-svg-icons';
 
-const DevSocial = (props) => {
+const styles = {};
+
+const DevSocial = props => {
+  const { classes } = props;
+
   return (
-    <div>Dev Social Links</div>
+    <div className={classes.devSocial}>
+      <List component="ul">
+        <ListItem>
+          <FontAwesomeIcon icon={faCertificate} size="2x" />
+        </ListItem>
+        <ListItem>
+          <FontAwesomeIcon icon={faShareAlt} size="2x" />
+        </ListItem>
+        <ListItem>
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </ListItem>
+        <ListItem>
+          <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+        </ListItem>
+        <ListItem>
+          <FontAwesomeIcon icon={faFile} size="2x" />
+        </ListItem>
+      </List>
+    </div>
   );
-}
- 
-export default DevSocial;
+};
+
+DevSocial.propTypes = {
+  github: PropTypes.string,
+  linkedIn: PropTypes.string,
+  portfolio: PropTypes.string,
+  acclaim: PropTypes.string
+};
+
+export default withStyles(styles)(DevSocial);
