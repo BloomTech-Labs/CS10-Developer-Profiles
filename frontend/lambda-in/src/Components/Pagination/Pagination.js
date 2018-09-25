995b587bf1f8b5e0e5ae357d523456092b0cb858
Danny Vail <danny_vail@outlook.com>
2018-09-25 18:27:53 -0400

    BUILD PAGINATION LINKS USING PAGE LINK RANGE In Pagination.js - Map over page link range and build the pagination links - Add click event handler to call passed in event handler

--- frontend/lambda-in/src/Components/Pagination/Pagination.js
+++ frontend/lambda-in/src/Components/Pagination/Pagination.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -16,7 +17,8 @@ class Pagination extends Component {
       maxPageLinks: PAGINATION_SETTINGS.maxPageLinks,
       lowLimit: PAGINATION_SETTINGS.maxPageLinks - 1,
       highLimit: this.props.pages - PAGINATION_SETTINGS.maxPageLinks + 2,
-      pageLinks: []
+      pageLinks: [],
+      query: '/dev-list'
     };
   }
 
@@ -54,8 +56,8 @@ class Pagination extends Component {
     const pageLinks = [];
     const start =
       this.props.currentPage - Math.floor((this.state.maxPageLinks - 1) / 2);
-    
-    for (let i = start; i < (start + this.state.maxPageLinks); i++) {
+
+    for (let i = start; i < start + this.state.maxPageLinks; i++) {
       pageLinks.push(i);
     }
 
@@ -75,9 +77,32 @@ class Pagination extends Component {
     return this.getMidRangePageLinks();
   };
 
+  handleClick = (event, page) => {
+    this.props.onPageChange(page);
+  };
+
   render() {
     const { classes } = this.props;
-    return <div>Pagination</div>;
+    const pageLinks = this.getPageLinks();
+
+    /**
+     * @todo Make href's dynamic
+     *
+     */
+    return (
+      <ul className={classes.pagination}>
+        {pageLinks.map(page => (
+          <li key={page} className={classes.pageItem}>
+            <Link
+              to={`${this.state.query}?page=${page}`}
+              onClick={event => this.handleClick(event, page)}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
@@ -85,7 +110,7 @@ Pagination.propTypes = {
   count: PropTypes.number.isRequired,
   pages: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(Pagination);
