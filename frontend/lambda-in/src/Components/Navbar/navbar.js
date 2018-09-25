3de945a2670f89d406a9eef3e4a89a2d1484f036
jesuarva <jesuarva.developer@gmail.com>
2018-09-26 00:54:52 +0200

    REFACTOR navbar.js to display links based on 'userType' and ìsSignedIn' states

--- frontend/lambda-in/src/Components/Navbar/navbar.js
+++ frontend/lambda-in/src/Components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch, Link, Redirect, withRouter } from 'react-router-dom';
 //import { Navbar, NavItem, NavDropdown, Nav, MenuItem } from 'react-bootstrap';
 import './navbar.css';
@@ -6,35 +6,63 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import SimpleMenu from '../SignUp Menu/sign-up';
 
-export default props => {
+/**
+ * Functional component responsible of Navigation links
+ *
+ * @prop {function} getGS - Return from APP's global state properties and its values.
+ */
+export default ({ getGS }) => {
+  const isSignedIn = getGS('isSignedIn');
+  const userType = getGS('userType');
+
+  /**
+   * Define Links to be displayed to authorized users
+   */
+  const withToken = {
+    /**
+     * Define links based on 'userType'
+     */
+    leftNavBar: (
+      <Fragment>
+        <Link to={userType === 'seeker' ? '/dev-profiles' : '/emp-profile'}>
+          <Button>profile</Button>
+        </Link>
+        <Link to="/billing">
+          <Button>Billing</Button>
+        </Link>
+      </Fragment>
+    ),
+    rightNavBar: <Button>Log out</Button>,
+  };
+
+  /**
+   * Define Links to be displayed to NON authorized users
+   */
+  const withNotToken = {
+    leftNavBar: <Fragment />, // There are no Links to display
+    rightNavBar: (
+      <Fragment>
+        <SimpleMenu />
+        <Link to="/dev-login">
+          <Button>Log-in</Button>
+        </Link>
+      </Fragment>
+    ),
+  };
+
   return (
     <Paper>
       <div className="navBar">
         <div className="leftNavBar">
           <Link to="/">
-            <Button>find Devlopers</Button>
+            <Button>meetdev</Button>
           </Link>
-          {/* <Link to="/browse-developers"> */}
-          <Link to="/dev-list">
+          <Link to={userType === 'seeker' ? '/meetdev' : '/meetpositions'}>
             <Button>Browse</Button>
           </Link>
-          <Link to="/dev-profiles">
-            <Button>profile</Button>
-          </Link>
-
-          {/* START - Just for test purpose */}
-          {/* <Link to="/billing"> */}
-          <Link to="/billing">
-            <Button>Billing</Button>
-          </Link>
-          {/* END - Just for test purpose */}
+          {isSignedIn ? withToken.leftNavBar : withNotToken.leftNavBar}
         </div>
-        <div className="rightNavBar">
-          <SimpleMenu />
-          <Link to="/dev-login">
-            <Button>Log-in</Button>
-          </Link>
-        </div>
+        <div className="rightNavBar">{isSignedIn ? withToken.rightNavBar : withNotToken.rightNavBar}</div>
       </div>
     </Paper>
   );
