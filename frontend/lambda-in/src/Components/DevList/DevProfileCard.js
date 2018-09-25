8307d35efe3dab7d6b845da962eb83b6b65e40b6
Danny Vail <danny_vail@outlook.com>
2018-09-24 20:58:39 -0400

    BLOCK OUT DYNAMIC DEV PROFILE CARD In DevProfileCard.js - Remove hardcoded developer profile and block out dynamic component - Break dev card up into componentes that can be reused on profile page

--- frontend/lambda-in/src/Components/DevList/DevProfileCard.js
+++ frontend/lambda-in/src/Components/DevList/DevProfileCard.js
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import Divider from '@material-ui/core/Divider';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-
-import testimage from '../../static/images/testimage.png';
+import DevSnapshot from '../DevSnapshot/DevSnapshot';
+import DevSocial from '../DevSocial/DevSocial';
 
 import './DevProfileCard.css';
 
-class DevProfileCard extends Component {
-  render() {
-    return (
-      <div>
-        <Card className="cardContainer">
-          <div className="personalInfo">
-            <div className="topCard">
-              <div className="imageInfo">
-                <Avatar alt="Remy Sharp" src={testimage} />
-              </div>
-              <div className="nameInfo">
-                <Typography variant="title">Samar vir</Typography>
-                <Typography variant="subheading">Delhi, India</Typography>
-                <Divider />
-                <Typography variant="caption">
-                  cool boi 101 cool boi 1cool boi 101
-                </Typography>
-              </div>
-            </div>
-            <Divider />
-            <div className="midCard">
-              <Typography variant="headline" component="h4">
-                Full stack engineer
-              </Typography>
-            </div>
-            <Divider />
-            <div className="bottomCard">
-              <Typography variant="caption">
-                python js python js python js python js python js python js
-                python js python js
-              </Typography>
-            </div>
-          </div>
-          <div className="socialInfo" />
-        </Card>
-      </div>
-    );
-  }
+const styles = {
+
+};
+
+const DevProfileCard = props => {
+  const { classes } = props;
+
+  return (
+    <Card className={classes.devProfileCard}>
+      <DevSnapshot />
+      <DevSocial />
+    </Card>
+  );
 };
 
 DevProfileCard.PropTypes = {
@@ -65,8 +39,8 @@ DevProfileCard.PropTypes = {
     linkedIn: PropTypes.string,
     portfolio: PropTypes.string,
     resume: PropTypes.string,
-    acclaim: PropTypes.string
-  }),
+    acclaim: PropTypes.string,
+  })
 };
 
-export default DevProfileCard;
+export default withStyles(styles)(DevProfileCard);
