024c299bb53a4f1980028b43dc0cd3c3c18c6fbb
Danny Vail <danny_vail@outlook.com>
2018-09-25 17:27:59 -0400

    ADD HELPER FUNCTIONS TO GET PAGE LINK RANGE FOR PAGINATION CONTROLS In Pagination.js - Calculate the page link range, dependent on the current page and total number of pages

--- frontend/lambda-in/src/Components/Pagination/Pagination.js
+++ frontend/lambda-in/src/Components/Pagination/Pagination.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 const PAGINATION_SETTINGS = {
-  maxPageLinks: 6,
+  maxPageLinks: 7,
   pageLimit: 10
 };
 
@@ -16,8 +16,63 @@ class Pagination extends Component {
       maxPageLinks: PAGINATION_SETTINGS.maxPageLinks,
       lowLimit: PAGINATION_SETTINGS.maxPageLinks - 1,
       highLimit: this.props.pages - PAGINATION_SETTINGS.maxPageLinks + 2,
-      pageLinks: [],
+      pageLinks: []
     };
+  }
+
+  getAllPageLinks = () => {
+    const pageLinks = [];
+
+    for (let i = 1; i <= this.props.pages; i++) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  };
+
+  getLowRangePageLinks = () => {
+    const pageLinks = [];
+
+    for (let i = 1; i <= this.state.lowLimit; i++) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  };
+
+  getHighRangePageLinks = () => {
+    const pageLinks = [];
+
+    for (let i = this.state.highLimit; i <= this.props.pages; i++) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  };
+
+  getMidRangePageLinks = () => {
+    const pageLinks = [];
+    const start =
+      this.props.currentPage - Math.floor((this.state.maxPageLinks - 1) / 2);
+    
+    for (let i = start; i < (start + this.state.maxPageLinks); i++) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  };
+
+  getPageLinks = () => {
+    if (this.props.pages <= this.state.maxPageLinks)
+      return this.getAllPageLinks();
+
+    if (this.props.currentPage < this.state.lowLimit)
+      return this.getLowRangePageLinks();
+
+    if (this.props.currentPage > this.state.highLimit)
+      return this.getHighRangePageLinks();
+
+    return this.getMidRangePageLinks();
   };
 
   render() {
