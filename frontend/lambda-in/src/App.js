af5fc275cac0bb101eab4f2534ff2f995f72735e
jesuarva <jesuarva.developer@gmail.com>
2018-09-25 19:15:57 +0200

    FIX merge conflict with frontend app.js

--- frontend/lambda-in/src/App.js
+++ frontend/lambda-in/src/App.js
@@ -6,44 +6,39 @@ import DevSignUp from './Components/DevSignUp/DevSignUp';
 import DevLogin from './Components/DevLogIn/DevLogIn';
 import LandingPage from './Components/Landing Page/landing-page';
 import DevProfile from './Components/DevProfile/DevProfile';
-import DevInfoEditz from './Components/DevInfoEditz/DevInfoEditz'
+import DevInfoEditz from './Components/DevInfoEditz/DevInfoEditz';
 import Billing from './Components/Billing/billing';
 import EmpSignUp from './Components/EmployerSignUp/EmployerSignUp';
 import EmpList from './Components/EmployerList/EmpOPList';
 
-
 import DevList from './Components/DevList/DevList';
-
 
 class App extends Component {
   state = {
     isSignedIn: false,
-    userInfo: {
-      
-    }
+    userInfo: {},
   };
   render() {
-      return (
-        <div>
-          <NavBar />
-          <div className="TopContainer">
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route path="/dev-profiles" component={DevProfile} />
-              <Route path="/billing" component={Billing} />
-              <Route path="/dev-list" component={DevList} />
-              <Route path="/employer-signup" component={EmpSignUp} />
-              <Route path="/employer-list" component={EmpList} />
-              <Route path="/dev-info-edit" component={DevInfoEditz} />
-              {/* For testing purpose */}
-              <Route path="/dev-signup" component={DevSignUp} />
-              <Route path="/dev-login" component={DevLogin} />
-            </Switch>
-          </div>
+    return (
+      <div>
+        <NavBar />
+        <div className="TopContainer">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/dev-profiles" component={DevProfile} />
+            <Route path="/billing" component={Billing} />
+            <Route path="/dev-list" component={DevList} />
+            <Route path="/employer-signup" component={EmpSignUp} />
+            <Route path="/employer-list" component={EmpList} />
+            <Route path="/dev-info-edit" component={DevInfoEditz} />
+            {/* For testing purpose */}
+            <Route path="/dev-signup" component={DevSignUp} />
+            <Route path="/dev-login" component={DevLogin} />
+          </Switch>
         </div>
-      );
-    }
-  
+      </div>
+    );
+  }
 }
 
 export default App;
