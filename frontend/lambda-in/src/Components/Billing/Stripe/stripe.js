9b51d42b1ada10d8b8ba13cf9b968fd89680d9f0
jesuarva <jesuarva.developer@gmail.com>
2018-09-25 02:35:29 +0200

    DOCUMENT frontend stripe file

--- frontend/lambda-in/src/Components/Billing/Stripe/stripe.js
+++ frontend/lambda-in/src/Components/Billing/Stripe/stripe.js
@@ -12,16 +12,49 @@ const PAYMENT_SERVER_URL = '/payments/stripe/';
 const STRIPE_PUBLISHABLE =
   process.env.NODE_ENV === 'production' ? REACT_APP_STRIPE_PUBLIC_KEY_PRODUCTION : REACT_APP_STRIPE_PUBLIC_KEY_TEST;
 
+/**
+ * Convert a float number to an Integer.
+ *
+ * @description Stripe API requires the amount/price to process to be an Integer number.
+ * @param {number} amount - Monetary quantity to process.
+ * @return {number} An integer representing the total quantity to process.
+ * @example
+ * fromRawAmountToCent( 19,99 ) will return '1999'
+ */
 const fromRawAmountToCent = amount => Math.floor(amount * 100);
 
+/**
+ * Action to perfome in transaction success.
+ *
+ * @param {object} data - The axios response object.
+ */
 const successPayment = data => {
   alert('Payment Successful');
 };
 
+/**
+ * Action to perfome in transaction error.
+ *
+ * @param {object} data - The axios response object.
+ */
 const errorPayment = data => {
   alert('Payment Error');
 };
 
+/**
+ * Process payment in Stripe API.
+ *
+ * @function onToken
+ * @param {!number} amount - Payment's total amount to process.
+ * @param {string} description - Product description.
+ * @return {funtion(string):void} - Callback
+ */
+/**
+ * Callback which process the payment through Stripe API
+ *
+ * @callback anonymous function
+ * @param {object} token - Token generated after processesing the credit card details in Stripe API.
+ */
 const onToken = (amount, description) => token =>
   axios
     .post(
@@ -41,6 +74,14 @@ const onToken = (amount, description) => token =>
     .then(successPayment)
     .catch(errorPayment);
 
+/**
+ * React Component constructore.
+ *
+ * @prop {string} name - Product name.
+ * @prop {string} description - Product description.
+ * @prop {!number} amount - Product price.
+ * @return {object}
+ */
 const Checkout = ({ name, description, amount }) => (
   <StripeCheckout
     name={name}
