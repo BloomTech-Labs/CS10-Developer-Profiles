95c03aaaed6d769353b98406461c4d624e184012
Danny Vail <danny_vail@outlook.com>
2018-09-25 17:12:59 -0400

    ADD PAGINATION SETTINGS In Pagination.js - Add settings constant for easy adjusting - Add state during constructor based on pagination settings

--- frontend/lambda-in/src/Components/Pagination/Pagination.js
+++ frontend/lambda-in/src/Components/Pagination/Pagination.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+const PAGINATION_SETTINGS = {
+  maxPageLinks: 6,
+  pageLimit: 10
+};
+
 const styles = {};
 
 class Pagination extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-  }
+    this.state = {
+      maxPageLinks: PAGINATION_SETTINGS.maxPageLinks,
+      lowLimit: PAGINATION_SETTINGS.maxPageLinks - 1,
+      highLimit: this.props.pages - PAGINATION_SETTINGS.maxPageLinks + 2,
+      pageLinks: [],
+    };
+  };
+
   render() {
     const { classes } = this.props;
-    return (
-      <div>Pagination</div>
-    );
+    return <div>Pagination</div>;
   }
 }
 
@@ -21,7 +30,6 @@ Pagination.propTypes = {
   count: PropTypes.number.isRequired,
   pages: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
-  pageLimit: PropTypes.number.isRequired,
   onPageChange: PropTypes.func
 };
 
