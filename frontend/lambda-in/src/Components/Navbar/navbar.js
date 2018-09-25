9c116a048c16ba0da95728a2a323de251f051b6b
jesuarva <jesuarva.developer@gmail.com>
2018-09-25 23:31:36 +0200

    REFACTOR navbar.js to a Functional component

--- frontend/lambda-in/src/Components/Navbar/navbar.js
+++ frontend/lambda-in/src/Components/Navbar/navbar.js
@@ -6,40 +6,36 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import SimpleMenu from '../SignUp Menu/sign-up';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <Paper>
-        <div className="navBar">
-          <div className="leftNavBar">
-            <Link to="/">
-              <Button>find Devlopers</Button>
-            </Link>
-            {/* <Link to="/browse-developers"> */}
-            <Link to="/dev-list">
-              <Button>Browse</Button>
-            </Link>
-            <Link to="/dev-profiles">
-              <Button>profile</Button>
-            </Link>
+export default props => {
+  return (
+    <Paper>
+      <div className="navBar">
+        <div className="leftNavBar">
+          <Link to="/">
+            <Button>find Devlopers</Button>
+          </Link>
+          {/* <Link to="/browse-developers"> */}
+          <Link to="/dev-list">
+            <Button>Browse</Button>
+          </Link>
+          <Link to="/dev-profiles">
+            <Button>profile</Button>
+          </Link>
 
-            {/* START - Just for test purpose */}
-            {/* <Link to="/billing"> */}
-            <Link to="/billing">
-              <Button>Billing</Button>
-            </Link>
-            {/* END - Just for test purpose */}
-          </div>
-          <div className="rightNavBar">
-            <SimpleMenu />
-            <Link to="/dev-login">
-              <Button>Log-in</Button>
-            </Link>
-          </div>
+          {/* START - Just for test purpose */}
+          {/* <Link to="/billing"> */}
+          <Link to="/billing">
+            <Button>Billing</Button>
+          </Link>
+          {/* END - Just for test purpose */}
         </div>
-      </Paper>
-    );
-  }
-}
-
-export default NavBar;
+        <div className="rightNavBar">
+          <SimpleMenu />
+          <Link to="/dev-login">
+            <Button>Log-in</Button>
+          </Link>
+        </div>
+      </div>
+    </Paper>
+  );
+};
