cac5e20f5db7248d43b4d596419cb7d7242c69f7
jesuarva <jesuarva.developer@gmail.com>
2018-09-27 18:42:16 +0200

    IMPLEMENT BioSkills.js component

---
+++ frontend/lambda-in/src/Components/DevInfoEditz/BioSkills.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react';
+import DropDown from '../utilityComponents/DropDown';
+import TextField from '@material-ui/core/TextField';
+
+const BioSkills = props => {
+  const { userInfo } = props;
+
+  return (
+    <Fragment>
+      <DropDown header="tell us something about yourself!">
+        <div className="inputFieldLargeMultiline">
+          <TextField
+            id="summary"
+            label="Your Bio"
+            fullWidth
+            multiline
+            rowsMax="4"
+            value={userInfo.summary}
+            margin="normal"
+            variant="outlined"
+          />
+        </div>
+        <div className="inputFieldLargeMultiline">
+          <TextField
+            id="topSkills"
+            label="Top skills"
+            fullWidth
+            multiline
+            rowsMax="4"
+            value={userInfo.topskill}
+            margin="normal"
+            variant="outlined"
+          />
+        </div>
+      </DropDown>
+    </Fragment>
+  );
+};
+
+export default BioSkills;
