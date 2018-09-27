406fa655ad72b84c10ac1499dc9869e38017bfd7
samarv <samarvir1996@gmail.com>
2018-09-27 22:02:53 +0530

    createa dnew component for devprofile

---
+++ frontend/lambda-in/src/Components/DevProfile2/DevProfile2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import './DevProfile2.css'
+export default class DevProfile2 extends Component {
+  render() {
+    return (
+      <div className="profileContainer">
+        <Paper className="profilePaper">
+          
+        </Paper>
+      </div>
+    )
+  }
+}
