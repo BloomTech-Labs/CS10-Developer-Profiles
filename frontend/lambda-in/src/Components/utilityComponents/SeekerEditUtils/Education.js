002d2a641437c98660be907126526b4c4906f268
jesuarva <jesuarva.developer@gmail.com>
2018-09-27 23:43:42 +0200

    ADD  a nested DropDown component in Education.js

--- frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Education.js
+++ frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Education.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import DropDown from '../DropDown/DropDown';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 
 const Education = props => {
   const { userInfo } = props;
@@ -8,44 +9,46 @@ const Education = props => {
   return (
     <Fragment>
       <DropDown header="Education">
-        <div className="inputFieldLargeMultiline">
-          <TextField
-            id="projectTitle"
-            label="Project Title"
-            fullWidth
-            multiline
-            rowsMax="4"
-            value={userInfo.projectTitle}
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
+        <Paper className="paperContainer">
+          <DropDown header="School 1">
+            <div className="inputFieldLargeMultiline">
+              <TextField
+                id="education.school"
+                label="School"
+                fullWidth
+                value={userInfo.education.school}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
 
-        <div className="inputFieldLargeMultiline">
-          <TextField
-            id="projectLink"
-            label="Project Link"
-            fullWidth
-            multiline
-            rowsMax="4"
-            value={userInfo.projectLink}
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
+            <div className="inputFieldLargeMultiline">
+              <TextField
+                id="projectLink"
+                label="Project Link"
+                fullWidth
+                multiline
+                rowsMax="4"
+                value={userInfo.projectLink}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
 
-        <div className="inputFieldLargeMultiline">
-          <TextField
-            id="projectRepo"
-            label="Project Repo"
-            fullWidth
-            multiline
-            rowsMax="4"
-            value={userInfo.projectRepo}
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
+            <div className="inputFieldLargeMultiline">
+              <TextField
+                id="projectRepo"
+                label="Project Repo"
+                fullWidth
+                multiline
+                rowsMax="4"
+                value={userInfo.projectRepo}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
+          </DropDown>
+        </Paper>
       </DropDown>
     </Fragment>
   );
