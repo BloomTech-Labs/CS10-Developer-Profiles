d669bd91089d6a2f9028191e3a05793a16dbe0d9
samarv <samarvir1996@gmail.com>
2018-09-27 22:13:51 +0530

    styled the topCard

--- frontend/lambda-in/src/Components/DevProfile2/DevProfile2.js
+++ frontend/lambda-in/src/Components/DevProfile2/DevProfile2.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
+import MapList from '../MapList/MapList';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import './DevProfile2.css'
-export default class DevProfile2 extends Component {
-  render() {
+export default ({ getGS }) => {
+  const userInfo = getGS('userInfo');
+
+  const TS = userInfo.topSkills;
+  const AS = userInfo.additionalSkills;
+  const SS = userInfo.skills;
+  const allSkills = TS.concat(AS, SS);
+  const skillList = allSkills.map(skill => (
+    <li>
+      {skill}
+      {` `}
+    </li>
+  ));
+
     return (
       <div className="profileContainer">
         <Paper className="profilePaper">
-          
+          <div className="profileTopCard">
+            <div className="profileImage">
+                <img className="profileImage" src={`https://robohash.org/tony`} alt="Italian " />
+            </div>
+            <div className="profileBio">
+              <div className="basicInfo">
+                  <div className="name">
+                    <Typography variant="headline" component="h3">
+                      {userInfo.firstName} {userInfo.lastName}
+                    </Typography>
+                  </div>
+                  <div className="currentLocation">
+                    <Typography variant="headline" component="h3">
+                      {userInfo.currentLocation}
+                    </Typography>
+                  </div>
+                  <div className="summary">
+                    <Typography>{userInfo.summary}</Typography>
+                  </div>
+                  <div className="desiredTitle">Desired Title: {userInfo.desiredTitle}</div>
+                  <div className="tagCloud">
+                    <MapList array={allSkills} />
+                  </div>
+                </div>
+            </div>
+            <div className="profileLinks">
+              <a href={userInfo.acclaimBadge}>
+                  <img
+                    src={require('./img/lambdaColor.png')}
+                    className="profileLinksIcon"
+                    title="Acclaimed Badge"
+                    alt="Badge"
+                  />
+                </a>
+                <a href={userInfo.github}>
+                  <img src={require('./img/GitHub.png')} className="profileLinksIcon" title="Github" alt="Github Repo" />
+                </a>
+                <a href={userInfo.linkedin}>
+                  <img src={require('./img/linkedin.jpg')} className="profileLinksIcon" title="Linkedin" alt="Linkedin" />
+                </a>
+                <a href={userInfo.portfolio}>
+                  <img src={require('./img/portfolio.png')} className="profileLinksIcon" title="Portfolio" alt="Portfolio" />
+                </a>
+            </div>
+          </div>
+          <div className="profileBottomCard">
+
+          </div>
         </Paper>
       </div>
     )
   }
-}
+
