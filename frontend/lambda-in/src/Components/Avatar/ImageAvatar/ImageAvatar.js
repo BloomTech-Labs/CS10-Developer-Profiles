b1acf5f7bb722e34589e4e9a7c459c309a0aa1fd
Danny Vail <danny_vail@outlook.com>
2018-09-28 11:38:28 -0400

    MODIFY COMMENTS AND PROP NAME In ImageAvatar.js - Changed use of @prop to @param to describe data being passed into Component - Changed prop name to avatarName since name is an HTML attribute

--- frontend/lambda-in/src/Components/Avatar/ImageAvatar/ImageAvatar.js
+++ frontend/lambda-in/src/Components/Avatar/ImageAvatar/ImageAvatar.js
@@ -6,10 +6,11 @@ import Avatar from '@material-ui/core/Avatar';
 const styles = {};
 
 /**
- * Given a name and an image, return a Material UI Avatar component formatted as an image
+ * Given a name and an image, return a Material UI Avatar component formatted as an image.
  *
- * @prop {String} name - Avatar name to be used as the alt attribute for rendered `img` element.
- * @prop {String} img - Image path to be used as the src attribute for the `img` element.
+ * @param {Object} props - Component properties.
+ * @param {String} props.avatarName - Avatar name to be used as the alt attribute for rendered `img` element.
+ * @param {String} props.img - Image path to be used as the src attribute for the `img` element.
  * @return {Component} Material UI Avatar component.
  *
  * @see {@link https://material-ui.com/demos/avatars/ } for a demo of the Avatar component.
@@ -18,12 +19,12 @@ const styles = {};
 const ImageAvatar = props => {
   const { classes } = props;
   return (
-    <Avatar className={classes.imageAvatar} alt={props.name} src={props.img} />
+    <Avatar className={classes.imageAvatar} alt={props.avatarName} src={props.img} />
   );
 };
 
 ImageAvatar.propTypes = {
-  name: PropTypes.string.isRequired,
+  avatarName: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired
 };
 
