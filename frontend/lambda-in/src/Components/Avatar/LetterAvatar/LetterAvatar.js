6124a551055e52fd149a6e3fb13e7e0c8337e5c6
Danny Vail <danny_vail@outlook.com>
2018-09-28 11:23:02 -0400

    MOVE EACH AVATAR COMPONENT TO ITS OWN FOLDER In /ImageAvatar /LetterAvatar - Create new folders for each avatar component in preperation for documentation

---
+++ frontend/lambda-in/src/Components/Avatar/LetterAvatar/LetterAvatar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+const styles = {
+  letterAvatar: {
+    backgroundColor: '#a0021e',
+  }
+};
+
+const LetterAvatar = props => {
+  const { classes } = props;
+  return <Avatar className={classes.letterAvatar}>{props.content}</Avatar>;
+};
+
+LetterAvatar.propTypes = {
+  content: PropTypes.string.isRequired
+};
+
+export default withStyles(styles)(LetterAvatar);
