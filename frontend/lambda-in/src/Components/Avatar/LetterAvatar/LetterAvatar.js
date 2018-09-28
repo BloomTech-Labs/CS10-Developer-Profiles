26a0e34f1b1d0cbf43daab463d5167c75cbcd569
Danny Vail <danny_vail@outlook.com>
2018-09-28 12:57:36 -0400

    ADD COMMENTS TO THE LETTER AVATAR COMPONENT

--- frontend/lambda-in/src/Components/Avatar/LetterAvatar/LetterAvatar.js
+++ frontend/lambda-in/src/Components/Avatar/LetterAvatar/LetterAvatar.js
@@ -5,10 +5,20 @@ import Avatar from '@material-ui/core/Avatar';
 
 const styles = {
   letterAvatar: {
-    backgroundColor: '#a0021e',
+    backgroundColor: '#a0021e'
   }
 };
 
+/**
+ * Given a string, return a Material UI Avatar component formatted with simple characters.
+ *
+ * @param {Object} props - Component properties.
+ * @param {String} props.content - Characters to render inside the Avatar.
+ * @return {Component} Material UI Avatar component.
+ *
+ * @see {@link https://material-ui.com/demos/avatars/#letter-avatars } for a demo of the Letter Avatar component.
+ * @see {@link https://material-ui.com/api/avatar/ } for the Avatar API.
+ */
 const LetterAvatar = props => {
   const { classes } = props;
   return <Avatar className={classes.letterAvatar}>{props.content}</Avatar>;
