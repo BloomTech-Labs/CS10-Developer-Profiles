271025f2c159fd29c6702cfd59ec966df5e13a11
jesuarva <jesuarva.developer@gmail.com>
2018-09-28 23:20:52 +0200

    COMMENT and explain methods in DevLogin.js

--- frontend/lambda-in/src/Components/DevLogIn/DevLogIn.js
+++ frontend/lambda-in/src/Components/DevLogIn/DevLogIn.js
@@ -22,6 +22,12 @@ export default class DevLogin extends Component {
     };
   }
 
+  /**
+   * Set global state with user data.
+   *
+   * @param {object} httpResponse - Axios http response.
+   * @param {string} userType - Type of user
+   */
   handleAxios(httpResponse, userType) {
     console.log(httpResponse, userType);
     localStorage.setItem('token', httpResponse.data.jwt);
@@ -37,9 +43,20 @@ export default class DevLogin extends Component {
       userInfo: { ...httpResponse.data.user }, // Set user data.
       isSignedIn: true,
       userType,
+      login: false,
     });
   }
 
+  /**
+   * Decide whcih type of user to login.
+   *
+   * @description Decide to which DB model a user belong.
+   * If the user has an account in both model enable a feature
+   * that allow the user to chose which model to use to login.
+   *
+   * @param {event} event - Even object
+   */
+  /* eslint-disable */
   handleLogin = async event => {
     event.preventDefault();
 
@@ -48,11 +65,18 @@ export default class DevLogin extends Component {
       password: this.state.password,
     };
 
+    /**
+     * Validate credential in both endpoints ('seeker' and 'employers')
+     */
     const seekersResponse = await axios.post('/api/login/seekers', loginData);
     const employersResponse = await axios.post('/api/login/employers', loginData);
 
-    // If both users are in the DB set set local state 'user' to 'conflic'
-    // 'conflict' will display a UI feature to resolve the conflict.
+    /**
+     * If both users are in the DB set set global's 'login' state to 'conflic'
+     * then save both http response in the local-state
+     *
+     * 'login' === 'conflict' will display a UI feature to resolve the conflict.
+     */
     if (seekersResponse.status === 200 && employersResponse.status === 200) {
       console.log('CONFLICT');
       this.setState({
@@ -64,25 +88,32 @@ export default class DevLogin extends Component {
       return;
     }
 
-    // If success only loginin 'seeker' process response and 'return'
+    /**
+     * If success loginin as a 'seeker', process response and 'return'
+     */
     if (seekersResponse.status === 200) {
       this.handleAxios(seekersResponse, 'seeker');
       return;
     }
 
-    // If success only loginin 'employer' process response and 'return'
+    /**
+     * If success loginin as an 'employer', process response and 'return'
+     */
     if (employersResponse.status === 200) {
       this.handleAxios(employersResponse, 'employer');
       return;
     }
 
-    // If any Error reset password field
+    /**
+     * If any Error reset password field
+     */
     this.setState({ password: '' });
     alert('Error with your credential'); // TODO: Improve UX
 
     console.log({ 'HTTP login seekersResponse status': seekersResponse.status });
     console.log({ 'HTTP login employersResponse status': employersResponse.status });
   };
+  /* eslint-enable */
 
   handleChange = event => {
     this.setState({
@@ -90,6 +121,12 @@ export default class DevLogin extends Component {
     });
   };
 
+  /**
+   * Set APP's global state according to user choise
+   *
+   * @param {string} userType - The type of profile to login.
+   * @return {void}
+   */
   resolveUserConflic = userType => {
     console.log();
     if (userType === 'seeker') {
