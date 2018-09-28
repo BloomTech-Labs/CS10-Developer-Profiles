5dcc43e76982c416eb272cad6a3cdcfe46ce9ff8
Danny Vail <danny_vail@outlook.com>
2018-09-28 11:20:51 -0400

    ADD COMMENTS TO IMAGE AVATAR COMPONENT

--- frontend/lambda-in/src/Components/Avatar/ImageAvatar.js
+++ frontend/lambda-in/src/Components/Avatar/ImageAvatar.js
@@ -5,9 +5,21 @@ import Avatar from '@material-ui/core/Avatar';
 
 const styles = {};
 
+/**
+ * Given a name and an image, return a Material UI Avatar component formatted as an image
+ *
+ * @prop {String} name - Avatar name to be used as the alt attribute for rendered `img` element.
+ * @prop {String} img - Image path to be used as the src attribute for the `img` element.
+ * @return {Component} Material UI Avatar component.
+ *
+ * @see {@link https://material-ui.com/demos/avatars/ } for a demo of the Avatar component.
+ * @see {@link https://material-ui.com/api/avatar/ } for the Avatar API.
+ */
 const ImageAvatar = props => {
   const { classes } = props;
-  return <Avatar className={classes.imageAvatar} alt={props.name} src={props.img} />;
+  return (
+    <Avatar className={classes.imageAvatar} alt={props.name} src={props.img} />
+  );
 };
 
 ImageAvatar.propTypes = {
