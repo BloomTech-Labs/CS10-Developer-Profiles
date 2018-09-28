ae76a50a9f789ab75d00a4ff6296fd58f82fb466
Danny Vail <danny_vail@outlook.com>
2018-09-28 13:43:51 -0400

    REFACTOR SKILL CLOUD TO FIX SKILL RESHUFFLE ON EACH RENDER In SkillCloud.js - Change SKillCloud to class component - Remove shuffleArray function and sort skills by alphabetical order - Destructure state in render - Auto formatter

--- frontend/lambda-in/src/Components/SkillCloud/SkillCloud.js
+++ frontend/lambda-in/src/Components/SkillCloud/SkillCloud.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
@@ -21,53 +21,55 @@ const styles = {
   }
 };
 
-const shuffleArray = array => {
-  let index = array.length;
-  let temp, randomIndex;
-
-  while (index !== 0) {
-    randomIndex = Math.floor(Math.random() * index);
-    index -= 1;
-
-    temp = array[index];
-    array[index] = array[randomIndex];
-    array[randomIndex] = temp;
+class SkillCloud extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skills: this.combineSkills().sort((a, b) => (a.skill > b.skill ? 1 : -1))
+    };
   }
 
-  return array;
-};
+  combineSkills = () => {
+    const { classes } = this.props;
+    const skills = [];
 
-const SkillCloud = props => {
-  const { classes } = props;
-  const skills = [];
+    this.props.topSkills.forEach(skill =>
+      skills.push({ type: 'topSkills', class: classes.top, skill })
+    );
 
-  props.topSkills.forEach(skill =>
-    skills.push({ type: 'topSkills', class: classes.top, skill })
-  );
+    this.props.addSkills.forEach(skill =>
+      skills.push({ type: 'addSkills', class: classes.additional, skill })
+    );
 
-  props.addSkills.forEach(skill =>
-    skills.push({ type: 'addSkills', class: classes.additional, skill })
-  );
+    this.props.familiar.forEach(skill =>
+      skills.push({ type: 'familiar', class: classes.familiar, skill })
+    );
 
-  props.familiar.forEach(skill =>
-    skills.push({ type: 'familiar', class: classes.familiar, skill })
-  );
+    return skills;
+  };
 
-  const shuffledSkills = shuffleArray(skills);
+  render() {
+    const { classes } = this.props;
+    const { skills } = this.state;
 
-  return (
-    <div className="skill-cloud">
-      {shuffledSkills.map((skill, index) => (
-        <Typography
-          key={index}
-          className={classnames(classes.skill, skill.class)}
-        >
-          <Link to={`/dev-list?${skill.type}=${skill.skill.replace(/ /g, '+')}`}>{skill.skill}</Link>
-        </Typography>
-      ))}
-    </div>
-  );
-};
+    return (
+      <div className="skill-cloud">
+        {skills.map((skill, index) => (
+          <Typography
+            key={index}
+            className={classnames(classes.skill, skill.class)}
+          >
+            <Link
+              to={`/dev-list?${skill.type}=${skill.skill.replace(/ /g, '+')}`}
+            >
+              {skill.skill}
+            </Link>
+          </Typography>
+        ))}
+      </div>
+    );
+  }
+}
 
 SkillCloud.propTypes = {
   topSkills: PropTypes.array,
