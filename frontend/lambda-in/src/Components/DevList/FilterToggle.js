5d3647ca1380b54cca36411575e4f0eea7a0d570
Danny Vail <danny_vail@outlook.com>
2018-09-28 15:57:22 -0400

    ADD COMMENTS TO THE FILTER TOGGLE COMPONENT

--- frontend/lambda-in/src/Components/DevList/FilterToggle.js
+++ frontend/lambda-in/src/Components/DevList/FilterToggle.js
@@ -9,6 +9,28 @@ import { ENABLE, DISABLE } from '../constants';
 
 const styles = {};
 
+/**
+ * Given filter data and event handlers for onClick and onChange, return a togglable filter comporised
+ * of a Material UI FormGroup component containing a Material UI Button and Switch component.
+ *
+ * @param {Object} props - Component properties.
+ * @param {Object} props.filter - Filter data.
+ * @param {String} props.fiter.label - Display name for filter toggle.
+ * @param {String} props.filter.name - Name assigned to Switch component for event targetting.
+ * @param {String} props.filter.toggleName - Name assigned to Button component for event targetting.
+ * @param {Boolean} props.checked - True/False if Switch component is checked.
+ * @param {Boolean} props.enable - True/False if Filter Toggle is enabled.
+ * @param {Function} props.onCheck - Switch component on change event handler.
+ * @param {Function} props.onEnable - Button component on click event handler.
+ * @return {Component} Material UI Avatar component.
+ *
+ * @see {@link https://material-ui.com/api/button/ } for the Button API.
+ * @see {@link https://material-ui.com/api/form-control-label/ } for the FromControlLabel API.
+ * @see {@link https://material-ui.com/api/form-group/ } for the FormGroup API.
+ * @see {@link https://material-ui.com/api/switch/ } for the Switch API.
+ * @see {@link https://material-ui.com/demos/selection-controls/#switches-with-formcontrollabel } for a
+ * demo of the Switch Component.
+ */
 const FilterToggle = props => {
   return (
     <FormGroup row>
