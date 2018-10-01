e3564ea2f72f46af2afef0691246c20510fdb282
Danny Vail <danny_vail@outlook.com>
2018-10-01 18:36:22 -0400

    FIX LINT ERRORS In Pagination.js - Fix lint errors including:   * Prefer normal functions over arrow functions   * Shorten long lines   * Destructure props and state before usage   * Remove unused state and props   * Replauce unuary operator ++ with +=   * Add default props   * Add classes to propTypes

--- frontend/lambda-in/src/Components/Pagination/Pagination.js
+++ frontend/lambda-in/src/Components/Pagination/Pagination.js
@@ -15,7 +15,8 @@ const styles = {};
  * @prop {Integer} lowLimit - Tha max point the current page can be at to generate a low page range.
  * @example
  *  Ex: If current page is less than low limit, the page range will always be 1,2,3,4,5,6
- * @prop {Integer} highLimit - The min point the current page can be at to generate a high page range.
+ * @prop {Integer} highLimit - The min point the current page can be at to generate a high page
+ * range.
  * @example
  *  Ex: If current page is more than high limit, the page range will always be 34,35,36,37,38,39
  * @prop {Array} pageLinks - Range of page links to generate.
@@ -27,7 +28,6 @@ class Pagination extends Component {
    * Create a Pagination component
    *
    * @param {Object} props - Component properties.
-   * @param {Integer} props.count - The total number of records being displayed.
    * @param {Integer} props.pages - The total number of pages that should exist.
    * @param {Integer} props.currentPage - The current page number.
    * @param {String} props.pathName - The current page url pathname.
@@ -39,12 +39,99 @@ class Pagination extends Component {
    */
   constructor(props) {
     super(props);
+    const { pages } = this.props;
     this.state = {
       maxPageLinks: PAGINATION_SETTINGS.maxPageLinks,
       lowLimit: PAGINATION_SETTINGS.maxPageLinks - 1,
-      highLimit: this.props.pages - PAGINATION_SETTINGS.maxPageLinks + 2,
-      pageLinks: [],
+      highLimit: pages - PAGINATION_SETTINGS.maxPageLinks + 2,
     };
+  }
+
+  /**
+   * Generate a range from 1 to the total number of pages and return the list in an array.
+   *
+   * @return {Array} Array with page range.
+   */
+  getAllPageLinks() {
+    const { pages } = this.props;
+    const pageLinks = [];
+
+    for (let i = 1; i <= pages; i += 1) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  }
+
+  /**
+   * Generate a range from 1 to the lowLimit and return the list in an array.
+   *
+   * @return {Array} Array with page range.
+   */
+  getLowRangePageLinks() {
+    const { lowLimit } = this.state;
+    const pageLinks = [];
+
+    for (let i = 1; i <= lowLimit; i += 1) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  }
+
+  /**
+   * Generate a range from the highLimit to the total number of pages and return the list in an
+   * array.
+   *
+   * @return {Array} Array with page range.
+   */
+  getHighRangePageLinks() {
+    const { highLimit } = this.state;
+    const { pages } = this.props;
+    const pageLinks = [];
+
+    for (let i = highLimit; i <= pages; i += 1) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  }
+
+  /**
+   * Determine the start point for the page range and generate a range from the start to the max
+   * number of page linkes and return list in an array.
+   *
+   * @return {Array} Array with page range.
+   */
+  getMidRangePageLinks() {
+    const { maxPageLinks } = this.state;
+    const { currentPage } = this.props;
+    const pageLinks = [];
+    const start = currentPage - Math.floor((maxPageLinks - 1) / 2);
+
+    for (let i = start; i < start + maxPageLinks; i += 1) {
+      pageLinks.push(i);
+    }
+
+    return pageLinks;
+  }
+
+  /**
+   * Using the passed in props pages and currentPage and the maxPageLinks, lowLimit and highLimit
+   * on the state, determine which method to call to generate the appropriate page range. Return
+   * the page range as an array of integers.
+   *
+   * @return {Array} Array with page range
+   */
+  getPageLinks() {
+    const { maxPageLinks, lowLimit, highLimit } = this.state;
+    const { pages, currentPage } = this.props;
+
+    if (pages <= maxPageLinks) return this.getAllPageLinks();
+    if (currentPage < lowLimit) return this.getLowRangePageLinks();
+    if (currentPage > highLimit) return this.getHighRangePageLinks();
+
+    return this.getMidRangePageLinks();
   }
 
   /**
@@ -55,100 +142,11 @@ class Pagination extends Component {
    *  'page=2&desiredTitle=Front+End|Back+End'
    * @return {String} Query string with page parameter removed.
    */
-  cleanQuery = query => {
-    const cleanQuery = query.replace(/^page=[0-9]+&?|&page=[0-9]+/i, '');
-    return cleanQuery === '' ? cleanQuery : `&${cleanQuery}`;
-  };
-
-  /**
-   * Generate a range from 1 to the total number of pages and return the list in an array.
-   *
-   * @return {Array} Array with page range.
-   */
-  getAllPageLinks = () => {
-    const pageLinks = [];
-
-    for (let i = 1; i <= this.props.pages; i++) {
-      pageLinks.push(i);
-    }
-
-    return pageLinks;
-  };
-
-  /**
-   * Generate a range from 1 to the lowLimit and return the list in an array.
-   *
-   * @return {Array} Array with page range.
-   */
-  getLowRangePageLinks = () => {
-    const pageLinks = [];
-
-    for (let i = 1; i <= this.state.lowLimit; i++) {
-      pageLinks.push(i);
-    }
-
-    return pageLinks;
-  };
-
-  /**
-   * Generate a range from the highLimit to the total number of pages and return the list in an array.
-   *
-   * @return {Array} Array with page range.
-   */
-  getHighRangePageLinks = () => {
-    const pageLinks = [];
-
-    for (let i = this.state.highLimit; i <= this.props.pages; i++) {
-      pageLinks.push(i);
-    }
-
-    return pageLinks;
-  };
-
-  /**
-   * Determine the start point for the page range and generate a range from the start to the max number
-   * of page linkes and return list in an array.
-   *
-   * @return {Array} Array with page range.
-   */
-  getMidRangePageLinks = () => {
-    const pageLinks = [];
-    const start = this.props.currentPage - Math.floor((this.state.maxPageLinks - 1) / 2);
-
-    for (let i = start; i < start + this.state.maxPageLinks; i++) {
-      pageLinks.push(i);
-    }
-
-    return pageLinks;
-  };
-
-  /**
-   * Using the passed in props pages and currentPage and the maxPageLinks, lowLimit and highLimit on the
-   * state, determine which method to call to generate the appropriate page range. Return the page range as
-   * an array of integers.
-   *
-   * @return {Array} Array with page range
-   */
-  getPageLinks = () => {
-    if (this.props.pages <= this.state.maxPageLinks) return this.getAllPageLinks();
-
-    if (this.props.currentPage < this.state.lowLimit) return this.getLowRangePageLinks();
-
-    if (this.props.currentPage > this.state.highLimit) return this.getHighRangePageLinks();
-
-    return this.getMidRangePageLinks();
-  };
-
-  /**
-   * Click event handler
-   * When called, scroll the window up to the top of the page.
-   */
-  handleClick = () => {
-    /**
-     * @todo Replace with smooth scroll
-     */
-    window.scrollTo(0, 0);
-  };
+  cleanQuery() {
+    const { query } = this.props;
+    const cleanedQuery = query.replace(/^page=[0-9]+&?|&page=[0-9]+/i, '');
+    return cleanedQuery === '' ? cleanedQuery : `&${cleanedQuery}`;
+  }
 
   /**
    * Render method.
@@ -157,17 +155,20 @@ class Pagination extends Component {
    * @return {HTML} HTML unordered list of page links
    */
   render() {
-    const { classes } = this.props;
+    const { classes, pathname } = this.props;
     const pageLinks = this.getPageLinks();
-    const cleanQuery = this.cleanQuery(this.props.query);
+    const cleanQuery = this.cleanQuery();
 
+    /**
+     * @todo Replace window scroll to with smooth scroll
+     */
     return (
       <ul className={classes.pagination}>
         {pageLinks.map(page => (
           <li key={page} className={classes.pageItem}>
             <Link
-              to={`${this.props.pathname}?page=${page}${cleanQuery}`}
-              onClick={event => this.handleClick(event, page)}
+              to={`${pathname}?page=${page}${cleanQuery}`}
+              onClick={() => window.scrollTo(0, 0)}
             >
               {page}
             </Link>
@@ -179,11 +180,15 @@ class Pagination extends Component {
 }
 
 Pagination.propTypes = {
-  count: PropTypes.number.isRequired,
+  classes: PropTypes.shape({}).isRequired,
   pages: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
   pathname: PropTypes.string.isRequired,
   query: PropTypes.string,
+};
+
+Pagination.defaultProps = {
+  query: '',
 };
 
 export default withStyles(styles)(Pagination);
