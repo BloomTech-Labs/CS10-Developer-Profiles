c0ae7def83b0f7c0f044edd0982788cc95fc03f6
Danny Vail <danny_vail@outlook.com>
2018-10-01 17:48:20 -0400

    FIX LINT ERRORS In SkillCloud.js - Fix lint errors including:   * Change arrow functions to normal functions   * Add trailing comma   * Shorten long lines   * Remove unecessary semi colons   * Enfore linebreaks inside function paranthesis   * Destructure props before usage   * Add classes to propTypes   * Define default propTypes for non required props   * Do not use array index as key   * Change proptype array to arrayOf

--- frontend/lambda-in/src/Components/SkillCloud/SkillCloud.js
+++ frontend/lambda-in/src/Components/SkillCloud/SkillCloud.js
@@ -8,24 +8,24 @@ import Typography from '@material-ui/core/Typography';
 const styles = {
   skill: {
     display: 'inline-block',
-    padding: '0 5px'
+    padding: '0 5px',
   },
   top: {
-    fontSize: '1.5em'
+    fontSize: '1.5em',
   },
   additional: {
-    fontSize: '1em'
+    fontSize: '1em',
   },
   familiar: {
-    fontSize: '.5em'
-  }
+    fontSize: '.5em',
+  },
 };
 
 /**
  * Skill Cloud component
  *
- * @description A component that generates a skill cloud using arrays of top skills, additional skills
- * and skills familiar with.
+ * @description A component that generates a skill cloud using arrays of top skills, additional
+ * skills and skills familiar with.
  * @class A React class component
  * @prop {Array} skills - An array of objects holding all skills passed in.
  */
@@ -35,8 +35,10 @@ class SkillCloud extends Component {
    *
    * @param {Object} props - Component properties.
    * @param {Array} props.topSkills - List of seeker's top skills to be displayed in large text.
-   * @param {Array} props.addSkills - List of seeker's additional skills to be displayed in medium text.
-   * @param {Array} props.familiar - List of skills the seeker is familiar with to be displayed in small
+   * @param {Array} props.addSkills - List of seeker's additional skills to be displayed in medium
+   * text.
+   * @param {Array} props.familiar - List of skills the seeker is familiar with to be displayed in
+   * small
    * text.
    *
    * @see {@link https://reacttraining.com/react-router/web/api/Link } for the Link API.
@@ -46,35 +48,30 @@ class SkillCloud extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      skills: this.combineSkills().sort((a, b) => (a.skill > b.skill ? 1 : -1))
+      skills: this.combineSkills().sort((a, b) => (a.skill > b.skill ? 1 : -1)),
     };
   }
 
   /**
-   * Using the passed in props topSkills, addSkills and familiar, combine the skills into an array of
-   * objects. Each object contains the skill type, a class associated with the skill type and the skill.
-   * Return the single skills array.
+   * Using the passed in props topSkills, addSkills and familiar, combine the skills into an array
+   * of objects. Each object contains the skill type, a class associated with the skill type and
+   * the skill. Return the single skills array.
    *
-   * @return {Array} Array of objects holding top skills, additional skills and skills familiar with.
+   * @return {Array} Array of objects holding top skills, additional skills and skills familiar
+   * with.
    */
-  combineSkills = () => {
-    const { classes } = this.props;
+  combineSkills() {
+    const {
+      classes, topSkills, addSkills, familiar,
+    } = this.props;
     const skills = [];
 
-    this.props.topSkills.forEach(skill =>
-      skills.push({ type: 'topSkills', class: classes.top, skill })
-    );
-
-    this.props.addSkills.forEach(skill =>
-      skills.push({ type: 'addSkills', class: classes.additional, skill })
-    );
-
-    this.props.familiar.forEach(skill =>
-      skills.push({ type: 'familiar', class: classes.familiar, skill })
-    );
+    topSkills.forEach(skill => skills.push({ skill, type: 'topSkills', class: classes.top }));
+    addSkills.forEach(skill => skills.push({ skill, type: 'addSkills', class: classes.additional }));
+    familiar.forEach(skill => skills.push({ skill, type: 'familiar', class: classes.familiar }));
 
     return skills;
-  };
+  }
 
   /**
    * Render method.
@@ -89,14 +86,9 @@ class SkillCloud extends Component {
 
     return (
       <div className="skill-cloud">
-        {skills.map((skill, index) => (
-          <Typography
-            key={index}
-            className={classnames(classes.skill, skill.class)}
-          >
-            <Link
-              to={`/meetdev?${skill.type}=${skill.skill.replace(/ /g, '+')}`}
-            >
+        {skills.map(skill => (
+          <Typography key={skill} className={classnames(classes.skill, skill.class)}>
+            <Link to={`/meetdev?${skill.type}=${skill.skill.replace(/ /g, '+')}`}>
               {skill.skill}
             </Link>
           </Typography>
@@ -107,9 +99,16 @@ class SkillCloud extends Component {
 }
 
 SkillCloud.propTypes = {
-  topSkills: PropTypes.array,
-  addSkills: PropTypes.array,
-  familiar: PropTypes.array
+  classes: PropTypes.shape({}).isRequired,
+  topSkills: PropTypes.arrayOf(PropTypes.string),
+  addSkills: PropTypes.arrayOf(PropTypes.string),
+  familiar: PropTypes.arrayOf(PropTypes.string),
+};
+
+SkillCloud.defaultProps = {
+  topSkills: [],
+  addSkills: [],
+  familiar: [],
 };
 
 export default withStyles(styles)(SkillCloud);
