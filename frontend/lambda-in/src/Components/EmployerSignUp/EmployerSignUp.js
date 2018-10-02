a48cf3404a6e0bc01404ea8a2f88b452438dfdc5
Danny Vail <danny_vail@outlook.com>
2018-10-02 11:31:59 -0400

    FIX LINT ERRORS In EmployerSignUp.js - Fix lint errors including:   * Prefer normal functions over arrow functions   * Remove unused imports   * Destructure state and props before usage   * Shorten long lines

--- frontend/lambda-in/src/Components/EmployerSignUp/EmployerSignUp.js
+++ frontend/lambda-in/src/Components/EmployerSignUp/EmployerSignUp.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 
-import '../EmployerSignUp/EmployerSignUp.css';
+import './EmployerSignUp.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -23,26 +22,29 @@ export default class EmployerSignUp extends Component {
       confirmPassword: '!Nechayev7alpha',
     };
   }
-  handleChange = event => {
+
+  handleChange(event) {
     this.setState({
       [event.target.id]: event.target.value,
     });
-  };
+  }
 
-  handleNewEmp = event => {
-    event.preventDefault();
-
+  handleNewEmp(event) {
+    const { companyName, email, password } = this.state;
+    const { setGS } = this.props;
     const newEmp = {
-      companyName: this.state.companyName,
-      email: this.state.email,
-      password: this.state.password,
+      companyName,
+      email,
+      password,
     };
 
+    event.preventDefault();
+
     axios
-      .post(`/api/register/employers`, newEmp)
-      .then(response => {
+      .post('/api/register/employers', newEmp)
+      .then((response) => {
         localStorage.setItem('token', response.data.jwt);
-        localStorage.setItem('_id', response.data.newUser._id);
+        localStorage.setItem('_id', response.data.newUser._id); // eslint-disable-line no-underscore-dangle
         // RESET local state
         this.setState({
           companyName: '',
@@ -53,7 +55,7 @@ export default class EmployerSignUp extends Component {
         /**
          * SET GLOBAL STATE
          */
-        this.props.setGS({
+        setGS({
           userInfo: { ...response.data.newUser }, // Set user data.
           isSignedIn: true,
           userType: 'employer',
@@ -61,82 +63,29 @@ export default class EmployerSignUp extends Component {
 
         console.log(response);
       })
-      .catch(err => {
+      .catch((err) => {
         this.setState({
           password: '',
           confirmPassword: '',
         });
         console.log(err);
       });
-  };
+  }
 
   render() {
+    const {
+      companyName, email, password, confirmPassword,
+    } = this.state;
+
     return (
-        <div data-aos="zoom-in-down" class="signupContainer">
-          <div class="formContainer">
-            <Paper class="paper">
-              <div className="form2">
-                <div>
-                  <Typography variant="display1" gutterBottom align="center">
-                    Employer
-                  </Typography>
-
-                  <Typography variant="headline" gutterBottom align="center">
-                    Sign Up
-                  </Typography>
-                </div>
-
-                <TextField
-                  id="companyName"
-                  label="Company Name"
-                  value={this.state.companyName}
-                  onChange={this.handleChange("companyName")}
-                  margin="normal"
-                  fullWidth="true"
-                />
-
-                <TextField
-                  id="companyEmail"
-                  label="Company Email"
-                  value={this.state.email}
-                  onChange={this.handleChange("email")}
-                  margin="normal"
-                  fullWidth="true"
-                />
-
-                <TextField
-                  id="password"
-                  type="password"
-                  label="Password"
-                  value={this.state.password}
-                  onChange={this.handleChange("password")}
-                  margin="normal"
-                />
-
-                <TextField
-                  id="confirmPassword"
-                  type="password"
-                  label="Confirm Password"
-                  value={this.state.confirmPassword}
-                  onChange={this.handleChange("confirmPassword")}
-                  margin="normal"
-                />
-                {/* 
-              <TextField
-                id="hiringManagerFirstName"
-                label="Hiring Manager's FirstName"
-                value={this.state.hiringManagerFirstName}
-                onChange={this.handleChange("hiringManagerFirstName")}
-                margin="normal"
-              />
-
-              <TextField
-                id="hiringManagerLastName"
-                label="Hiring Manager's Last Name"
-                value={this.state.hiringManagerLastName}
-                onChange={this.handleChange("hiringManagerLastName")}
-                margin="normal"
-              />
+      <div data-aos="zoom-in-down" className="signupContainer">
+        <div className="formContainer">
+          <Paper class="paper">
+            <div className="form2">
+              <div>
+                <Typography variant="display1" gutterBottom align="center">
+                  Employer
+                </Typography>
 
                 <Typography variant="headline" gutterBottom align="center">
                   Sign Up
@@ -146,15 +95,78 @@ export default class EmployerSignUp extends Component {
               <TextField
                 id="companyName"
                 label="Company Name"
-                value={this.state.companyName}
+                value={companyName}
+                onChange={this.handleChange('companyName')}
                 margin="normal"
                 fullWidth="true"
               />
 
-              <TextField id="email" label="Company Email" value={this.state.email} margin="normal" fullWidth="true" />
+              <TextField
+                id="companyEmail"
+                label="Company Email"
+                value={email}
+                onChange={this.handleChange('email')}
+                margin="normal"
+                fullWidth="true"
+              />
 
-              <TextField id="password" type="password" label="Password" value={this.state.password} margin="normal" />
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                value={password}
+                onChange={this.handleChange('password')}
+                margin="normal"
+              />
 
+              <TextField
+                id="confirmPassword"
+                type="password"
+                label="Confirm Password"
+                value={confirmPassword}
+                onChange={this.handleChange('confirmPassword')}
+                margin="normal"
+              />
+              {/*
+              <TextField
+                id="hiringManagerFirstName"
+                label="Hiring Manager's FirstName"
+                value={this.state.hiringManagerFirstName}
+                onChange={this.handleChange("hiringManagerFirstName")}
+                margin="normal"
+              />
+              <TextField
+                id="hiringManagerLastName"
+                label="Hiring Manager's Last Name"
+                value={this.state.hiringManagerLastName}
+                onChange={this.handleChange("hiringManagerLastName")}
+                margin="normal"
+              />
+                <Typography variant="headline" gutterBottom align="center">
+                  Sign Up
+                </Typography>
+              </div>
+              <TextField
+                id="companyName"
+                label="Company Name"
+                value={this.state.companyName}
+                margin="normal"
+                fullWidth="true"
+              />
+              <TextField
+                id="email"
+                label="Company Email"
+                value={this.state.email}
+                margin="normal"
+                fullWidth="true"
+              />
+              <TextField
+                id="password"
+                type="password"
+                label="Password"
+                value={this.state.password}
+                margin="normal"
+              />
               <TextField
                 id="confirmPassword"
                 type="password"
