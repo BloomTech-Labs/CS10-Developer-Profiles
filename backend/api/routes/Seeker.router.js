123b744a5032bbbca06fa92b025150b5df66d43e
Danny Vail <danny_vail@outlook.com>
2018-10-03 01:00:18 -0400

    AUTHENTICATE EACH SEEKER ENDPOINT In Seeker.router.js - Add userHasToken to each endpoint as middleware to authenticate.

--- backend/api/routes/Seeker.router.js
+++ backend/api/routes/Seeker.router.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const { RouterFactory } = require('express-router-factory');
+const { userHasToken } = require('../middleware/authentication');
 const { getSeekers } = require('../middleware/getters');
 const Seekers = require('../models/Seeker/Seeker.model');
 
 /**
  * Seekers CRUD endpoints
  *
- * Handle POST, GET/:id, PUT and DELETE with default Router Factory
+ * Handle POST, GET/:id, PUT and DELETE with Router Factory
  * Handle GET with custom middleware
+ * Authenticate all endpoints with userHasToken()
  */
 const router = express.Router();
 const seekersRF = new RouterFactory(router, Seekers);
 
 seekersRF.setProjection({ password: 0, __v: 0 });
-seekersRF.POST();
-seekersRF.GET('/', getSeekers.bind(this, Seekers));
-seekersRF.GET_id();
-seekersRF.PUT();
-seekersRF.DELETE();
+seekersRF.POST('/', userHasToken.bind(this), 'handlePOST');
+seekersRF.GET('/', userHasToken.bind(this), getSeekers.bind(this, Seekers));
+seekersRF.GET_id('/:id', userHasToken.bind(this), 'handleGET');
+seekersRF.PUT('/:id', userHasToken.bind(this), 'handlePUT');
+seekersRF.DELETE('/:id', userHasToken.bind(this), 'handleDELETE');
 
 module.exports = router;
