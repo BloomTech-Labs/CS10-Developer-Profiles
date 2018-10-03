52ec48e5cdb315c724ba80fcd155b66b21fd83f9
jesuarva <jesuarva.developer@gmail.com>
2018-10-04 00:51:54 +0200

    SMALL REFACTOR Comment an Element no yet needed in BasicInfo.js

--- frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/BasicInfo.js
+++ frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/BasicInfo.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import StateCapsule from '../StateCapsule/StateCapsule';
 
@@ -14,67 +14,65 @@ const BasicInfo = props => {
   };
 
   return (
-    <Fragment>
-      <StateCapsule schema={schema} object={schema}>
-        {({ stateCapsule }) => (
-          <div className="smallInputContainer">
-            <div className="expansionPanel">
-              <div className="inputField">
-                <TextField
-                  id="new-firstName"
-                  label="First Name"
-                  fullWidth
-                  value={stateCapsule.firstName}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-
-              <div className="inputField">
-                <TextField
-                  id="new-lastName"
-                  label="Last Name"
-                  fullWidth
-                  value={stateCapsule.lastName}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-
-              <div className="inputField">
-                <TextField
-                  id="new-desiredTitle"
-                  label="Desired Title"
-                  fullWidth
-                  value={stateCapsule.desiredTitle}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-
-              {/* TODO : WIRE InputGeolocation component here */}
-              <div className="inputField">
-                <TextField
-                  id="new-currentLocation"
-                  label="Current Location"
-                  fullWidth
-                  value={stateCapsule.currentLocation}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-            </div>
-            <div className="imageContainer">
-              <img
-                className="displayPic"
-                src={`https://robohash.org/1${stateCapsule.firstName}`}
-                alt="Italian "
+    <StateCapsule schema={schema} object={schema}>
+      {({ stateCapsule }) => (
+        <div className="smallInputContainer">
+          <div className="expansionPanel">
+            <div className="inputField">
+              <TextField
+                id="new-firstName"
+                label="First Name"
+                fullWidth
+                value={stateCapsule.firstName}
+                margin="normal"
+                variant="outlined"
               />
             </div>
+
+            <div className="inputField">
+              <TextField
+                id="new-lastName"
+                label="Last Name"
+                fullWidth
+                value={stateCapsule.lastName}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
+
+            <div className="inputField">
+              <TextField
+                id="new-desiredTitle"
+                label="Desired Title"
+                fullWidth
+                value={stateCapsule.desiredTitle}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
+
+            {/* TODO : WIRE InputGeolocation component here */}
+            {/* <div className="inputField">
+              <TextField
+                id="new-currentLocation"
+                label="Current Location"
+                fullWidth
+                value={stateCapsule.currentLocation}
+                margin="normal"
+                variant="outlined"
+              />
+            </div> */}
           </div>
-        )}
-      </StateCapsule>
-    </Fragment>
+          <div className="imageContainer">
+            <img
+              className="displayPic"
+              src={`https://robohash.org/1${stateCapsule.firstName}`}
+              alt="Italian "
+            />
+          </div>
+        </div>
+      )}
+    </StateCapsule>
   );
 };
 
