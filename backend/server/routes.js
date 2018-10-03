8f4b7e1d29683de5e2d881e3f1e25b8df47f2e1b
Danny Vail <danny_vail@outlook.com>
2018-10-02 23:47:19 -0400

    REFACTOR PUBLIC, PRIVATE AND PAYMENT ROUTES In routes.js - Setup all routes in one function. - Setup all routes before production frontend catch all route.

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -5,11 +5,13 @@ const loginRouter = require('../api/routes/login.router');
 const registerRouter = require('../api/routes/register.router');
 const stripeRouter = require('../payments/routes/stripe.router');
 
-// prettier-ignore
 module.exports = {
-  public: (server) => {
+  routes: (server) => {
+    server.use('/payments/stripe', stripeRouter);
     server.use('/api/login', loginRouter);
     server.use('/api/register', registerRouter);
+    server.use('/api/seekers', seekersRouter);
+    server.use('/api/employers', employerRouter);
     server.get('/api', (req, res) => {
       res.set('Content-Type', 'application/json');
       res.send('{"message":"Developer Profiles API"}');
@@ -21,20 +23,5 @@ module.exports = {
         res.sendFile(path.resolve(__dirname, '../../frontend/lambda-in/build', 'index.html'));
       });
     }
-  },
-  private: (server) => {
-    // This serves the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/seekers', seekersRouter);
-
-    // This serves the Employers DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/employers', employerRouter);
-  },
-  payments: {
-    /**
-     * Define an endpoint to process Stripe payment requests
-     */
-    stripe: (server) => {
-      server.use('/payments/stripe', stripeRouter);
-    },
   },
 };
