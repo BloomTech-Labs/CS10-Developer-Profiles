2580a01f332416a6419c80a742d7ec00b09ee56a
jesuarva <jesuarva.developer@gmail.com>
2018-10-03 19:55:13 +0200

    REFACTOR Wrap BioSkills.js with StateCapsule.js

--- frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/BioSkills.js
+++ frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/BioSkills.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import DropDown from '../DropDown/DropDown';
 import MapChips from '../MapArrays/MapChips';
 import ArrayController from '../MapArrays/ArrayController';
+import StateCapsule from '../StateCapsule/StateCapsule';
 
 /**
  * Render the Developer's 'summary' and 'topSkills' fields.Fragment
@@ -16,69 +17,84 @@ const BioSkills = props => {
   // eslint-disable-next-line react/prop-types
   const { userInfo, setFS } = props;
 
-  return (
-    <Fragment>
-      <DropDown header="Biio and Skills">
-        <div className="inputFieldLargeMultiline">
-          <TextField
-            id="summary"
-            index="an index"
-            label="Your Bio"
-            fullWidth
-            multiline
-            rowsMax="20"
-            value={userInfo.summary}
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
-        {/* TODO */}
-        <ArrayController
-          itemSchema="singleItem"
-          title="Top Skills"
-          field="topSkills"
-          setPFS={setFS}
-          arr={userInfo.topSkills}
-        >
-          {/* eslint-disable-next-line object-curly-newline */}
-          {({
-            handleChange,
-            updateFormState,
-            removeItem,
-            state,
-            arr,
-            title,
-          }) => (
-            <Fragment>
-              <div className="inputFieldLargeMultiline" onChange={handleChange}>
-                <TextField
-                  id="newItem"
-                  label="Top skills"
-                  fullWidth
-                  multiline
-                  rowsMax="4"
-                  value={state.newItem}
-                  margin="normal"
-                  variant="outlined"
-                />
-              </div>
-              {/* <MapUnorderedList array={arr} /> */}
-              <MapChips array={arr} removeItem={removeItem} />
-              <Button
-                variant="outlined"
-                color="primary"
-                align="center"
-                onClick={updateFormState}
-              >
-                {`Add new ${title}`}
-              </Button>
-            </Fragment>
-          )}
-        </ArrayController>
+  const field = 'topSkills';
+  const schema = {
+    summary: userInfo.summary,
+    topSkills: userInfo.topSkills,
+  };
 
-        {/* TODO: Add summary - enable uploading a file */}
-      </DropDown>
-    </Fragment>
+  return (
+    <StateCapsule schema={schema} object={schema}>
+      {({ stateCapsule }) => (
+        <DropDown header="Biio and Skills">
+          <div className="inputFieldLargeMultiline">
+            <TextField
+              id="new-summary"
+              index="an index"
+              label="Your Bio"
+              fullWidth
+              multiline
+              rowsMax="20"
+              value={stateCapsule.summary}
+              margin="normal"
+              variant="outlined"
+            />
+          </div>
+          {/* TODO */}
+          <ArrayController
+            itemSchema="singleItem"
+            title="Top Skills"
+            field={field}
+            setPFS={setFS}
+            arr={stateCapsule.topSkills}
+          >
+            {/* eslint-disable-next-line object-curly-newline */}
+            {({
+              handleChange,
+              updateFormState,
+              removeItem,
+              state,
+              arr,
+              title,
+            }) => (
+              <Fragment>
+                <div
+                  className="inputFieldLargeMultiline"
+                  onChange={handleChange}
+                >
+                  <TextField
+                    id="newItem"
+                    label="Add skills"
+                    fullWidth
+                    multiline
+                    rowsMax="4"
+                    value={state.newItem}
+                    margin="normal"
+                    variant="outlined"
+                    inputProps={{
+                      'data-value': state.newItem,
+                      'data-field': field,
+                    }}
+                  />
+                </div>
+                {/* <MapUnorderedList array={arr} /> */}
+                <MapChips array={arr} removeItem={removeItem} />
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  align="center"
+                  onClick={updateFormState}
+                >
+                  {`Add new ${title}`}
+                </Button>
+              </Fragment>
+            )}
+          </ArrayController>
+
+          {/* TODO: Add summary - enable uploading a file */}
+        </DropDown>
+      )}
+    </StateCapsule>
   );
 };
 
