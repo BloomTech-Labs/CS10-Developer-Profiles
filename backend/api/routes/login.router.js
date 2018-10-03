338789f612244ae52d9f9629f5a6140e531e9d3b
Danny Vail <danny_vail@outlook.com>
2018-10-03 02:09:08 -0400

    USE LOGIN MIDDLEWARE FROM AUTHENTICATION In login.router.js - Use login middleware from authentication file instead of routerUtils.js - Reformat to match other router files.

--- backend/api/routes/login.router.js
+++ backend/api/routes/login.router.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const { RouterFactory } = require('express-router-factory');
-const { login } = require('../utils/routerUtils');
+const { login } = require('../middleware/authentication');
 const Seekers = require('../models/Seeker/Seeker.model');
 const Employers = require('../models/Employer/Employer.model');
 
+/**
+ * Login endpoints for seekers and employers
+ *
+ * Handle login with 'login(model, req, res)' middleware
+ */
 const router = express.Router();
-
-/**
- * Seekers: login
- */
 const seekersRF = new RouterFactory(router, Seekers);
-// Currying Seekers model into 'login(model, req, res)' middleware
-seekersRF.POST('/seekers', 'login', login.bind(this, Seekers));
-
-/**
- * Employers: login
- */
 const employersRF = new RouterFactory(router, Employers);
-// Currying Employers model into 'login(model, req, res)' middleware
+
+// Create login/POST endpoints.
+seekersRF.POST('/seekers', 'login', login.bind(this, Seekers));
 employersRF.POST('/employers', 'login', login.bind(this, Employers));
 
 module.exports = router;
