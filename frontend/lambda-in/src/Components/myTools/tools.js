bf713d6520a6698717edcaa91608dd712913486f
jesuarva <jesuarva.developer@gmail.com>
2018-10-03 13:07:20 +0200

    ADD utility functions file tools.js

---
+++ frontend/lambda-in/src/Components/myTools/tools.js
@@ -0,0 +1,21 @@
+export default {
+  // prettier-ignore
+  mapAndResetKeysValues: object => Object.keys(object).reduce((resetKeys, key) => {
+    // eslint-disable-next-line no-param-reassign
+    const valueType = tools.typeOf(resetKeys[key]);
+
+    switch (valueType) {
+      case '[object Array]':
+        // eslint-disable-next-line no-param-reassign
+        resetKeys[key] = [];
+        break;
+      default:
+        // eslint-disable-next-line no-param-reassign
+        resetKeys[key] = '';
+    }
+
+
+    return resetKeys;
+  }, {}),
+  typeOf: item => Object.prototype.toString.call(item),
+};
