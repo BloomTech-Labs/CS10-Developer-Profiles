d56724741c9d4dec41a6778a74301a559cef9a9e
Danny Vail <danny_vail@outlook.com>
2018-10-03 01:09:34 -0400

    AUTHENTICATE EACH EMPLOYERS ENDPOINT In Employer.router.js - Add userHasToken to each endpoint as middleware to authenticate. - Format like Seeker.router.js

--- backend/api/routes/Employer.router.js
+++ backend/api/routes/Employer.router.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const { RouterFactory } = require('express-router-factory');
+const { userHasToken } = require('../middleware/authentication');
 const Employer = require('../models/Employer/Employer.model');
 
+/**
+ * Employer CRUD endpoints
+ *
+ * Handle POST, GET, GET/:id, PUT and DELETE with Router Factory
+ * Authenticate all endpoints with userHasToken()
+ */
 const router = express.Router();
-
-const RF = new RouterFactory(router, Employer);
+const employerRF = new RouterFactory(router, Employer);
 
 // Set projections
-RF.setProjection({ password: 0, __v: 0 });
+employerRF.setProjection({ password: 0, __v: 0 });
 
 // Create all CRUD endpoints.
-RF.CRUD();
+employerRF.POST('/', userHasToken.bind(this), 'handlePOST');
+employerRF.GET('/', userHasToken.bind(this), 'handleGET');
+employerRF.GET_id('/:id', userHasToken.bind(this), 'handleGET');
+employerRF.PUT('/:id', userHasToken.bind(this), 'handlePUT');
+employerRF.DELETE('/:id', userHasToken.bind(this), 'handleDELETE');
 
 module.exports = router;
