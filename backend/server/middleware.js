0bbb3d6c50ff5925cd1fb913f8abf9a884c5a65d
Danny Vail <danny_vail@outlook.com>
2018-10-02 23:41:16 -0400

    REMOVE AUTH UTILITIES In middleware.js - Remove auth utility import - Remove auth function export. Will apply auth middleware to each route rather than as middleware to server.

--- backend/server/middleware.js
+++ backend/server/middleware.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
-const { userHasToken } = require('../api/utils/authUtils');
 
 module.exports = {
-  globals(server) {
+  server(server) {
     server.use(express.json());
     server.use(helmet());
 
@@ -12,9 +11,5 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       server.use(express.static(path.resolve(__dirname, '../../frontend/lambda-in/build')));
     }
-  },
-  auth(server) {
-    // Validate if client has a valid token
-    server.use(userHasToken);
   },
 };
