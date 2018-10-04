84cfd5f791a0d4a4b485e0004f8a8694b0cae1ec
Danny Vail <danny_vail@outlook.com>
2018-10-04 11:39:32 -0400

    EDIT STYLING In DevSignUp.css - Delete file. Use CSS-in-JS.
    In DevSignUp.js
    - Add styles as CSS-in-JS
    - Hide submit input

--- frontend/lambda-in/src/Components/DevSignUp/DevSignUp.js
+++ frontend/lambda-in/src/Components/DevSignUp/DevSignUp.js
@@ -3,15 +3,41 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AOS from 'aos';
 import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
-import './DevSignUp.css';
 import 'aos/dist/aos.css';
 
 AOS.init();
+
+const styles = {
+  signInContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    padding: '10px 0',
+    width: '100vw',
+  },
+  signInForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '20px',
+    padding: '5px',
+    width: '30vw',
+  },
+  signUp: {
+    margin: '16px',
+  },
+  submitButton: {
+    width: '100%',
+  },
+  submitInput: {
+    display: 'none',
+  },
+};
 
 class DevSignUp extends Component {
   constructor(props) {
@@ -93,14 +119,15 @@ class DevSignUp extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const {
       firstName, lastName, email, password, confirmPassword,
     } = this.state;
 
     return (
-      <div data-aos="zoom-in-down" className="signupContainer">
+      <div data-aos="zoom-in-down" className={classes.signInContainer}>
         <Paper className="paper" onChange={this.handleChange}>
-          <form className="form2" onSubmit={this.handleNewUser}>
+          <form className={classes.signInForm} onSubmit={this.handleNewUser}>
             <div>
               <Typography variant="display1" gutterBottom align="center">
                 MeetDev
@@ -128,13 +155,18 @@ class DevSignUp extends Component {
             />
             <br />
             <label htmlFor="input-submit-button">
-              <input id="input-submit-button" type="submit" />
-              <Button variant="contained" color="primary" onClick={this.handleNewUser}>
+              <input id="input-submit-button" className={classes.submitInput} type="submit" />
+              <Button
+                className={classes.submitButton}
+                variant="contained"
+                color="primary"
+                onClick={this.handleNewUser}
+              >
                 Sign Up
               </Button>
             </label>
           </form>
-          <div className="signup">
+          <div className={classes.signUp}>
             <Link to="/employer-signup">
               <Typography variant="caption" gutterBottom align="center">
                 Are You An Employer? Sign Up Here!
@@ -153,7 +185,8 @@ class DevSignUp extends Component {
 }
 
 DevSignUp.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
   setGS: PropTypes.func.isRequired,
 };
 
-export default DevSignUp;
+export default withStyles(styles)(DevSignUp);
