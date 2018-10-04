8ddc995cd3e6f89dc400a1a5521dd8b0f54ca63b
jesuarva <jesuarva.developer@gmail.com>
2018-10-04 12:44:04 +0200

    REFACTOR MapDropDown Components
    Now it handle and renders different schemas dinamically.

--- frontend/lambda-in/src/Components/utilityComponents/MapArrays/MapDropDown.js
+++ frontend/lambda-in/src/Components/utilityComponents/MapArrays/MapDropDown.js
@@ -1,54 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DropDown from '../DropDown/DropDown';
+import StateCapsule from '../StateCapsule/StateCapsule';
 
-// prettier-ignore
-const MapDropDown = ({ array, classes, removeItem }) => array.map((item, index) => (
-  <DropDown header={item.title} key={`${item.title}${Math.random()}`}>
-    <Button
-      variant="outlined"
-      color="primary"
-      align="center"
-      onClick={removeItem(index)}
+// eslint-disable-next-line object-curly-newline
+const MapDropDown = ({ array, field, itemType, schema, propsAndLabels }) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  array.map((item, index) => (
+    <DropDown
+      header={item.title || item.degree}
+      key={`${item.title || item.degree}${Math.random()}`}
     >
-      X - delete
-    </Button>
-    <div className={`dropdown-item-list ${classes}`}>
-      <TextField
-        id={`edit-${index}-title`}
-        label="Email"
-        value={item.title}
-        margin="normal"
-      />
-
-      <TextField
-        id={`edit-${index}-description`}
-        label="Description"
-        value={item.description}
-        margin="normal"
-      />
-      <TextField
-        id={`edit-${index}-img`}
-        label="Image"
-        value={item.img}
-        margin="normal"
-      />
-
-      <TextField
-        id={`edit-${index}-link`}
-        label="Link"
-        value={item.link}
-        margin="normal"
-      />
-
-      <TextField
-        id={`edit-${index}-repo`}
-        label="Repository"
-        value={item.repo}
-        margin="normal"
-      />
-    </div>
-  </DropDown>
-));
+      <StateCapsule schema={schema} object={item}>
+        {({ stateCapsule, removeItem }) => (
+          <div className="dropdown-item-list blur-handler">
+            <Fragment>
+              <Button
+                variant="outlined"
+                color="primary"
+                align="center"
+                onClick={removeItem(field, index)}
+              >
+                delete
+              </Button>
+              {propsAndLabels.map((propLabel, propIndex) => (
+                <div className="inputFieldLargeMultiline">
+                  <TextField
+                    id={`edit-${propLabel[0]}`}
+                    label={propLabel[1]}
+                    fullWidth
+                    multiline
+                    rowsMax="20"
+                    value={stateCapsule[propLabel[0]]}
+                    margin="normal"
+                    variant="outlined"
+                    inputProps={{
+                      'data-field': field,
+                      'data-itemtype': itemType,
+                      'data-index': index,
+                      'data-property': propLabel[0],
+                      'data-value': stateCapsule[propLabel[0]],
+                    }}
+                  />
+                </div>
+              ))}
+            </Fragment>
+          </div>
+        )}
+      </StateCapsule>
+    </DropDown>
+  ));
 export default MapDropDown;
