d981f384de68f22d385e4d916412f7e616e51b09
jesuarva <jesuarva.developer@gmail.com>
2018-10-04 13:28:52 +0200

    REFACTOR Experience-section ussing ArraySeciton.js component

--- frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Experience.js
+++ frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Experience.js
@@ -1,9 +1,5 @@
 import React, { Fragment } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import DropDown from '../DropDown/DropDown';
-import StateCapsule from '../StateCapsule/StateCapsule';
-import MapDropDown from '../MapArrays/MapDropDown';
+import ArraySection from '../ArraySection/ArraySection';
 
 // eslint-disable-next-line arrow-parens
 const Experience = (props) => {
@@ -20,54 +16,15 @@ const Experience = (props) => {
     startYear: 'Start year',
     endYear: 'End year',
   };
-  const propsAndLabels = Object.entries(schema);
 
   return (
-    <DropDown header="Experience">
-      <DropDown header="Add new experience">
-        <StateCapsule schema={schema} object={{}}>
-          {({ stateCapsule, createItem }) => (
-            <Fragment>
-              <Button
-                variant="outlined"
-                color="primary"
-                align="center"
-                onClick={createItem(field)}
-              >
-                Create
-              </Button>
-              {propsAndLabels.map((propLabel, index) => (
-                <div className="inputFieldLargeMultiline">
-                  <TextField
-                    id={`new-${propLabel[0]}`}
-                    label={propLabel[1]}
-                    fullWidth
-                    multiline
-                    rowsMax="20"
-                    value={stateCapsule[propLabel[0]]}
-                    margin="normal"
-                    variant="outlined"
-                    inputProps={{
-                      'data-field': field,
-                      'data-itemtype': itemType,
-                      'data-property': propLabel[0],
-                      'data-value': stateCapsule[propLabel[0]],
-                    }}
-                  />
-                </div>
-              ))}
-            </Fragment>
-          )}
-        </StateCapsule>
-      </DropDown>
-      <MapDropDown
-        array={userInfo[field]}
-        field={field}
-        itemType={itemType}
-        schema={schema}
-        propsAndLabels={propsAndLabels}
-      />
-    </DropDown>
+    <ArraySection
+      header="Experience"
+      userInfo={userInfo}
+      field={field}
+      itemType={itemType}
+      schema={schema}
+    />
   );
 };
 
--- frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Projects.js
+++ frontend/lambda-in/src/Components/utilityComponents/SeekerEditUtils/Projects.js
@@ -1,176 +1,29 @@
-import React, { Fragment } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import DropDown from '../DropDown/DropDown';
-import ArrayController from '../MapArrays/ArrayController';
-import MapChips from '../MapArrays/MapChips';
-import MapDropDown from '../MapArrays/MapDropDown';
+import React from 'react';
+import ArraySection from '../ArraySection/ArraySection';
 
 // eslint-disable-next-line arrow-parens
-const Projects = props => {
-  const { userInfo, setFS } = props;
+const Projects = (props) => {
+  const { userInfo } = props;
 
-  const projectSchema = {
-    title: '',
-    description: '',
-    img: '',
-    link: '',
-    repo: '',
-    tech: [],
+  const field = 'projects';
+  const itemType = 'object';
+  const schema = {
+    title: 'Title',
+    description: 'Description',
+    img: 'Image',
+    link: 'Link',
+    repo: 'Repository',
+    // tech: ['Stack'], // TODO
   };
 
   return (
-    <Fragment>
-      <DropDown header="Add your best projects">
-        <ArrayController
-          itemSchema={projectSchema}
-          title="Projects"
-          field="projects"
-          setPFS={setFS}
-          arr={userInfo.projects}
-        >
-          {/* eslint-disable-next-line object-curly-newline */}
-          {({
-            handleChange,
-            setArrayControllerState,
-            updateFormState,
-            removeItem,
-            state,
-            arr,
-            title,
-          }) => (
-            <Fragment>
-              <div className="project-container" onChange={handleChange}>
-                <DropDown header="New project details">
-                  <div className="inputs-container">
-                    <div className="inputFieldLargeMultiline">
-                      <TextField
-                        id="title"
-                        label="Project"
-                        fullWidth
-                        multiline
-                        rowsMax="4"
-                        value={state.title}
-                        margin="normal"
-                        variant="outlined"
-                      />
-                    </div>
-                    <div className="inputFieldLargeMultiline">
-                      <TextField
-                        id="description"
-                        label="Description"
-                        fullWidth
-                        multiline
-                        rowsMax="4"
-                        value={state.description}
-                        margin="normal"
-                        variant="outlined"
-                      />
-                    </div>
-                    <div className="inputFieldLargeMultiline">
-                      <TextField
-                        id="img"
-                        label="Img url"
-                        fullWidth
-                        multiline
-                        rowsMax="4"
-                        value={state.img}
-                        margin="normal"
-                        variant="outlined"
-                      />
-                    </div>
-                    <div className="inputFieldLargeMultiline">
-                      <TextField
-                        id="link"
-                        label="Link to project"
-                        fullWidth
-                        multiline
-                        rowsMax="4"
-                        value={state.link}
-                        margin="normal"
-                        variant="outlined"
-                      />
-                    </div>
-                    <div className="inputFieldLargeMultiline">
-                      <TextField
-                        id="repo"
-                        label="Link to repo"
-                        fullWidth
-                        multiline
-                        rowsMax="4"
-                        value={state.repo}
-                        margin="normal"
-                        variant="outlined"
-                      />
-                    </div>
-                  </div>
-                  <ArrayController
-                    itemSchema="singleItem"
-                    title="Tech stack"
-                    field="tech"
-                    setPFS={setArrayControllerState}
-                    arr={state.tech}
-                  >
-                    {/* eslint-disable-next-line object-curly-newline, no-shadow */}
-                    {({
-                      handleChange,
-                      updateFormState,
-                      removeItem,
-                      state,
-                      arr,
-                      title,
-                    }) => (
-                      <Fragment>
-                        <div
-                          className="inputFieldLargeMultiline"
-                          onChange={handleChange}
-                        >
-                          <TextField
-                            id="newItem"
-                            label="Tech stack"
-                            fullWidth
-                            multiline
-                            rowsMax="4"
-                            value={state.newItem}
-                            margin="normal"
-                            variant="outlined"
-                          />
-                        </div>
-                        {/* <MapUnorderedList array={arr} /> */}
-                        <MapChips array={arr} removeItem={removeItem} />
-                        <Button
-                          variant="outlined"
-                          color="primary"
-                          align="center"
-                          onClick={updateFormState}
-                        >
-                          {`Add new ${title}`}
-                        </Button>
-                      </Fragment>
-                    )}
-                  </ArrayController>
-
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    align="center"
-                    onClick={updateFormState}
-                  >
-                    {`Add new ${title}`}
-                  </Button>
-                </DropDown>
-                {/* MapDropdown */}
-                <MapDropDown
-                  array={arr}
-                  removeItem={removeItem}
-                  classes={null}
-                />
-              </div>
-            </Fragment>
-          )}
-        </ArrayController>
-      </DropDown>
-    </Fragment>
+    <ArraySection
+      header="Projects"
+      userInfo={userInfo}
+      field={field}
+      itemType={itemType}
+      schema={schema}
+    />
   );
 };
 
