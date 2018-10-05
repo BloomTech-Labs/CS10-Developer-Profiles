4d63cc3aa3cb6c798563e5025744c29df3abfdd6
jesuarva <jesuarva.developer@gmail.com>
2018-10-05 18:59:57 +0200

    FIX MERGE CONFLICT

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -23,37 +23,27 @@ module.exports = {
     // In production build all other requests are handled by the frontend
     if (process.env.NODE_ENV === 'production') {
       server.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../frontend/lambda-in/build', 'index.html'));
+        res.sendFile(
+          path.resolve(
+            __dirname,
+            '../../frontend/lambda-in/build',
+            'index.html',
+          ),
+        );
       });
     } else {
       server.get('*', (req, res) => {
         res.set('Content-Type', 'application/json');
-        res.send('{"message":"On production build this action will result in a redirect to the frontend."}');
+        res.send(
+          '{"message":"On production build this action will result in a redirect to the frontend."}',
+        );
       });
     }
   },
-<<<<<<< HEAD
-||||||| merged common ancestors
   private: (server) => {
     // This serves the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
     server.use('/api/seekers', seekersRouter);
 
-    // This serves the Employers DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/employers', employerRouter);
-  },
-  payments: {
-    /**
-     * Define an endpoint to process Stripe payment requests
-     */
-    stripe: (server) => {
-      server.use('/payments/stripe', stripeRouter);
-    },
-  },
-=======
-  private: (server) => {
-    // This serves the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/seekers', seekersRouter);
-    
     // This serves the Employers DB. It allows GET, POST, PUT and DELETE
     server.use('/api/employers', employerRouter);
     // server.use('/api/resetPassword', forgotPasswordRouter);
@@ -66,5 +56,4 @@ module.exports = {
       server.use('/payments/stripe', stripeRouter);
     },
   },
->>>>>>> f208695987f92b68ce62ed87a15a7ee39e3dab6a
 };
