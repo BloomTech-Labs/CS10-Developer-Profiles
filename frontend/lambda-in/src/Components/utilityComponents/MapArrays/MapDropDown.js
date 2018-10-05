3e1527d2d7fdf2dad1f986b99cf42a3c8d4ad872
jesuarva <jesuarva.developer@gmail.com>
2018-10-05 17:56:13 +0200

    Revert "ENHANCEMENT make MapDropDown.js render an array-section in either an Edit or Display mode"
    This reverts commit 8f17ad3111a5718c0893b99538d1d56413216cf2.

--- frontend/lambda-in/src/Components/utilityComponents/MapArrays/MapDropDown.js
+++ frontend/lambda-in/src/Components/utilityComponents/MapArrays/MapDropDown.js
@@ -1,14 +1,13 @@
 import React, { Fragment } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import DropDown from '../DropDown/DropDown';
 import StateCapsule from '../StateCapsule/StateCapsule';
 
-const MapDropDown = (props) => {
-  const { isProfile, array, field, itemType, schema, propsAndLabels } = props;
-
-  const renderForm = array.map((item, index) => (
+// eslint-disable-next-line object-curly-newline
+const MapDropDown = ({ array, field, itemType, schema, propsAndLabels }) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  array.map((item, index) => (
     <DropDown
       header={item.title || item.degree}
       key={`${item.title || item.degree}${Math.random()}`}
@@ -26,10 +25,7 @@ const MapDropDown = (props) => {
                 delete
               </Button>
               {propsAndLabels.map((propLabel, propIndex) => (
-                <div
-                  className="inputFieldLargeMultiline"
-                  key={`${Math.random()}-${propIndex}`}
-                >
+                <div className="inputFieldLargeMultiline">
                   <TextField
                     id={`edit-${propLabel[0]}`}
                     label={propLabel[1]}
@@ -55,33 +51,4 @@ const MapDropDown = (props) => {
       </StateCapsule>
     </DropDown>
   ));
-
-  const renderProfile = array.map((item, index) => (
-    <DropDown
-      header={item.title || item.degree}
-      key={`${item.title || item.degree}${Math.random()}`}
-    >
-      <div className="dropdown-item-list blur-handler">
-        <Fragment>
-          {propsAndLabels.map((propLabel, propIndex) => (
-            <div
-              className="inputFieldLargeMultiline"
-              key={`${Math.random()}-${propIndex}`}
-            >
-              <div>
-                <Typography variant="caption">{propLabel[1]}</Typography>
-                <Typography variant="subheading">
-                  {item[propLabel[0]]}
-                </Typography>
-              </div>
-            </div>
-          ))}
-        </Fragment>
-      </div>
-    </DropDown>
-  ));
-
-  return isProfile ? renderProfile : renderForm;
-};
-
 export default MapDropDown;
