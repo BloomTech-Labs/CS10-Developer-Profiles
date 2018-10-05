455f8728124b4cbeec96ef9b286fa1dbc21bbe9a
samarv <samarvir1996@gmail.com>
2018-10-05 23:34:41 +0530

    added newer version of routes

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -13,46 +13,22 @@ module.exports = {
     server.use('/api/register', registerRouter);
     server.use('/api/seekers', seekersRouter);
     server.use('/api/employers', employerRouter);
+    server.use('/api/saveresethash', forgotPasswordRouter);
     server.get('/api', (req, res) => {
       res.set('Content-Type', 'application/json');
       res.send('{"message":"Developer Profiles API"}');
     });
 
-    server.use('/api/saveresethash', forgotPasswordRouter);
-
     // In production build all other requests are handled by the frontend
     if (process.env.NODE_ENV === 'production') {
       server.get('*', (req, res) => {
-        res.sendFile(
-          path.resolve(
-            __dirname,
-            '../../frontend/lambda-in/build',
-            'index.html',
-          ),
-        );
+        res.sendFile(path.resolve(__dirname, '../../frontend/lambda-in/build', 'index.html'));
       });
     } else {
       server.get('*', (req, res) => {
         res.set('Content-Type', 'application/json');
-        res.send(
-          '{"message":"On production build this action will result in a redirect to the frontend."}',
-        );
+        res.send('{"message":"On production build this action will result in a redirect to the frontend."}');
       });
     }
-  },
-  private: (server) => {
-    // This serves the Seekers (Employees) DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/seekers', seekersRouter);
-    // This serves the Employers DB. It allows GET, POST, PUT and DELETE
-    server.use('/api/employers', employerRouter);
-    // server.use('/api/resetPassword', forgotPasswordRouter);
-  },
-  payments: {
-    /**
-     * Define an endpoint to process Stripe payment requests
-     */
-    stripe: (server) => {
-      server.use('/payments/stripe', stripeRouter);
-    },
   },
 };
