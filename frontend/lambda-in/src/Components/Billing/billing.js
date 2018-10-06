eb7897b47115cac12905d646b5ff7ea204e93c96
Nathaniel Flory <nathanielflory@gmail.com>
2018-10-06 09:10:53 -0400

    Billing Redesign -Added MUI Theme Provider to change color of navbar to theme -Changed HTML Layout of Billing page -Added billingImage to files -Added billingImage to billing page -Styled checkout cards *centered text *added padding *moved into separate div and put in row -Slight adjustment to position of cta on landing page

--- frontend/lambda-in/src/Components/Billing/billing.js
+++ frontend/lambda-in/src/Components/Billing/billing.js
@@ -3,11 +3,16 @@ import Checkout from './Stripe/stripe';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import billingImage from '../../static/images/billingImage.png';
 //import RadioButtonsGroup from './Radio Buttons/radio-buttons'
 import './billing.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+const backgroundImage = {
+    backgroundImage: `url(${billingImage})`,
+  };
 
 AOS.init();
 
@@ -15,79 +20,61 @@ class Billing extends React.Component {
     render() {
         return (
             <div>
-                <h1 className='title'>Join our guild of talented developers and reputable employers!</h1>
-                <div className='container'>
-                <Card data-aos='flip-up' className='checkout'>
-                <CardContent>
-                    <Typography variant="headline" component="h1">
-                        Employer 1 month Sub
-                    </Typography>
-                    <Typography>
-                        $3.99
-                    </Typography>
-                    <Checkout 
-                        name={'Employer 1 month Subscription'}
-                        description={'Employer 1 month fee'}
-                        amount={3.99}
-                        />
-                </CardContent>
-                </Card>
-                <Card data-aos='flip-down' className='checkout'>
-                <CardContent>
-                    <Typography variant="headline" component="h1">
-                        Employer 6 month Sub
-                    </Typography>
-                    <Typography>
-                        $9.99 - Save $2.32/month
-                    </Typography>
-                    <Checkout 
-                        name={'Employer 6 month Subscription'}
-                        description={'Employer 6 month fee'}
-                        amount={9.99}
-                        />
-                </CardContent>
-                </Card>
-                <Card data-aos='flip-up' className='checkout'>
-                <CardContent>
-                    <Typography variant="headline" component="h1">
-                        Employer 1 Year Sub
-                    </Typography>
-                    <Typography>
-                        $15.99 - Save $2.62/month
-                    </Typography>
-                    <Checkout 
-                        name={'Employer 1 year subscription'}
-                        description={'Employer 1 year fee'}
-                        amount={15.99}
-                        />
-                </CardContent>
-                </Card>
-                {/*<div className='checkout'>
-                    <div className='text'>
-                        <p>1 month - $9.99</p>
-                        <p>3 months - $19.99</p>
-                        <p>6 months - $29.99</p>
-                    </div>
-                    <div className='buttons'>
-                        <Checkout 
-                        name={'Subscription Fee'}
-                        description={'1 month'}
-                        amount={9.99}
-                        />
-                        <Checkout 
-                        name={'Subscription Fee'}
-                        description={'3 months'}
-                        amount={19.99}
-                        />
-                        <Checkout 
-                        name={'Subscription Fee'}
-                        description={'6 months'}
-                        amount={29.99}
-                        />
-                    </div>
-        </div>*/}
+                <div className='billingimage' style={backgroundImage}></div>
+                <div className='checkoutcontainer'>
+                    <span className='checkout1'>
+                        <Card data-aos='flip-up'>
+                            <CardContent align='center'>
+                                 <Typography variant="headline" component="h1">
+                                    Employer 1 month Sub
+                                </Typography>
+                                <Typography>
+                                    $3.99
+                                </Typography>
+                                <Checkout align='center'
+                                name={'Employer 1 month Subscription'}
+                                description={'Employer 1 month fee'}
+                                amount={3.99}
+                                />
+                            </CardContent>
+                        </Card>
+                    </span>
+                    <span className='checkout2'>    
+                        <Card data-aos='flip-down' className='checkout'>
+                            <CardContent align='center'>
+                                <Typography variant="headline" component="h1">
+                                    Employer 6 month Sub
+                                </Typography>
+                                <Typography>
+                                    $9.99 - Save $2.32/month
+                                </Typography>
+                                <Checkout 
+                                    name={'Employer 6 month Subscription'}
+                                    description={'Employer 6 month fee'}
+                                    amount={9.99}
+                                />
+                            </CardContent>
+                        </Card>
+                    </span>
+                    <span className='checkout3'>
+                        <Card data-aos='flip-up' className='checkout'>
+                            <CardContent align='center'>
+                                <Typography variant="headline" component="h1">
+                                    Employer 1 Year Sub
+                                </Typography>
+                                <Typography>
+                                    $15.99 - Save $2.62/month
+                                </Typography>
+                                 <Checkout 
+                                    name={'Employer 1 year subscription'}
+                                    description={'Employer 1 year fee'}
+                                    amount={15.99}
+                                />
+                            </CardContent>
+                        </Card>
+                    </span>
+                </div>
             </div>
-        </div>
         )
     }
 }
