b7a636cb6c0eb2aa2fd38b202f66e36cbb2c2c10
jesuarva <jesuarva.developer@gmail.com>
2018-10-06 21:05:09 +0200

    CLEAN Code

--- frontend/lambda-in/src/Components/utilityComponents/StateCapsule/StateCapsule.js
+++ frontend/lambda-in/src/Components/utilityComponents/StateCapsule/StateCapsule.js
@@ -20,7 +20,6 @@ class StateCapsule extends Component {
     this.mapAndResetKeysValues = (object) =>
       // eslint-disable-next-line implicit-arrow-linebreak
       Object.keys(object).reduce((resetKeys, key) => {
-        // eslint-disable-next-line no-param-reassign
         const valueType = Object.prototype.toString.call(object[key]);
 
         switch (valueType) {
@@ -50,34 +49,26 @@ class StateCapsule extends Component {
       ...object,
       ready: true,
     };
-    // console.log('SC: resetState', newState);
+
     // Set local state with object-properties and initialize its values.
     this.setState(newState);
-    // console.log('SC: resetState', this.state);
   }
 
   /**
    * Sync local state with input field.
    */
   handleChange(event) {
-    // // console.log('Form Dev update');
     event.stopPropagation();
 
     const { id } = event.target;
     const details = id.split('-');
     const field = details[1];
     const { value } = event.target;
-    // // console.log('SC handleChange:', this.state, { details, value });
 
     this.setState({ [field]: value });
   }
 
   handleKeyPress(e) {
-    // console.log({
-    //   key: e.key,
-    //   value: e.target.value,
-    //   'data-chips': e.target.dataset.chips,
-    // });
     e.stopPropagation();
     const { chips } = e.target.dataset;
 
@@ -85,17 +76,11 @@ class StateCapsule extends Component {
       e.preventDefault();
 
       const details = e.target.id.split('-');
-
       const { field } = e.target.dataset;
       const { value } = e.target;
-      // eslint-disable-next-line react/destructuring-assignment
-      this.setState((prevState, props) => {
+
+      this.setState((prevState) => {
         const toUpdate = prevState[field];
-        // // console.log({
-        //   [field]: toUpdate,
-        //   prevState,
-        //   'prevState[field]': prevState[field],
-        // });
         toUpdate.push(value);
         return { [field]: toUpdate, [details[1]]: '' };
       });
@@ -113,9 +98,7 @@ class StateCapsule extends Component {
         field,
       },
     });
-    // eslint-disable-next-line arrow-parens
     return (e) => {
-      // console.log('SC createItem', { field });
       e.stopPropagation();
 
       e.target.dispatchEvent(createEvent);
@@ -129,9 +112,7 @@ class StateCapsule extends Component {
       detail: { field, index },
     });
 
-    // eslint-disable-next-line arrow-parens
     return (e) => {
-      // console.log('SC removeItem', { field, index });
       e.stopPropagation();
 
       //
@@ -145,7 +126,7 @@ class StateCapsule extends Component {
         || typeOfField === '[object Number]'
         || typeOfField === '[object Boolean]'
       ) {
-        this.setState((prevState, props) => {
+        this.setState((prevState) => {
           const toUpdate = prevState[field];
           toUpdate.splice(index, 1);
           return { [field]: toUpdate };
@@ -160,6 +141,7 @@ class StateCapsule extends Component {
     const stateCapsule = { ...this.state };
     delete stateCapsule.ready;
 
+    // eslint-disable-next-line react/prop-types
     const { children } = this.props;
 
     return ready ? (
