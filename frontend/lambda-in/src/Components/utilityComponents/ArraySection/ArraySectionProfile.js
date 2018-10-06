533996de0e687b15f29adee95d02f74f9f8e465d
jesuarva <jesuarva.developer@gmail.com>
2018-10-06 22:12:15 +0200

    CLEAN Code

--- frontend/lambda-in/src/Components/utilityComponents/ArraySection/ArraySectionProfile.js
+++ frontend/lambda-in/src/Components/utilityComponents/ArraySection/ArraySectionProfile.js
@@ -1,13 +1,16 @@
-import React, { Fragment } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from 'react';
 import DropDown from '../DropDown/DropDown';
-import StateCapsule from '../StateCapsule/StateCapsule';
 import MapDropDown from '../MapArrays/MapDropDown';
 
-// eslint-disable-next-line arrow-parens
 const ArraySectionProfile = (props) => {
-  const { header, userInfo, field, itemType, schema } = props;
+  // prettier-ignore
+  const {
+    header,
+    userInfo,
+    field,
+    itemType,
+    schema,
+  } = props;
 
   const propsAndLabels = Object.entries(schema);
 
