a8a8e25c2c5feebd765e57f7f2a463cd3ec9dcff
Nathaniel Flory <nathanielflory@gmail.com>
2018-10-05 20:52:55 -0400

    Style updates -Changed navBar component into an AppBar instead of Paper element -Imported MUI Theme -Created theme using mui theme for consistency of colors -Changed navBar primary color to indigo and font to white -Changed Landing page image to new image -Added new landing page png to project

--- frontend/lambda-in/src/Components/SignUpMenu/sign-up.js
+++ frontend/lambda-in/src/Components/SignUpMenu/sign-up.js
@@ -3,6 +3,22 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Route, Switch, Link, Redirect, withRouter } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#B79A3F',
+    },
+  },
+});
 
 class SignUpMenu extends React.Component {
   state = {
@@ -22,7 +38,7 @@ class SignUpMenu extends React.Component {
 
     return (
       <div>
-        <Button
+        <Button color='inherit'
           aria-owns={anchorEl ? 'simple-menu' : null}
           aria-haspopup="true"
           onClick={this.handleClick}
