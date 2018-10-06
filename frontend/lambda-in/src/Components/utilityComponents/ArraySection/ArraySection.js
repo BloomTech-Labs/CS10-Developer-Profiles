67c4755c6ce2980598a103b9130277b43d332364
jesuarva <jesuarva.developer@gmail.com>
2018-10-06 22:11:49 +0200

    ADD PropTypes to ArraySection.js component

--- frontend/lambda-in/src/Components/utilityComponents/ArraySection/ArraySection.js
+++ frontend/lambda-in/src/Components/utilityComponents/ArraySection/ArraySection.js
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DropDown from '../DropDown/DropDown';
 import StateCapsule from '../StateCapsule/StateCapsule';
 import MapDropDown from '../MapArrays/MapDropDown';
 
-// eslint-disable-next-line arrow-parens
 const ArraySection = (props) => {
-  const { header, userInfo, field, itemType, schema } = props;
+  // prettier-ignore
+  const {
+    header,
+    userInfo,
+    field,
+    itemType,
+    schema,
+  } = props;
 
   const propsAndLabels = Object.entries(schema);
 
@@ -58,6 +65,14 @@ const ArraySection = (props) => {
       />
     </DropDown>
   );
+};
+
+ArraySection.propTypes = {
+  header: PropTypes.string.isRequired,
+  userInfo: PropTypes.shape({}).isRequired,
+  field: PropTypes.string.isRequired,
+  itemType: PropTypes.string.isRequired,
+  schema: PropTypes.shape({}).isRequired,
 };
 
 export default ArraySection;
