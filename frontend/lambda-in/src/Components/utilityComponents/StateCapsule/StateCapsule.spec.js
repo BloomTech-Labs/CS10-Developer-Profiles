f8fcb8f1e45f7a1eda01e7585a3f493d2a9d6dad
jesuarva <jesuarva.developer@gmail.com>
2018-10-07 19:46:39 +0200

    TEST: wrap StateCapusle test in a  describe statement

--- frontend/lambda-in/src/Components/utilityComponents/StateCapsule/StateCapsule.spec.js
+++ frontend/lambda-in/src/Components/utilityComponents/StateCapsule/StateCapsule.spec.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import StateCapsule from './StateCapsule';
 
-const schema = {
-  summary: 'summary',
-  topSkills: 'topSkills',
-};
+describe('StateCapsule component', () => {
+  const schema = {
+    summary: 'summary',
+    topSkills: 'topSkills',
+  };
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <StateCapsule schema={schema} object={schema}>
-        {() => {}}
-      </StateCapsule>,
-    )
-    .toJSON();
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <StateCapsule schema={schema} object={schema}>
+          {() => {}}
+        </StateCapsule>,
+      )
+      .toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
 });
