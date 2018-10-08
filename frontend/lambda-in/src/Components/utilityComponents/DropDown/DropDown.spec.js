9a8b3a6ca1f15b088467fbb2991284c77b3879ec
jesuarva <jesuarva.developer@gmail.com>
2018-10-08 14:44:13 +0200

    REFACTOR DropDown.spec.js test to use TestWrapper.js helper

--- frontend/lambda-in/src/Components/utilityComponents/DropDown/DropDown.spec.js
+++ frontend/lambda-in/src/Components/utilityComponents/DropDown/DropDown.spec.js
@@ -1,29 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import DropDown from './DropDown';
+import TestWrapper from '../TestWrapper/TestWrapper';
 
 configure({ adapter: new Adapter() });
 
 describe('DropDown component', () => {
   const dropDown = <DropDown header="Testing DropDown component" />;
-
-  class Wrapper extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        header: 'Testing DropDown component',
-      };
-    }
-
-    render() {
-      // eslint-disable-next-line react/prop-types
-      const { children } = this.props;
-      return <Fragment>{children({ ...this.state })}</Fragment>;
-    }
-  }
 
   it('should renders correctly', () => {
     const tree = renderer.create(dropDown).toJSON();
@@ -32,7 +18,7 @@ describe('DropDown component', () => {
 
   it('should have a `header` props', () => {
     const instance = ReactTestUtils.renderIntoDocument(
-      <Wrapper>{({ header }) => <DropDown header={header} />}</Wrapper>,
+      <TestWrapper>{() => dropDown}</TestWrapper>,
     );
     const p = ReactTestUtils.findRenderedDOMComponentWithTag(instance, 'p');
 
