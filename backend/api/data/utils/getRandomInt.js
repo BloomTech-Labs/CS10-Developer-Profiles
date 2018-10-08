11e216f63dde81fc6fa1016bdf7e55731612fa84
Danny Vail <danny_vail@outlook.com>
2018-10-08 18:01:56 -0400

    FIX LINT ERRORS IN GETRANDOMINT

--- backend/api/data/utils/getRandomInt.js
+++ backend/api/data/utils/getRandomInt.js
@@ -1,6 +1,6 @@
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+const getRandomInt = (start, end) => {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
