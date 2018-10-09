b17b7d9f6073374c63ead455fb39480e9788f339
jesuarva <jesuarva.developer@gmail.com>
2018-10-10 00:00:02 +0200

    ORGANIZE Imports

--- frontend/lambda-in/src/Components/DevInfoEditz/SeekerEditUtils/BasicInfo.js
+++ frontend/lambda-in/src/Components/DevInfoEditz/SeekerEditUtils/BasicInfo.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import StateCapsule from '../../utilityComponents/StateCapsule/StateCapsule';
+import React from 'react';
 import SEEKER_PROP_TYPES from '../../seekerPropTypes';
+import StateCapsule from '../../utilityComponents/StateCapsule/StateCapsule';
 
 const BasicInfo = ({ userInfo }) => {
   const schema = {
