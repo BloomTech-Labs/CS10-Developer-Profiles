7d1bd37732fea1e0be42a4ebd19259df857875d0
Danny Vail <danny_vail@outlook.com>
2018-10-09 10:40:11 -0400

    ADD A UTIL FILE TO GENERATE RANDOM PROJECT JSON SCHEMA In getProjectSchema.js - Add getProjectImage() to return a randomly generated dummy image - Add getProjectLink() to return a randomly generated project link - Add getProjectRepo() to return a randomly generated GitHub Repo link - Add getProjects() to return a randomly generated list of projects. Can specify the max number of projects to return.

---
+++ backend/api/data/utils/getProjectSchema.js
@@ -0,0 +1,47 @@
+const faker = require('faker');
+const { getRandomInt } = require('./getRandomInt');
+const { getTechStack } = require('../datasets/skills');
+
+const getProjectImage = (blank) => {
+  const rand = Math.random();
+  const img = 'http://placeimg.com/1000/480/tech';
+
+  return blank && (rand >= 0 && rand < blank / 100) ? '' : img;
+};
+
+const getProjectLink = (blank) => {
+  const rand = Math.random();
+  const link = faker.internet.url();
+
+  return blank && (rand >= 0 && rand < blank / 100) ? '' : link;
+};
+
+const getProjectRepo = (firstName, lastName, blank) => {
+  const rand = Math.random();
+  const repo = `https://github.com/${firstName.toLowerCase()}-${lastName.toLowerCase()}/${faker.lorem.word()}`;
+
+  return blank && (rand >= 0 && rand < blank / 100) ? '' : repo;
+};
+
+const getProjects = (maxProjects, firstName, lastName, blank) => {
+  const rand = Math.random();
+  const numProjects = blank && (rand >= 0 && rand < blank / 100) ? 0 : getRandomInt(1, maxProjects);
+  const projects = [];
+
+  for (let i = 0; i < numProjects; i += 1) {
+    projects.push({
+      title: faker.lorem.words(getRandomInt(3, 5)),
+      description: faker.lorem.sentences(getRandomInt(1, 10)),
+      img: getProjectImage(20),
+      link: getProjectLink(60),
+      repo: getProjectRepo(firstName, lastName, 10),
+      tech: getTechStack(3, 5, 10),
+    });
+  }
+
+  return projects;
+};
+
+module.exports = {
+  getProjects,
+};
