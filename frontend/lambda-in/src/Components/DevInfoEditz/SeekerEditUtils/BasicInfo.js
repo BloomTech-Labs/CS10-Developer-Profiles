cf8a37e750d9a1d41eb3a2fbface828981b33a41
jesuarva <jesuarva.developer@gmail.com>
2018-10-09 23:52:27 +0200

    REFACTOR BasicInfo.js to use seekerPropTypes.js

--- frontend/lambda-in/src/Components/DevInfoEditz/SeekerEditUtils/BasicInfo.js
+++ frontend/lambda-in/src/Components/DevInfoEditz/SeekerEditUtils/BasicInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import StateCapsule from '../../utilityComponents/StateCapsule/StateCapsule';
+import SEEKER_PROP_TYPES from '../../seekerPropTypes';
 
 const BasicInfo = ({ userInfo }) => {
   const schema = {
@@ -75,7 +75,7 @@ const BasicInfo = ({ userInfo }) => {
 };
 
 BasicInfo.propTypes = {
-  userInfo: PropTypes.shape({}).isRequired,
+  userInfo: SEEKER_PROP_TYPES.isRequired,
 };
 
 export default BasicInfo;
