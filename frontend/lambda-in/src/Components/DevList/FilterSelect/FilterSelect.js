54f399402d84ff7c26832ced0d1611beb19ae8e6
Danny Vail <danny_vail@outlook.com>
2018-10-10 00:50:15 -0400

    REMOVE UNUSED IMPORTS IN FILTER SELECT COMPONENT

--- frontend/lambda-in/src/Components/DevList/FilterSelect/FilterSelect.js
+++ frontend/lambda-in/src/Components/DevList/FilterSelect/FilterSelect.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Select from 'react-select';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
