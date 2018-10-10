ff0c07dedf28d008bb5a6a3af50ae1bdcceda4dd
Danny Vail <danny_vail@outlook.com>
2018-10-10 02:14:08 -0400

    TRANSFORM FILTER SELECT COMPONENT INTO A STATELESS FUNCTIONAL COMPONENT

--- frontend/lambda-in/src/Components/DevList/FilterSelect/FilterSelect.js
+++ frontend/lambda-in/src/Components/DevList/FilterSelect/FilterSelect.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Select from 'react-select';
@@ -42,129 +42,120 @@ const styles = {
   },
 };
 
-class FilterSelect extends Component {
-  static inputComponent({ inputRef, ...props }) {
-    return <div ref={inputRef} {...props} />;
-  }
+const inputComponent = ({ inputRef, ...props }) => <div ref={inputRef} {...props} />;
 
-  static controlComponent(props) {
-    const { inputComponent } = FilterSelect;
-    const {
-      selectProps, innerRef, children, innerProps,
-    } = props;
+const controlComponent = (props) => {
+  const {
+    selectProps, innerRef, children, innerProps,
+  } = props;
 
-    return (
-      <TextField
-        fullWidth
-        InputProps={{
-          inputComponent,
-          inputProps: {
-            className: selectProps.classes.input,
-            inputRef: innerRef,
-            children,
-            ...innerProps,
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: selectProps.classes.input,
+          inputRef: innerRef,
+          children,
+          ...innerProps,
+        },
+      }}
+      {...selectProps.textFieldProps}
+    />
+  );
+};
+
+const menuComponent = (props) => {
+  const { selectProps, innerProps, children } = props;
+
+  return (
+    <Paper square className={selectProps.classes.paper} {...innerProps}>
+      {children}
+    </Paper>
+  );
+};
+
+const multiValueComponent = (props) => {
+  const {
+    children, selectProps, isFocused, removeProps,
+  } = props;
+
+  return (
+    <Chip
+      tabIndex={-1}
+      label={children}
+      className={classNames(selectProps.classes.chip, {
+        [selectProps.classes.chipFocused]: isFocused,
+      })}
+      onDelete={removeProps.onClick}
+      deleteIcon={<CancelIcon {...removeProps} />}
+    />
+  );
+};
+
+const optionComponent = (props) => {
+  const {
+    children, innerRef, innerProps, isFocused, isSelected,
+  } = props;
+
+  return (
+    <MenuItem
+      buttonRef={innerRef}
+      selected={isFocused}
+      component="div"
+      style={{
+        fontWeight: isSelected ? 500 : 400,
+      }}
+      {...innerProps}
+    >
+      {children}
+    </MenuItem>
+  );
+};
+
+const valueContainerComponent = (props) => {
+  const { selectProps, children } = props;
+  return <div className={selectProps.classes.valueContainer}>{children}</div>;
+};
+
+const FilterSelect = (props) => {
+  const {
+    classes, filterName, label, onChange, options, placeholder, val,
+  } = props;
+
+  return (
+    <div className={classes.filterSelect}>
+      <Select
+        classes={classes}
+        textFieldProps={{
+          label,
+          InputLabelProps: {
+            shrink: true,
+            className: classes.label,
           },
         }}
-        {...selectProps.textFieldProps}
-      />
-    );
-  }
-
-  static menuComponent(props) {
-    const { selectProps, innerProps, children } = props;
-
-    return (
-      <Paper square className={selectProps.classes.paper} {...innerProps}>
-        {children}
-      </Paper>
-    );
-  }
-
-  static multiValueComponent(props) {
-    const {
-      children, selectProps, isFocused, removeProps,
-    } = props;
-
-    return (
-      <Chip
-        tabIndex={-1}
-        label={children}
-        className={classNames(selectProps.classes.chip, {
-          [selectProps.classes.chipFocused]: isFocused,
-        })}
-        onDelete={removeProps.onClick}
-        deleteIcon={<CancelIcon {...removeProps} />}
-      />
-    );
-  }
-
-  static optionComponent(props) {
-    const {
-      children, innerRef, innerProps, isFocused, isSelected,
-    } = props;
-
-    return (
-      <MenuItem
-        buttonRef={innerRef}
-        selected={isFocused}
-        component="div"
-        style={{
-          fontWeight: isSelected ? 500 : 400,
+        options={options}
+        components={{
+          Control: controlComponent,
+          Menu: menuComponent,
+          MultiValue: multiValueComponent,
+          Option: optionComponent,
+          ValueContainer: valueContainerComponent,
         }}
-        {...innerProps}
-      >
-        {children}
-      </MenuItem>
-    );
-  }
-
-  static valueContainerComponent(props) {
-    const { selectProps, children } = props;
-    return <div className={selectProps.classes.valueContainer}>{children}</div>;
-  }
-
-  handleChange(value) {
-    const { filterName, onChange } = this.props;
-    onChange(value, filterName);
-  }
-
-  render() {
-    const {
-      classes, placeholder, options, label, val,
-    } = this.props;
-
-    return (
-      <div className={classes.filterSelect}>
-        <Select
-          classes={classes}
-          textFieldProps={{
-            label,
-            InputLabelProps: {
-              shrink: true,
-              className: classes.label,
-            },
-          }}
-          options={options}
-          components={{
-            Control: FilterSelect.controlComponent,
-            Menu: FilterSelect.menuComponent,
-            MultiValue: FilterSelect.multiValueComponent,
-            Option: FilterSelect.optionComponent,
-            ValueContainer: FilterSelect.valueContainerComponent,
-          }}
-          value={val}
-          onChange={value => this.handleChange(value)}
-          placeholder={placeholder}
-          isMulti
-        />
-      </div>
-    );
-  }
-}
+        value={val}
+        onChange={value => onChange(value, filterName)}
+        placeholder={placeholder}
+        isMulti
+      />
+    </div>
+  );
+};
 
 FilterSelect.propTypes = {
   classes: PropTypes.shape({}).isRequired,
   filterName: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
