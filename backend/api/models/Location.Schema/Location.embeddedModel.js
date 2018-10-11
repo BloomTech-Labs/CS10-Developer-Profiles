52dcbd6b0b6fe5bd8dfea3b730983f495618ecc5
Danny Vail <danny_vail@outlook.com>
2018-10-11 16:08:37 -0400

    MODIFY LOCATION MODEL SCHEMA TO SUPPORT GEOSPATIAL QUERIES In Location.embeddedModel.js - Add geoJSONSchema to model a GeoJSON object type - Modify locationSchema to hold a geoJSONSchema subdocument

--- backend/api/models/Location.Schema/Location.embeddedModel.js
+++ backend/api/models/Location.Schema/Location.embeddedModel.js
@@ -2,19 +2,26 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const geoJSONSchema = new Schema({
+  type: {
+    type: String,
+    required: [true, 'A GeoJSON object type must be declared.'],
+    enum: ['Point'],
+    default: 'Point',
+  },
+  coordinates: {
+    type: [Number],
+    required: [true, 'Coordinates are required as [longitude, latitude].'],
+  },
+  index: '2dsphere',
+});
+
 const locationSchema = new Schema({
   place: {
     type: String,
     required: [true, 'A location name is required'],
   },
-  lat: {
-    type: Number,
-    required: [true, 'Latitude cooridinate is required.'],
-  },
-  lng: {
-    type: Number,
-    required: [true, 'Longitude cooridinate is required.'],
-  },
+  location: geoJSONSchema,
 });
 
 module.exports = locationSchema;
