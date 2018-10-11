19bf8b6ef0b8a3ad80db57c4524918df795a25c0
jesuarva <jesuarva.developer@gmail.com>
2018-10-11 22:28:03 +0200

    TEST ComponentDidMount in DevInfoEditz.js component

--- frontend/lambda-in/src/Components/DevInfoEditz/DevInfoEditz.spec.js
+++ frontend/lambda-in/src/Components/DevInfoEditz/DevInfoEditz.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import DevInfoEdit from './DevInfoEditz';
@@ -14,6 +14,9 @@ import { DEV_TEST_DATA } from '../testData';
 
 configure({ adapter: new Adapter() });
 
+/**
+ * @todo test custom event listener attached to 'document'
+ */
 describe('DevInfoEditz component', () => {
   /**
    * Mock getting global state's userInfo data.
@@ -30,6 +33,40 @@ describe('DevInfoEditz component', () => {
     it('should renders correctly', () => {
       const tree = renderer.create(devInfoEditzComponent).toJSON();
       expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('Component did mount', () => {
+    const DEV_TEST_DATA_KEYS = Object.keys(DEV_TEST_DATA);
+    let mounted;
+    let instance;
+    beforeAll(() => {
+      mounted = mount(devInfoEditzComponent);
+      instance = mounted.instance();
+    });
+    it('should initialize "state"', () => {
+      const { state } = instance;
+
+      /**
+       * 'this.state' shall have all global-state properties.
+       */
+      const isInitializedCorrectly = DEV_TEST_DATA_KEYS.reduce(
+        (control, key) => (!(key in state) ? false : control),
+        true,
+      );
+      expect(isInitializedCorrectly).toBeTruthy();
+    });
+    it('should initialize "userStateCopy"', () => {
+      const { userStateCopy } = instance;
+
+      /**
+       * 'this.state' shall have all global-state properties.
+       */
+      const isInitializedCorrectly = DEV_TEST_DATA_KEYS.reduce(
+        (control, key) => (!(key in userStateCopy) ? false : control),
+        true,
+      );
+      expect(isInitializedCorrectly).toBeTruthy();
     });
   });
 });
